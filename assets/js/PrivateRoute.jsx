91ea31259b37c6173b7f920dc27ed8fa1a87205b
Andrew Allen <allenan@helium.com>
2018-03-22 10:26:57 -0700

    cleanup

--- assets/js/PrivateRoute.jsx
+++ assets/js/PrivateRoute.jsx
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Redirect, withRouter } from 'react-router'
 import { connect } from 'react-redux';
-// import socket from './socket';
 
 class PrivateRoute extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    // do websocket stuff here...
-    // let channel = socket.channel("event:all", {})
-    // channel.join()
-    //   .receive("ok", resp => { console.log("Joined successfully", resp) })
-    //   .receive("error", resp => { console.log("Unable to join", resp) })
-  }
-
   render() {
     const { path } = this.props;
     const { isLoggedIn } = this.props.auth;
