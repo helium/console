28b7856d051a0f58416820b1a3b373bab30390b5
victor <victor@helium.com>
2018-03-22 17:55:43 -0700

    Add toggling of captcha sitekey dependent on environment

--- assets/js/config/common.js
+++ assets/js/config/common.js
@@ -1,6 +1,12 @@
+let sitekey
+
+if (process.env.NODE_ENV === "development") {
+  sitekey = "6Lew200UAAAAACN3_-tS_UvTcnhF2mlZCzzQ4Na5"
+}
+
 const config = {
-  "recaptcha": {
-    "sitekey": "6Lew200UAAAAACN3_-tS_UvTcnhF2mlZCzzQ4Na5"
+  recaptcha: {
+    sitekey
   }
 }
 
