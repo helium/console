d6af70f316807f888b5f0f8b51ff7196d09d8561
Andrew Allen <allenan@helium.com>
2018-03-28 15:33:24 -0700

    remove unnecessary event actions

--- assets/js/actions/event.js
+++ assets/js/actions/event.js
@@ -1,26 +1,8 @@
 import * as rest from '../util/rest';
 import { normalizeEvent } from '../schemas/event'
 
-export const FETCH_EVENTS = 'FETCH_EVENTS'
-export const RECEIVED_EVENTS = 'RECEIVED_EVENTS'
 export const RECEIVED_EVENT = 'RECEIVED_EVENT'
 
-
-export const fetchEvents = () => {
-  return (dispatch) => {
-    rest.get('/api/events')
-      .then(response => {
-        return dispatch(receivedEvents(response.data))
-      })
-  }
-}
-
-export const receivedEvents = (events) => {
-  return {
-    type: RECEIVED_EVENTS,
-    events
-  }
-}
 
 export const receivedEvent = (event) => {
   const entities = normalizeEvent(event)
