f33662994b0da499dfd22843637f11bb32be3ed9
Andrew Allen <allenan@helium.com>
2018-04-03 15:03:28 -0700

    revert common.js to old way

--- assets/js/config/common.js
+++ assets/js/config/common.js
@@ -1,20 +1,15 @@
-const setConfig = (env) => {
-  switch(env) {
-    case "production":
-      return {
-        recaptcha: {
-          sitekey: "6LdqwFAUAAAAAJ8_ili9I1J_Yga3SO35VrIFNqHE"
-        }
-      }
-    default:
-      return {
-        recaptcha: {
-          sitekey: "6Lew200UAAAAACN3_-tS_UvTcnhF2mlZCzzQ4Na5"
-        }
-      }
-  }
+let sitekey
+
+if (process.env.REACT_ENV === "production") {
+  sitekey = "6LdqwFAUAAAAAJ8_ili9I1J_Yga3SO35VrIFNqHE"
+} else {
+  sitekey = "6Lew200UAAAAACN3_-tS_UvTcnhF2mlZCzzQ4Na5"
 }
 
-const config = setConfig(process.env.REACT_ENV)
+const config = {
+  recaptcha: {
+    sitekey
+  }
+}
 
 export default config
