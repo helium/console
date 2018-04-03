eca5cc30ddf85ac56a14b87053347dfe44228dd6
Andrew Allen <allenan@helium.com>
2018-04-03 14:50:01 -0700

    node env config

--- assets/js/config/common.js
+++ assets/js/config/common.js
@@ -1,13 +1,22 @@
-let sitekey
-
-if (process.env.NODE_ENV === "development") {
-  sitekey = "6Lew200UAAAAACN3_-tS_UvTcnhF2mlZCzzQ4Na5"
-}
-
-const config = {
-  recaptcha: {
-    sitekey
+const setConfig = (env) => {
+  switch(env) {
+    case "development":
+      return {
+        recaptcha: {
+          sitekey: "6Lew200UAAAAACN3_-tS_UvTcnhF2mlZCzzQ4Na5"
+        }
+      }
+    case "production":
+      return {
+        recaptcha: {
+          sitekey: "6LdqwFAUAAAAAJ8_ili9I1J_Yga3SO35VrIFNqHE"
+        }
+      }
+    default:
+      return {}
   }
 }
+
+const config = setConfig(process.env.NODE_ENV)
 
 export default config
