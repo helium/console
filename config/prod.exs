de5f47c796f6dab66c74a2f8062f73e976d66de8
Andrew Allen <allenan@helium.com>
2018-04-03 13:44:48 -0700

    production config for staging on heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,11 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :console, ConsoleWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "helium-console-staging.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :console, Console.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
