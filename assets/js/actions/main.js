fe788fad8f979af8083a85a6708baafa96cad844
Andrew Allen <allenan@helium.com>
2018-04-04 13:25:31 -0700

    prevent flashes by fetching index pages when app loads

--- assets/js/actions/main.js
+++ assets/js/actions/main.js
@@ -1,1 +1,16 @@
+import { fetchDevices } from './device'
+import { fetchGateways } from './gateway'
+import { fetchChannels } from './channel'
+import { fetchTeams } from './team'
+
 export const DELETED_ENTITY = "DELETED_ENTITY"
+
+
+export const fetchIndices = () => {
+  return (dispatch) => {
+    dispatch(fetchDevices())
+    dispatch(fetchGateways())
+    dispatch(fetchChannels())
+    dispatch(fetchTeams())
+  }
+}
