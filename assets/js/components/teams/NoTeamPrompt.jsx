5c0b00002b400aef433a944fd51b9820f0682d08
Andrew Allen <allenan@helium.com>
2018-04-19 12:56:20 -0700

    new team form when user has no teams

---
+++ assets/js/components/teams/NoTeamPrompt.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { createTeam, switchTeam } from '../../actions/team'
+import { logOut } from '../../actions/auth'
+import AuthLayout from '../common/AuthLayout'
+
+// MUI
+import TextField from 'material-ui/TextField';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  title: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
+  input: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+  forgot: {
+    textAlign: 'right',
+    marginBottom: theme.spacing.unit * 2,
+  },
+  formButton: {
+    marginTop: theme.spacing.unit * 2,
+  },
+  extraLinks: {
+    marginTop: theme.spacing.unit * 2,
+    textAlign: 'center'
+  }
+});
+
+class NoTeamPrompt extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputUpdate = this.handleInputUpdate.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.props.createTeam(this.state.name, '/devices')
+  }
+
+  handleInputUpdate(e) {
+    this.setState({ name: e.target.value})
+  }
+
+  render() {
+    const { classes, logOut } = this.props
+
+    return (
+      <AuthLayout>
+        <Card>
+          <CardContent>
+            <Typography variant="headline" className={classes.title}>
+              You don't have any teams
+            </Typography>
+            <Typography variant="subheading" className={classes.title}>
+              Create a new team to get started
+            </Typography>
+
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                label="Team Name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleInputUpdate}
+                className={classes.input}
+                fullWidth
+              />
+              <Button
+                type="submit"
+                variant="raised"
+                color="primary"
+                size="large"
+                fullWidth
+                className={classes.formButton}
+              >
+                Create Team
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+
+        <Typography component="p" className={classes.extraLinks}>
+          <a
+            href="#"
+            onClick={logOut}
+          >
+            Log out
+          </a>
+        </Typography>
+
+      </AuthLayout>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    teams: Object.values(state.entities.teams),
+    currentTeam: state.entities.teams[state.auth.currentTeamId]
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ createTeam, switchTeam, logOut }, dispatch);
+}
+
+const styled = withStyles(styles)(NoTeamPrompt)
+export default connect(mapStateToProps, mapDispatchToProps)(styled);
--- lib/console_web/plug/put_current_team.ex
+++ lib/console_web/plug/put_current_team.ex
@@ -4,18 +4,26 @@ defmodule ConsoleWeb.Plug.PutCurrentTeam do
   def init(default), do: default
 
   def call(conn, _default) do
-    %{"team" => team_id} = ConsoleWeb.Guardian.Plug.current_claims(conn)
-    current_user = conn.assigns.current_user
+    case ConsoleWeb.Guardian.Plug.current_claims(conn) do
+      %{"team" => team_id} ->
+        current_user = conn.assigns.current_user
 
-    if current_team = Console.Teams.get_team(current_user, team_id) do
-      conn
-      |> assign(:current_team, current_team)
-    else
-      conn
-      |> send_resp(
-        :forbidden,
-        Poison.encode!(%{type: "forbidden_team", errors: ["You don't have access to this team"]})
-      )
+        if current_team = Console.Teams.get_team(current_user, team_id) do
+          conn
+          |> assign(:current_team, current_team)
+        else
+          conn
+          |> send_resp(
+            :forbidden,
+            Poison.encode!(%{
+              type: "forbidden_team",
+              errors: ["You don't have access to this team"]
+            })
+          )
+        end
+
+      _ ->
+        conn
     end
   end
 end
