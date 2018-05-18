b152a9fdf5664e67d323d3d5c0d1a1b3addae5da
Andrew Allen <allenan@helium.com>
2018-05-18 15:44:04 -0700

    restrict events lookup by current team context

--- lib/console/events/event_resolver.ex
+++ lib/console/events/event_resolver.ex
@@ -3,7 +3,6 @@ defmodule Console.Events.EventResolver do
 
   alias Console.Repo
   alias Absinthe.Relay.Connection
-  alias Console.Events.Event
 
   def connection(pagination_args, %{source: device}) do
     Ecto.assoc(device, :events)
@@ -11,13 +10,13 @@ defmodule Console.Events.EventResolver do
     |> Connection.from_query(&Repo.all/1, pagination_args)
   end
 
-  def paginate(%{device_id: device_id, page: page, page_size: page_size}, context) do
+  def paginate(%{device_id: device_id, page: page, page_size: page_size}, %{context: %{current_team: current_team}}) do
+    device = Ecto.assoc(current_team, :devices) |> Repo.get!(device_id)
+
     events =
-      Event
-      |> where([e], e.device_id == ^device_id)
+      Ecto.assoc(device, :events)
       |> order_by([desc: :reported_at])
       |> Repo.paginate(page: page, page_size: page_size)
-    IO.inspect(events)
     {:ok, events}
   end
 end
