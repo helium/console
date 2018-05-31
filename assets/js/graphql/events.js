43dfa54d24c0ac38411b75a90ca9ef37dae4f9c3
victor <victor@helium.com>
2018-05-30 17:19:37 -0700

    Fix issue

--- assets/js/graphql/events.js
+++ assets/js/graphql/events.js
@@ -1,19 +1,5 @@
 import gql from 'graphql-tag';
 
-export const EVENTS_SUBSCRIPTION = gql`
-  subscription onEventAdded($contextId: String) {
-    eventAdded(contextId: $contextId) {
-      id,
-      description,
-      rssi,
-      payload_size,
-      reported_at,
-      status,
-      direction
-    }
-  }
-`
-
 export const EventFragment = gql`
   fragment EventFragment on Event {
     id,
@@ -24,4 +10,12 @@ export const EventFragment = gql`
     status,
     direction
   }
+`
+export const EVENTS_SUBSCRIPTION = gql`
+  subscription onEventAdded($contextId: String) {
+    eventAdded(contextId: $contextId) {
+      ...EventFragment
+    }
+  }
+  ${EventFragment}
 `
