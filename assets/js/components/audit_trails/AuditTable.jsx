336d8336f8e8fdd7972ed150eee65524d51cc4c8
victor <victor@helium.com>
2018-06-04 10:02:47 -0700

    Refactor table pagination

--- assets/js/components/audit_trails/AuditTable.jsx
+++ assets/js/components/audit_trails/AuditTable.jsx
@@ -14,8 +14,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
+import TableFooterPagination from '../common/TableFooterPagination'
 
 // Icons
 import DashboardIcon from '@material-ui/icons/Dashboard';
@@ -65,7 +64,7 @@ class AuditTable extends Component {
       )
     }
     const { auditTrails } = this.props.data
-    const { page } = this.state
+    const { page, pageSize } = this.state
 
     return (
       <CardContent>
@@ -93,17 +92,13 @@ class AuditTable extends Component {
               </TableRow>
             ))}
           </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                count={auditTrails.totalEntries}
-                onChangePage={(e, page) => this.handleChangePage(page + 1)}
-                onChangeRowsPerPage={(e) => this.handleChangeRowsPerPage(e.target.value)}
-                page={page - 1}
-                rowsPerPage={auditTrails.pageSize}
-              />
-            </TableRow>
-          </TableFooter>
+          <TableFooterPagination
+            totalEntries={auditTrails.totalEntries}
+            page={page}
+            pageSize={pageSize}
+            handleChangePage={this.handleChangePage}
+            handleChangeRowsPerPage={this.handleChangeRowsPerPage}
+          />
         </Table>
       </CardContent>
     )
