049fec39e166055b3882c5855dfe8d98145fba71
Andrew Allen <allenan@helium.com>
2018-06-07 14:13:30 -0700

    refactor gateways index and map

--- assets/js/components/gateways/GatewaysTable.jsx
+++ assets/js/components/gateways/GatewaysTable.jsx
@@ -1,95 +1,90 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import random from 'lodash/random'
-import sample from 'lodash/sample'
-import userCan from '../../util/abilities'
 import { parseLocation } from '../../util/geolocation'
 
+import UserCan from '../common/UserCan'
+import PaginatedTable from '../common/PaginatedTable'
+import { PAGINATED_GATEWAYS, GATEWAY_SUBSCRIPTION } from '../../graphql/gateways'
+import BlankSlate from '../common/BlankSlate'
+
+// redux
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { deleteGateway } from '../../actions/gateway'
+
 // MUI
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableFooterPagination from '../common/TableFooterPagination'
 import Button from '@material-ui/core/Button';
 
+@connect(null, mapDispatchToProps)
 class GatewaysTable extends Component {
-
   render() {
-    const { gateways, deleteGateway, totalEntries, page, pageSize } = this.props
+    const { deleteGateway } = this.props
 
-    return(
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>MAC</TableCell>
-            <TableCell>Location</TableCell>
-            <TableCell>Earnings</TableCell>
-            <TableCell>Ask</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {gateways.map(gateway => {
-            return (
-              <TableRow key={gateway.id}>
-                <TableCell>
-                  <Link to={`/gateways/${gateway.id}`}>{gateway.name}</Link>
-                </TableCell>
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name',
+        Cell: props => <Link to={`/gateways/${props.row.id}`}>{props.value}</Link>
+      },
+      {
+        Header: 'MAC',
+        accessor: 'mac'
+      },
+      {
+        Header: 'Location',
+        accessor: 'location',
+        Cell: props => <span>{parseLocation(props.value)}</span>
+      },
+      {
+        Header: 'Earnings',
+        accessor: 'mac',
+        Cell: props => <span> {random(0, 100000).toLocaleString()} HLM </span>
+      },
+      {
+        Header: 'Ask',
+        accessor: 'mac',
+        Cell: props => <span> {random(0.01, 5.0).toFixed(2)} HLM </span>
+      },
+      {
+        Header: '',
+        numeric: true,
+        Cell: props => <span>
+          <Button
+            color="primary"
+            component={Link}
+            to={`/gateways/${props.row.id}`}
+            size="small"
+          >
+            View
+          </Button>
 
-                <TableCell>
-                  {gateway.mac}
-                </TableCell>
+          <UserCan action="delete" itemType="gateway" item={props.row}>
+            <Button
+              color="secondary"
+              onClick={() => deleteGateway(props.row.id)}
+              size="small"
+            >
+              Delete
+            </Button>
+          </UserCan>
+        </span>
+      },
+    ]
 
-                <TableCell>
-                  {parseLocation(gateway.location)}
-                </TableCell>
-
-                <TableCell>
-                  {random(0, 100000).toLocaleString()} HLM
-                </TableCell>
-
-                <TableCell>
-                  {random(0.01, 5.0).toFixed(2)} HLM
-                </TableCell>
-
-                <TableCell numeric>
-                  <Button
-                    color="primary"
-                    component={Link}
-                    to={`/gateways/${gateway.id}`}
-                    size="small"
-                  >
-                    View
-                  </Button>
-
-                  {userCan('delete', 'gateway', gateway) &&
-                    <Button
-                      color="secondary"
-                      onClick={() => deleteGateway(gateway.id)}
-                      size="small"
-                    >
-                      Delete
-                    </Button>
-                  }
-
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-        <TableFooterPagination
-          totalEntries={totalEntries}
-          page={page}
-          pageSize={pageSize}
-          handleChangePage={this.props.handleChangePage}
-          handleChangeRowsPerPage={this.props.handleChangeRowsPerPage}
-        />
-      </Table>
+    return (
+      <PaginatedTable
+        columns={columns}
+        query={PAGINATED_GATEWAYS}
+        subscription={GATEWAY_SUBSCRIPTION}
+        EmptyComponent={ props => <BlankSlate title="No gateways" subheading="To create a new gateway, click the red button in the corner" /> }
+      />
     )
   }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ deleteGateway }, dispatch);
 }
 
 export default GatewaysTable
