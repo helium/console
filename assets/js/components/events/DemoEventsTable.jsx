9976577df73176ad7d714947ccc6b5f3b9517817
victor <victor@helium.com>
2018-11-06 14:25:35 -0800

    add effect for new row

--- assets/js/components/events/DemoEventsTable.jsx
+++ assets/js/components/events/DemoEventsTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { formatDatetime } from '../../util/time'
 import { DEMO_EVENTS_SUBSCRIPTION, EVENT_FRAGMENT } from '../../graphql/events'
+import { withStyles } from '@material-ui/core/styles';
 
 // GraphQL
 import { graphql } from 'react-apollo';
@@ -13,9 +14,34 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
+const styles = theme => ({
+  newRow: {
+    backgroundColor: "#E8E8E8"
+  },
+});
+
+@withStyles(styles)
 class DemoEventsTable extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newRow: false
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.data.demoEvents && this.props.data.demoEvents.length - 1 === prevProps.data.demoEvents.length) {
+      this.setState({ newRow: true })
+      setTimeout(() => {
+        this.setState({ newRow: false})
+      }, 1000)
+    }
+  }
+
   render() {
     const { demoEvents } = this.props.data
+    const { classes } = this.props
+    const { newRow } = this.state
     const columns = [
       {
         Header: 'Description',
@@ -63,12 +89,12 @@ class DemoEventsTable extends Component {
           </TableHead>
           <TableBody>
             {demoEvents.map((event, i) =>
-              <TableRow key={`row-${i}`}>
+              <TableRow key={`row-${i}`} className={(i == 0 && newRow) ? classes.newRow : ""}>
                 {columns.map(column =>
                     <TableCell key={`row-${i}-${column.accessor}`}>
                       {
                         (column.Header == "Time") ?
-                          formatDatetime(demoEvents[i][column.accessor]) : 
+                          formatDatetime(demoEvents[i][column.accessor]) :
                           demoEvents[i][column.accessor]
                       }
                     </TableCell>
