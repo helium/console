e0c446d65c98a3a195a4459483357b15ea920918
victor <victor@helium.com>
2019-10-08 12:27:25 -0700

    Update console url

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :console, ConsoleWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "demo.helium.com", port: 443],
+  url: [scheme: "https", host: "helium-console-staging.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
