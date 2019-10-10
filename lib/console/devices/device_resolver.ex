b6d65a294d326cb4f8dd492af2a41af9f90ccb4c
victor <victor@helium.com>
2019-10-10 15:45:41 -0700

    Add sorting to device resolver

--- lib/console/devices/device_resolver.ex
+++ lib/console/devices/device_resolver.ex
@@ -1,9 +1,12 @@
 defmodule Console.Devices.DeviceResolver do
   alias Console.Repo
+  alias Console.Devices.Device
+  import Ecto.Query
 
   def paginate(%{page: page, page_size: page_size}, %{context: %{current_team: current_team}}) do
-    devices =
-      Ecto.assoc(current_team, :devices)
+    devices = Device
+      |> where([d], d.team_id == ^current_team.id)
+      |> order_by(asc: :seq_id)
       |> Repo.paginate(page: page, page_size: page_size)
     {:ok, devices}
   end
