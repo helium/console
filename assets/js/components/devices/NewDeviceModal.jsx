012a4b2a5ff2a695b95ddca82f3c5e345540e82e
victor <victor@helium.com>
2020-01-09 10:16:16 -0800

    Update device modal to antd

--- assets/js/components/devices/NewDeviceModal.jsx
+++ assets/js/components/devices/NewDeviceModal.jsx
@@ -4,30 +4,10 @@ import { bindActionCreators } from 'redux';
 import { createDevice } from '../../actions/device'
 import { randomMac } from '../../util/random'
 import analyticsLogger from '../../util/analyticsLogger'
+import { Modal, Button, Typography, Input } from 'antd';
+const { Text } = Typography
 
-// MUI
-import Typography from '@material-ui/core/Typography';
-import Modal from '@material-ui/core/Modal';
-import Button from '@material-ui/core/Button'
-import Paper from '@material-ui/core/Paper'
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
-
-const styles = theme => ({
-  paper: {
-    margin: 'auto',
-    marginTop: '10%',
-    width: '50%',
-    padding: theme.spacing.unit * 2,
-    minWidth: 420,
-  },
-  input: {
-    marginBottom: theme.spacing.unit * 2,
-  },
-})
-
-@withStyles(styles)
-@connect(mapStateToProps, mapDispatchToProps)
+@connect(null, mapDispatchToProps)
 class NewDeviceModal extends Component {
   constructor(props) {
     super(props);
@@ -59,42 +39,28 @@ class NewDeviceModal extends Component {
 
     return (
       <Modal
-        open={open}
-        onClose={onClose}
+        title="Create a new device"
+        visible={open}
+        onCancel={onClose}
+        onOk={this.handleSubmit}
+        footer={[
+          <Button key="back" onClick={onClose}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={this.handleSubmit}>
+            Submit
+          </Button>,
+        ]}
       >
-        <Paper className={classes.paper}>
-          <Typography variant="title">
-            Create a new device
-          </Typography>
-
-          <form onSubmit={this.handleSubmit}>
-            <TextField
-              label="Device Name"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleInputUpdate}
-              className={classes.input}
-              fullWidth
-            />
-
-            <Button
-              type="submit"
-              variant="raised"
-              color="primary"
-              size="large"
-              className={classes.formButton}
-            >
-              Create Device
-            </Button>
-          </form>
-        </Paper>
+        <Input
+          placeholder="Device Name"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleInputUpdate}
+        />
       </Modal>
     )
   }
-}
-
-function mapStateToProps(state) {
-  return {}
 }
 
 function mapDispatchToProps(dispatch) {
