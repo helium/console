0259ad497be03c3b2d1d24097e4f848740a04d8d
victor <victor@helium.com>
2020-01-30 12:25:42 -0800

    Force https

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :console, ConsoleWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [hsts: true],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
