2c5c14c06f49895ea5dced6d36f69cd68a9d3554
victor <victor@helium.com>
2020-02-25 16:30:13 -0800

    Remove restrictions on manager permissions temporarily

--- lib/console_web/abilities.ex
+++ lib/console_web/abilities.ex
@@ -6,13 +6,14 @@ defmodule ConsoleWeb.Abilities do
   end
 
   def can?(%Membership{role: "manager"}, action, controller) do
-    cond do
-      controller == ConsoleWeb.DeviceController and action in [:create, :update, :delete] -> false
-      controller == ConsoleWeb.InvitationController and action in [:create, :delete] -> false
-      controller == ConsoleWeb.MembershipController and action in [:update, :delete] -> false
-      controller == ConsoleWeb.OrganizationController and action in [:delete_organization] -> false
-      true -> true
-    end
+    # cond do
+    #   controller == ConsoleWeb.DeviceController and action in [:create, :update, :delete] -> false
+    #   controller == ConsoleWeb.InvitationController and action in [:create, :delete] -> false
+    #   controller == ConsoleWeb.MembershipController and action in [:update, :delete] -> false
+    #   controller == ConsoleWeb.OrganizationController and action in [:delete_organization] -> false
+    #   true -> true
+    # end
+    true
   end
 
   def can?(_membership, _action, _controller), do: false
