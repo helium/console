8dce568b02843840aeaad75aa88aa3ccfce189e5
victor <victor@helium.com>
2020-03-12 17:08:46 -0700

    Update rate limiting field for heroku

--- lib/console_web/plug/rate_limit.ex
+++ lib/console_web/plug/rate_limit.ex
@@ -4,7 +4,7 @@ defmodule ConsoleWeb.Plug.RateLimit do
   def init(default), do: default
 
   def call(conn, [action, limit]) do
-    ip_address = conn.remote_ip |> :inet_parse.ntoa |> to_string()
+    ip_address = conn |> get_req_header("x-forwarded-for") |> List.first()
 
     case Hammer.check_rate("#{action}:#{ip_address}", 60_000, limit) do
       {:allow, _count} ->
