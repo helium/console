9cfdd08ef6e6e1d76b7233578f11b572192044eb
victor <victor@helium.com>
2020-03-26 12:47:11 -0700

    Change labels table on device page to small LabelTags

---
+++ assets/js/components/devices/DeviceShowLabelsAttached.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import LabelTag from '../common/LabelTag'
+import UserCan from '../common/UserCan'
+import { Tag, Icon } from 'antd';
+
+class DeviceShowLabelsAttached extends Component {
+  handleRemoveLabel = label => {
+    this.props.openDeviceRemoveLabelModal([label])
+  }
+
+  render() {
+    const { labels, openDevicesAddLabelModal } = this.props
+
+    return (
+      <UserCan
+        alternate={
+          <div>
+            {
+              labels.map(l => (
+                <LabelTag
+                  key={l.id}
+                  text={l.name}
+                  color={l.color}
+                  style={{ marginBottom: 3 }}
+                />
+              ))
+            }
+          </div>
+        }
+      >
+        <div>
+          {
+            labels.map(l => (
+              <LabelTag
+                key={l.id}
+                text={l.name}
+                color={l.color}
+                style={{ marginBottom: 3 }}
+                closable
+                onClose={e => {
+                  e.preventDefault()
+                  this.handleRemoveLabel(l)
+                }}
+              />
+            ))
+          }
+          <Tag size="small" onClick={openDevicesAddLabelModal}>
+            + Add Label
+          </Tag>
+        </div>
+      </UserCan>
+    )
+  }
+}
+
+export default DeviceShowLabelsAttached
