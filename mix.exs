e7347ec9d3219765c22bea9fc9a3b1bb585c7cb4
Andrew Allen <allenan@helium.com>
2020-04-02 11:36:27 -0700

    revert elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Console.Mixfile do
     [
       app: :console,
       version: "0.0.1",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9.0",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
