4321b0364b6875c4144141b4508eb4c37ff26491
victor <victor@helium.com>
2020-04-13 18:01:41 -0700

    Add port number to function new

--- assets/js/components/functions/FunctionValidator.jsx
+++ assets/js/components/functions/FunctionValidator.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import DashboardLayout from '../common/DashboardLayout'
 import { primaryBlue } from '../../util/colors'
-import { Typography, Card, Icon, Input, Row, Col } from 'antd';
+import { Typography, Card, Icon, Input, InputNumber, Row, Col } from 'antd';
 const { Text } = Typography
 const { TextArea } = Input
 import Editor from 'react-simple-code-editor';
@@ -19,16 +19,18 @@ class FunctionValidator extends Component {
       <Row gutter={20} type="flex">
         <Col sm={14}>
           <Card title="Step 2 - Enter Custom Script" style={{ height: 'calc(100% - 20px)'}} bodyStyle={{ padding: 0 }}>
-          <Editor
-            value={this.state.code}
-            onValueChange={code => this.setState({ code })}
-            highlight={code => highlight(code, languages.js)}
-            padding={10}
-            style={{
-              fontFamily: 'monospace',
-              fontSize: 14,
-            }}
-          />
+            <div>
+              <Editor
+                value={this.state.code}
+                onValueChange={code => this.setState({ code })}
+                highlight={code => highlight(code, languages.js)}
+                padding={10}
+                style={{
+                  fontFamily: 'monospace',
+                  fontSize: 14,
+                }}
+              />
+            </div>
           </Card>
         </Col>
 
@@ -39,18 +41,27 @@ class FunctionValidator extends Component {
               <Icon type="play-circle" theme="filled" style={{ color: primaryBlue, fontSize: 22 }} onClick={() => {}}/>
             }
           >
-            <div>
-              <Text>Payload Input</Text>
-              <Input
-                style={{ marginTop: 5 }}
-                placeholder="Enter encoded payload here"
-              />
-            </div>
+            <Row gutter={10}>
+              <Col span={16}>
+                <Text>Payload Input</Text>
+                <Input
+                  style={{ marginTop: 5, width: '100%' }}
+                  placeholder="Enter encoded payload here"
+                />
+              </Col>
+              <Col span={8}>
+                <Text>Port Number</Text>
+                <InputNumber
+                  type="number"
+                  style={{ marginTop: 5, width: '100%' }}
+                />
+              </Col>
+            </Row>
             <div style={{ marginTop: 24 }}>
               <Text>Payload Output</Text>
               <TextArea
                 value={"test"}
-                style={{ minHeight: 200, marginTop: 5 }}
+                style={{ minHeight: 200, maxHeight: 200, marginTop: 5 }}
               />
             </div>
           </Card>
