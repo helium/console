8b9738f4e3c5be7a9eb5f3b38e0ff8599af7d206
victor <victor@helium.com>
2020-04-16 15:55:21 -0700

    Style validator code entry box

--- assets/js/components/functions/FunctionValidator.jsx
+++ assets/js/components/functions/FunctionValidator.jsx
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
 import DashboardLayout from '../common/DashboardLayout'
-import { primaryBlue } from '../../util/colors'
+import { primaryBlue, codeEditorLineColor, codeEditorBgColor } from '../../util/colors'
 import { Typography, Card, Icon, Input, InputNumber, Row, Col } from 'antd';
 const { Text } = Typography
 const { TextArea } = Input
+import range from 'lodash/range'
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 
 class FunctionValidator extends Component {
+  onClickEditor = () => {
+    const editor = document.getElementsByClassName("npm__react-simple-code-editor__textarea")[0]
+    editor.focus()
+  }
   render() {
     return (
       <Row gutter={20} type="flex">
         <Col sm={14}>
-          <Card title={this.props.title} style={{ height: 'calc(100% - 20px)'}} bodyStyle={{ padding: 0 }}>
-            <div>
+          <Card title={this.props.title} style={{ height: 560, overflow: 'hidden'}} bodyStyle={{ padding: 0 }}>
+            <div style={{ display: 'flex', flexDirection: 'row', cursor: 'text', height: 503, overflowY: 'scroll' }} onClick={this.onClickEditor}>
+              <div style={{ backgroundColor: codeEditorBgColor, paddingTop: 9, marginTop: 1, paddingBottom: 9 }}>
+                {
+                  range(201).map(i => (
+                    <p
+                      key={i}
+                      style={{
+                        textAlign: 'right',
+                        fontFamily: 'monospace',
+                        color: codeEditorLineColor,
+                        fontSize: 14,
+                        marginBottom: 0,
+                        paddingLeft: 10,
+                        paddingRight: 10,
+                        backgroundColor: codeEditorBgColor
+                      }}
+                    >
+                      {i}
+                    </p>
+                  ))
+                }
+              </div>
+
               <Editor
                 value={this.props.body}
                 onValueChange={this.props.handleFunctionUpdate}
@@ -33,6 +60,7 @@ class FunctionValidator extends Component {
         <Col sm={10}>
           <Card
             title="Script Validator"
+            style={{ height: 560 }}
             extra={
               <Icon type="play-circle" theme="filled" style={{ color: primaryBlue, fontSize: 22 }} onClick={() => {}}/>
             }
@@ -57,7 +85,7 @@ class FunctionValidator extends Component {
               <Text>Payload Output</Text>
               <TextArea
                 value={"test"}
-                style={{ minHeight: 200, maxHeight: 200, marginTop: 5 }}
+                style={{ minHeight: 340, maxHeight: 340, marginTop: 5 }}
               />
             </div>
           </Card>
