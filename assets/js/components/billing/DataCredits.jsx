29decd9d680b50d173675b48bc219d84930d4542
Peter Main <toiletbleach@gmail.com>
2020-05-08 13:48:26 +0100

    DC page blank state

--- assets/js/components/billing/DataCredits.jsx
+++ assets/js/components/billing/DataCredits.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import DashboardLayout from '../common/DashboardLayout'
 import analyticsLogger from '../../util/analyticsLogger'
 import { Icon, Typography, Card, Badge } from 'antd';
+import DCIMg from '../../../img/datacredits.svg'
+import classNames from 'classnames';
 const { Text, Title } = Typography
 
 class DataCredits extends Component {
@@ -12,21 +14,91 @@ class DataCredits extends Component {
   render() {
     return (
       <DashboardLayout title="Data Credits">
-      <Card>
-        
-          <Title level={3}>
-            Purchase Data Credits from Helium to send device data.
-          </Title>
-          <br />
-          <Text>
-            The cost per fragment is $0.00001 USD (fragments are 24 bytes) which is equivalent to 1 Data Credit (DC).
-          </Text>
-          <br />
-          <Text>
-            During the Beta, the cost to send packets is 0 DC.
-          </Text>
-      
-        </Card>
+
+
+      <div className="blankstateWrapper">
+      <div className="message">
+<img src={DCIMg} />
+<h1>You have no Data Credits</h1>
+<p>Data Credits are an upcoming feature.</p>
+
+<div className="explainer">
+  <h2>What are Data Credits?</h2>
+<p>Data Credits are used by devices to send data via the Helium Network. The cost per fragment is $0.00001 USD (fragments are 24 bytes) which is equivalent to 1 Data Credit (DC). During the Beta, the cost to send packets is 0 DC.</p>
+<p>More details can be found <a href="/https://developer.helium.com/longfi/data-credits" target="_blank"> here.</a></p>
+
+</div>
+
+
+
+      </div>
+      <style jsx>{`
+
+         .message {
+            
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+            text-align: center;
+
+          }
+
+          .explainer {
+            background: #DFE8F4;
+            padding: 20px 60px;
+            border-radius: 20px;
+            border: 1px solid #CCD7E9;
+            text-align: center;
+            margin-top: 50px;
+            box-sizing: border-box;
+          }
+
+          .explainer h2 {
+            color: #242425;
+            font-size: 20px;
+          }
+          .explainer p {
+            color: #556B8C;
+            font-size: 15px;
+          }
+
+          .explainer p a {
+            color: #096DD9;
+          }
+
+          h1, p  {
+
+            color: #242425;
+          }
+          h1 {
+            font-size: 46px;
+            margin-bottom: 10px;
+          }
+          p {
+            font-size: 20px;
+            font-weight: 300;
+          }
+
+
+          .blankstateWrapper {
+            width: 100%;
+            padding-top: 150px;
+            margin: 0 auto;
+            position: relative;
+
+            
+          }
+        `}</style>
+
+      </div>
+
+
+
+
+
+     
+
+
       </DashboardLayout>
     )
   }
