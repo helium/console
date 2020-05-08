0e1ee55229b9fead452aa11bc2c349672205da1d
Peter Main <toiletbleach@gmail.com>
2020-05-08 10:37:37 +0100

    fix to a links

--- assets/js/components/Welcome.jsx
+++ assets/js/components/Welcome.jsx
@@ -144,11 +144,11 @@ class Welcome extends Component {
 
               }
 
-              a:hover {
+              .welcomewrapper a:hover {
                 text-decoration: underline;
               }
 
-              a:hover i {
+              .welcomewrapper a:hover i {
                 margin-left: 12px;
               }
 
