fc4ccefdd016017ce40c84d66dc111eb0f04ceef
Peter Main <toiletbleach@gmail.com>
2020-05-08 11:07:44 +0100

    topbar and search tidy

--- assets/js/components/common/TopBar.jsx
+++ assets/js/components/common/TopBar.jsx
@@ -33,15 +33,13 @@ class TopBar extends Component {
     const { logOut, currentOrganizationName } = this.props
 
     return (
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
         <div>
           <img src={Logo} style={{height:30}}/>
         </div>
-        <div style={{ flexGrow: 1 }}/>
         {
           currentOrganizationName && <SearchBar />
         }
-        <div style={{ width: 120 }}/>
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
           <Link to="/welcome">
             <Icon type="home" style={{ color: '#ffffff', fontSize: 18 }}/>
