b0fa285b6f8b650af1f6394b66163d6eb95bf930
David Sornberger <david@helium.com>
2020-05-08 12:55:18 -0700

    Updates to use EnvironmentPlugin

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const webpack = require('webpack')
 
 module.exports = function(env) {
   const production = process.env.NODE_ENV === 'production';
+  console.error('AUTH_0_DOMAIN', process.env.AUTH_0_DOMAIN);
+  console.error('AUTH_0_DOMAIN', env);
   return {
     devtool: production ? 'source-maps' : 'eval',
     entry: './js/app.js',
@@ -40,5 +43,8 @@ module.exports = function(env) {
       modules: ['node_modules', path.resolve(__dirname, 'js')],
       extensions: ['.js', '.jsx'],
     },
+    plugins: [
+      new webpack.EnvironmentPlugin(['AUTH_0_DOMAIN', 'AUTH_0_CLIENT_ID'])
+    ],
   };
 };
