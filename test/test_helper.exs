27f2e4cee15f112cc184c319410e9d5a70e86fcf
David Sornberger <david@helium.com>
2020-05-12 17:18:01 -0700

    Updates to fix broken tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,7 +3,7 @@ defmodule Console.AccessTokenDecoder.MockDecodeAccessToken do
   @behaviour ConsoleWeb.AccessTokenDecoder
   def decode_conn_access_token(token) do
     user_id_and_email = token |> String.split(" ")
-    %{user_id: List.first(user_id_and_email), email: List.last(user_id_and_email)}
+    %{user_id: List.first(user_id_and_email), email: List.last(user_id_and_email), auth0_id: "example"}
   end
 end
 defmock(Console.DecodeAccessMock, for: ConsoleWeb.AccessTokenDecoder)
