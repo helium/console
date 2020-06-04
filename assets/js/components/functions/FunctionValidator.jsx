bf62a6567890bf233b04a2a5e7ac0b4bf772dcd1
David Sornberger <david876534@gmail.com>
2020-06-04 10:04:52 -0700

    updates to include the check for zeroness (#219)

--- assets/js/components/functions/FunctionValidator.jsx
+++ assets/js/components/functions/FunctionValidator.jsx
@@ -32,7 +32,19 @@ class FunctionValidator extends Component {
   runValidator = () => {
     const frame = document.getElementById('myFrame')
     const { port, input } = this.state
-    const code = this.props.body + `\nlet parsedInput = "${input}".trim().split(" ").join(""); if (parsedInput.length == 0) { "Payload input must not be blank" } else { if (parsedInput.length % 2 == 1) parsedInput = "0" + parsedInput; parsedInput = parsedInput.match(/.{1,2}/g).map(x => parseInt(x, 16)); if (parsedInput.filter(x => !x).length > 0) { "Payload input could not be validly parsed" } else { JSON.stringify(Decoder(parsedInput, ${port}), null, 4) } }`
+    const code = this.props.body + `
+      \nlet parsedInput = "${input}".trim().split(" ").join("");
+      if (parsedInput.length == 0) {
+        "Payload input must not be blank"
+      } else {
+        if (parsedInput.length % 2 == 1) parsedInput = "0" + parsedInput;
+        parsedInput = parsedInput.match(/.{1,2}/g).map(x => parseInt(x, 16));
+        if (parsedInput.filter(x => x !== 0 && !x).length > 0) {
+          "Payload input could not be validly parsed"
+        } else {
+          JSON.stringify(Decoder(parsedInput, ${port}), null, 4)
+        }
+      }`
     frame.contentWindow.postMessage(code, '*')
   }
 
