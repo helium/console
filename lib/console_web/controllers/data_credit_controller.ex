a2a7f8531defe4ba5ae760f419cedb92c2d69a97
victor <victor@helium.com>
2020-07-10 11:04:53 -0700

    TEMP - set min charge to

--- lib/console_web/controllers/data_credit_controller.ex
+++ lib/console_web/controllers/data_credit_controller.ex
@@ -17,7 +17,7 @@ defmodule ConsoleWeb.DataCreditController do
 
   def create_customer_id_and_charge(conn, %{ "amountUSD" => amountUSD }) do
     { amount, _ } = Float.parse(amountUSD)
-    if amount < 10 do
+    if amount < 5 do
       {:error, :bad_request, "Credit card charges cannot be less than $10"}
     else
       current_organization = conn.assigns.current_organization
@@ -53,7 +53,7 @@ defmodule ConsoleWeb.DataCreditController do
   def create_charge(conn, %{ "amountUSD" => amountUSD }) do
     { amount, _ } = Float.parse(amountUSD)
 
-    if amount < 10 do
+    if amount < 5 do
       {:error, :bad_request, "Credit card charges cannot be less than $10"}
     else
       current_organization = conn.assigns.current_organization
@@ -177,7 +177,7 @@ defmodule ConsoleWeb.DataCreditController do
   def set_automatic_payments(conn, %{ "chargeAmount" => charge_amount, "paymentMethod" => payment_method, "chargeOption" => charge_option }) do
     { amount, _ } = Float.parse(charge_amount)
 
-    if amount < 10 and charge_option != "none" do
+    if amount < 5 and charge_option != "none" do
       {:error, :bad_request, "Credit card charges cannot be less than $10"}
     else
       current_organization = conn.assigns.current_organization
