aae04f20ce6906c9bae6f0108a5126ccd4b4c8a3
victor <victor@helium.com>
2020-07-23 10:45:24 -0700

    Add broadcast device to router on delete

--- lib/console_web/controllers/device_controller.ex
+++ lib/console_web/controllers/device_controller.ex
@@ -55,6 +55,7 @@ defmodule ConsoleWeb.DeviceController do
 
     with {:ok, %Device{} = device} <- Devices.delete_device(device) do
       broadcast(device)
+      broadcast_router_update_devices(device)
 
       conn
       |> put_resp_header("message", "#{device.name} deleted successfully")
