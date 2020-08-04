5c6e15c49f163f1b974c8eef16eec73f09253ee1
David Sornberger <david@helium.com>
2020-08-04 11:17:47 -0700

    Adds popover to express csv format

--- assets/js/components/devices/import/ChooseImportType.jsx
+++ assets/js/components/devices/import/ChooseImportType.jsx
@@ -1,42 +1,57 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
-import { Button, Typography } from 'antd';
+import { Button, Typography, Popover } from 'antd';
 import { blueForDeviceStatsLarge, dragAndDropBackgroundColor } from '../../../util/colors';
 import { scanGenericDevices } from '../../../actions/device';
 import DragAndDrop from '../../common/DragAndDrop';
 
 const { Text, Title } = Typography
 
-const ChooseImportType = ({onImportSelect, scanGenericDevices}) => (
-  <Fragment>
-    <Title style={{fontSize: 40}}>Import Devices</Title>
-    <Text style={{width: '100%', textAlign: 'center', margin: '0px 40px 20px'}}>
-      You can import your devices directly from the Things Network, or in bulk via .csv upload.
-    </Text>
-    <Link style={{width: '100%', textAlign: 'center', marginBottom: 40}} to="/">How do I format my .csv?</Link>
-    <Button
-      type="primary"
-      onClick={() => onImportSelect('ttn')}
-      style={{ width: '100%', marginLeft: 60, marginRight: 60 }}
-    >
-      Import from The Things Network
-    </Button>
-    <DragAndDrop fileSelected={(file) => {
-      let fileReader = new FileReader();
-      fileReader.onloadend = () => {
-        scanGenericDevices(fileReader.result, file.name);
-      }
-      fileReader.readAsText(file);
-      onImportSelect('csv');
-    }}>
-      <Text style={{textAlign: 'center', margin: '30px 80px', color: blueForDeviceStatsLarge}}>
-        Drag .csv file here or click to choose file
+const ChooseImportType = ({onImportSelect, scanGenericDevices}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <Fragment>
+      <Title style={{fontSize: 40}}>Import Devices</Title>
+      <Text style={{width: '100%', textAlign: 'center', margin: '0px 40px 20px'}}>
+        You can import your devices directly from the Things Network, or in bulk via .csv upload.
       </Text>
-    </DragAndDrop>
-  </Fragment>
-)
+      <Popover
+        onVisibleChange={() => setOpen(!open)}
+        visible={open}
+        trigger="click"
+        content={
+          <Text style={{display: 'table', whiteSpace: 'normal', width: 200}}>
+            The fields <b>DevEUI, AppEUI, AppKey,</b> and <b>Name</b> are all required. Optionally you can include a <b>LabelID</b> column to add labels to imported devices.
+          </Text>
+        }
+      >
+        <Text style={{width: '100%', textAlign: 'center', marginBottom: 40, color: 'deepskyblue', textDecoration: 'underline'}}>How do I format my .csv?</Text>
+      </Popover>
+      
+      <Button
+        type="primary"
+        onClick={() => onImportSelect('ttn')}
+        style={{ width: '100%', marginLeft: 60, marginRight: 60 }}
+      >
+        Import from The Things Network
+      </Button>
+      <DragAndDrop fileSelected={(file) => {
+        let fileReader = new FileReader();
+        fileReader.onloadend = () => {
+          scanGenericDevices(fileReader.result, file.name);
+        }
+        fileReader.readAsText(file);
+        onImportSelect('csv');
+      }}>
+        <Text style={{textAlign: 'center', margin: '30px 80px', color: blueForDeviceStatsLarge}}>
+          Drag .csv file here or click to choose file
+        </Text>
+      </DragAndDrop>
+    </Fragment>
+  )
+}
 
 function mapStateToProps(state, ownProps) {
   return {}
