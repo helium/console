7ede25c0ffa6ed3188ef2742f60a69160ff63dc4
David Sornberger <david@helium.com>
2020-08-10 12:02:26 -0700

    Adds base of Inputs

--- assets/js/components/common/Downlink.jsx
+++ assets/js/components/common/Downlink.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Card, Typography } from 'antd';
+import { Card, Typography, Input, InputNumber } from 'antd';
 
 const { Title } = Typography;
 
 const Downlink = (props) => {
   return (
     <Card style={{marginRight: 15, marginLeft: 15}}>
-      <Title style={{fontSize: 22}}>Create Downlink Payload</Title>
+      <Title style={{fontSize: 26}}>Create Downlink Payload</Title>
+      <InputNumber/>
+      <Input/>
     </Card>
   )
 }
