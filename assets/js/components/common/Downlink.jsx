6dad64b9a3dc184f3df76aaf223dc73be4e1eca8
David Sornberger <david@helium.com>
2020-08-10 17:14:03 -0700

    Updates to style basic

--- assets/js/components/common/Downlink.jsx
+++ assets/js/components/common/Downlink.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import { Card, Typography, Input, InputNumber } from 'antd';
+import React, { useState } from 'react';
+import { Card, Typography, Input, InputNumber, Row, Col } from 'antd';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 const Downlink = (props) => {
+  const [port, setPort] = useState(1);
   return (
     <Card style={{marginRight: 15, marginLeft: 15}}>
-      <Title style={{fontSize: 26}}>Create Downlink Payload</Title>
-      <InputNumber/>
-      <Input/>
+      <Title style={{fontSize: 26, width: '100%'}}>Create Downlink Payload</Title>
+      <Row>
+        <Col sm={24}>
+          <Text style={{width: '100%'}}>Payload</Text>
+          <Input/>
+        </Col>
+      </Row>
+      
+      <Row>
+        <Col sm={8}>
+          <Text style={{width: '100%', textAlign: 'center'}}>Fport</Text>
+          <InputNumber defaultValue={0} onChange={setPort}/>
+        </Col>
+      </Row>
     </Card>
   )
 }
