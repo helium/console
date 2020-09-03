368914b2b7d34b7b39cb8cde0560a776d6d5d35f
David Sornberger <david@helium.com>
2020-09-03 12:56:35 -0700

    Only DC usage on up

--- lib/console_web/controllers/router/device_controller.ex
+++ lib/console_web/controllers/router/device_controller.ex
@@ -116,16 +116,23 @@ defmodule ConsoleWeb.Router.DeviceController do
               end
 
             Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, event_added: "devices/#{device_id}/event")
-            bytes_per_dc = 24
-            dc_for_packet = Float.ceil(event.payload_size / bytes_per_dc)
+
+            dc_usage = if event.category == "up" do
+              bytes_per_dc = 24
+              dc_for_packet = Float.ceil(event.payload_size / bytes_per_dc)
               |> Kernel.trunc()
+              device.dc_usage + dc_for_packet
+            else
+              device.dc_usage
+            end
+
 
             Devices.update_device(device, %{
               "last_connected" => event.reported_at_naive,
               "frame_up" => event.frame_up,
               "frame_down" => event.frame_down,
               "total_packets" => device.total_packets + 1,
-              "dc_usage" => device.dc_usage + dc_for_packet,
+              "dc_usage" => dc_usage,
             }, "router")
 
             label_ids = Labels.get_labels_of_device(device) |> Enum.map(fn dl -> dl.label_id end)
