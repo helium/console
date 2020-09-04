0ba9f1aec173fb9a7aea7b988cca9bfc9cf6460a
victor <victor@helium.com>
2020-09-04 13:39:57 -0700

    Add appsignal tracking back

--- lib/console_web/endpoint.ex
+++ lib/console_web/endpoint.ex
@@ -1,6 +1,7 @@
 defmodule ConsoleWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :console
   use Absinthe.Phoenix.Endpoint
+  use Appsignal.Phoenix
 
   socket "/socket", ConsoleWeb.UserSocket,
     websocket: [timeout: 45_000]
@@ -43,6 +44,8 @@ defmodule ConsoleWeb.Endpoint do
     signing_salt: "ECVJikn/"
 
   plug ConsoleWeb.Router
+
+  plug ConsoleWeb.Plug.AppsignalAbsinthePlug
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
