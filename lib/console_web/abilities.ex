6f0ab5adaf22c6e0e58771c08f653ed3e7e317ac
victor <victor@helium.com>
2020-10-15 09:41:40 -0700

    Fix debug read only issue

--- lib/console_web/abilities.ex
+++ lib/console_web/abilities.ex
@@ -5,15 +5,12 @@ defmodule ConsoleWeb.Abilities do
     true
   end
 
-  def can?(%Membership{role: "manager"}, action, controller) do
-    # cond do
-    #   controller == ConsoleWeb.DeviceController and action in [:create, :update, :delete] -> false
-    #   controller == ConsoleWeb.InvitationController and action in [:create, :delete] -> false
-    #   controller == ConsoleWeb.MembershipController and action in [:update, :delete] -> false
-    #   controller == ConsoleWeb.OrganizationController and action in [:delete_organization] -> false
-    #   true -> true
-    # end
-    false
+  def can?(%Membership{role: "read"}, action, controller) do
+    cond do
+      controller == ConsoleWeb.DeviceController and action in [:debug] -> true
+      controller == ConsoleWeb.LabelController and action in [:debug] -> true
+      true -> false
+    end
   end
 
   def can?(_membership, _action, _controller), do: false
