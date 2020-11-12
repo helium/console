926a9ac10cc5515891ee8c323906995057bfdf00
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2020-11-11 20:39:47 -0800

    improve org switching ux (#286)
    * add labels and ability to add new org from the org dropdown

--- assets/js/components/organizations/OrganizationMenu.jsx
+++ assets/js/components/organizations/OrganizationMenu.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Icon, Menu } from 'antd';
 
 export const OrganizationMenu = props => {
-  const { orgs, handleClick, ...rest } = props;
+  const { current, orgs, handleClick, ...rest } = props;
+
   return (
     <Menu {...rest} onClick={handleClick}>
-      {orgs.map(org => (
-        <Menu.Item key={org.id}>{org.name}</Menu.Item>
-      ))}
+      <Menu.ItemGroup title="Current Organization">
+        <Menu.Item disabled style={{ color: "#1b1f23"}}>{current}</Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.Divider />
+      <Menu.ItemGroup title="Switch Organization">
+        {orgs.map(org => (
+          <Menu.Item key={org.id}>{org.name}</Menu.Item>
+        ))}
+      </Menu.ItemGroup>
+      <Menu.Divider />
+      <Menu.Item key={'new'}><Icon type="plus" /> New Organization</Menu.Item>
     </Menu>
   );
 };
