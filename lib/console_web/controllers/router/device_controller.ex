d171ce9893aa0630318cbe26c7f17340d88a23ba
victor <victor@helium.com>
2020-11-17 12:51:00 -0800

    Update router add device event for consistency of DC used metric

--- lib/console_web/controllers/router/device_controller.ex
+++ lib/console_web/controllers/router/device_controller.ex
@@ -80,7 +80,7 @@ defmodule ConsoleWeb.Router.DeviceController do
   end
 
   def add_device_event(conn, %{"device_id" => device_id} = event) do
-    payload = event["payload"]
+    # for passing to debug panel, not stored in db
     channels_with_debug =
       event["channels"]
       |> Enum.map(fn c ->
@@ -93,21 +93,28 @@ defmodule ConsoleWeb.Router.DeviceController do
         c |> Map.new(fn {k, v} -> {String.to_atom(k), v} end)
       end)
 
+    # for storing event in db, no debug info attached
     channels_without_debug =
       event["channels"]
       |> Enum.map(fn c ->
         Map.drop(c, ["debug"])
       end)
 
-    event = Map.put(event, "channels", channels_without_debug)
+    payload = event["payload"]
 
-    event = Map.put(event, "reported_at_epoch", event["reported_at"])
-
+    event = event
+      |> Map.put("channels", channels_without_debug)
+      |> Map.put("reported_at_epoch", event["reported_at"])
     event =
       cond do
         is_integer(event["port"]) -> event
         event["port"] != nil and Integer.parse(event["port"]) != :error -> event
         true -> Map.put(event, "port", nil)
+      end
+    event =
+      case event["dc"]["used"] do
+        nil -> Map.put(event, "dc_used", 0)
+        dc -> Map.put(event, "dc_used", dc)
       end
 
     case Devices.get_device(device_id) do
@@ -115,130 +122,145 @@ defmodule ConsoleWeb.Router.DeviceController do
         conn
         |> send_resp(404, "")
       %Device{} = device ->
-        case Events.create_event(event) do
-          {:ok, event} ->
-            event =
-              case payload do
-                nil ->
-                  Map.merge(event, %{
-                    device_name: device.name,
-                    hotspots: Jason.encode!(event.hotspots),
-                    channels: Jason.encode!(event.channels)
-                  })
-                _ ->
-                  Map.merge(event, %{
-                    device_name: device.name,
-                    payload: payload,
-                    hotspots: Jason.encode!(event.hotspots),
-                    channels: Jason.encode!(channels_with_debug)
-                  })
-              end
+        organization = Organizations.get_organization!(device.organization_id)
+        prev_dc_balance = organization.dc_balance
 
-            Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, event_added: "devices/#{device_id}/event")
-            Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, device_debug_event_added: "devices/#{device_id}/event/debug")
-
-            dc_usage = if event.category == "up" do
-              bytes_per_dc = 24
-              dc_for_packet = Float.ceil(event.payload_size / bytes_per_dc)
-              |> Kernel.trunc()
-              device.dc_usage + dc_for_packet
-            else
-              device.dc_usage
-            end
-
+        result =
+          Ecto.Multi.new()
+          |> Ecto.Multi.run(:event, fn _repo, _ ->
+            Events.create_event(event)
+          end)
+          |> Ecto.Multi.run(:device, fn _repo, %{ event: event } ->
             Devices.update_device(device, %{
               "last_connected" => event.reported_at_naive,
               "frame_up" => event.frame_up,
               "frame_down" => event.frame_down,
               "total_packets" => device.total_packets + 1,
-              "dc_usage" => dc_usage,
+              "dc_usage" => device.dc_usage + event.dc_used,
             }, "router")
-
-            label_ids = Labels.get_labels_of_device(device) |> Enum.map(fn dl -> dl.label_id end)
-            Enum.each(label_ids, fn id ->
-              Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, label_debug_event_added: "labels/#{id}/event/debug")
-            end)
-        end
-
-        organization = Organizations.get_organization(device.organization_id)
-        prev_dc_balance = organization.dc_balance
-        if organization.dc_balance_nonce == event["dc"]["nonce"] do
-          {:ok, organization} = Organizations.update_organization(organization, %{ "dc_balance" => event["dc"]["balance"] })
-
-          if organization.automatic_charge_amount == nil do
+          end)
+          |> Ecto.Multi.run(:organization, fn _repo, %{ device: device, event: created_event } ->
             cond do
-              prev_dc_balance > 500_000 and organization.dc_balance <= 500_000 ->
-                # DC Balance has dipped below 500,000. Send a notice.
-                Organizations.get_administrators(organization)
-                |> Enum.each(fn administrator ->
-                  Email.dc_balance_notification_email(organization, administrator.email, organization.dc_balance)
-                  |> Mailer.deliver_later()
-                end)
-              prev_dc_balance > 0 and organization.dc_balance <= 0 ->
-                # DC Balance has gone to zero. Send a notice.
-                Organizations.get_administrators(organization)
-                |> Enum.each(fn administrator ->
-                  Email.dc_balance_notification_email(organization, administrator.email, 0) |> Mailer.deliver_later()
-                end)
-              true -> nil
+              organization.dc_balance_nonce == event["dc"]["nonce"] ->
+                Organizations.update_organization(organization, %{ "dc_balance" => event["dc"]["balance"] })
+              organization.dc_balance_nonce - 1 == event["dc"]["nonce"] ->
+                {:ok, updated_org} = Organizations.update_organization(organization, %{ "dc_balance" => organization.dc_balance - created_event.dc_used })
+                ConsoleWeb.DataCreditController.broadcast_router_refill_dc_balance(updated_org)
+
+                {:ok, updated_org}
+              true ->
+                {:error, "DC balance nonce inconsistent between router and console"}
+            end
+          end)
+          |> Repo.transaction()
+
+        with {:ok, %{ event: event, device: device, organization: organization }} <- result do
+          publish_created_event(event, payload, device, channels_with_debug)
+          check_org_dc_balance(organization, prev_dc_balance)
+
+          conn
+          |> send_resp(200, "")
+        end
+    end
+  end
+
+  defp publish_created_event(event, payload, device, channels_with_debug) do
+    event =
+      case payload do
+        nil ->
+          Map.merge(event, %{
+            device_name: device.name,
+            hotspots: Jason.encode!(event.hotspots),
+            channels: Jason.encode!(event.channels)
+          })
+        _ ->
+          Map.merge(event, %{
+            device_name: device.name,
+            payload: payload,
+            hotspots: Jason.encode!(event.hotspots),
+            channels: Jason.encode!(channels_with_debug)
+          })
+      end
+
+    Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, event_added: "devices/#{device.id}/event")
+    Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, device_debug_event_added: "devices/#{device.id}/event/debug")
+
+    label_ids = Labels.get_labels_of_device(device) |> Enum.map(fn dl -> dl.label_id end)
+    Enum.each(label_ids, fn id ->
+      Absinthe.Subscription.publish(ConsoleWeb.Endpoint, event, label_debug_event_added: "labels/#{id}/event/debug")
+    end)
+  end
+
+  defp check_org_dc_balance(organization, prev_dc_balance) do
+    if organization.automatic_charge_amount == nil do
+      cond do
+        prev_dc_balance > 500_000 and organization.dc_balance <= 500_000 ->
+          # DC Balance has dipped below 500,000. Send a notice.
+          Organizations.get_administrators(organization)
+          |> Enum.each(fn administrator ->
+            Email.dc_balance_notification_email(organization, administrator.email, organization.dc_balance)
+            |> Mailer.deliver_later()
+          end)
+        prev_dc_balance > 0 and organization.dc_balance <= 0 ->
+          # DC Balance has gone to zero. Send a notice.
+          Organizations.get_administrators(organization)
+          |> Enum.each(fn administrator ->
+            Email.dc_balance_notification_email(organization, administrator.email, 0) |> Mailer.deliver_later()
+          end)
+        true -> nil
+      end
+    end
+
+    if organization.automatic_charge_amount != nil
+      and organization.automatic_payment_method != nil
+      and organization.dc_balance < 500000
+      and not organization.pending_automatic_purchase do
+
+        organization = Organizations.get_organization_and_lock_for_dc(organization.id)
+        {:ok, organization} = Organizations.update_organization(organization, %{ "pending_automatic_purchase" => true })
+
+        request_body = URI.encode_query(%{
+          "customer" => organization.stripe_customer_id,
+          "amount" => organization.automatic_charge_amount,
+          "currency" => "usd",
+          "payment_method" => organization.automatic_payment_method,
+          "off_session" => "true",
+          "confirm" => "true",
+        })
+
+        with {:ok, stripe_response} <- HTTPoison.post("#{@stripe_api_url}/v1/payment_intents", request_body, @headers) do
+          with 200 <- stripe_response.status_code do
+            payment_intent = Poison.decode!(stripe_response.body)
+
+            with "succeeded" <- payment_intent["status"],
+              {:ok, stripe_response} <- HTTPoison.get("#{@stripe_api_url}/v1/payment_methods/#{payment_intent["payment_method"]}", @headers),
+              200 <- stripe_response.status_code do
+                card = Poison.decode!(stripe_response.body)
+
+                attrs = %{
+                  "dc_purchased" => payment_intent["amount"] * 1000,
+                  "cost" => payment_intent["amount"],
+                  "card_type" => card["card"]["brand"],
+                  "last_4" => card["card"]["last4"],
+                  "user_id" => "Recurring Charge",
+                  "organization_id" => organization.id,
+                  "payment_id" => payment_intent["id"],
+                }
+
+                with {:ok, %DcPurchase{} = dc_purchase } <- DcPurchases.create_dc_purchase_update_org(attrs, organization) do
+                  organization = Organizations.get_organization!(organization.id)
+                  Organizations.get_administrators(organization)
+                  |> Enum.each(fn administrator ->
+                    Email.dc_top_up_notification_email(organization, dc_purchase, administrator.email)
+                    |> Mailer.deliver_later()
+                  end)
+                  ConsoleWeb.DataCreditController.broadcast(organization, dc_purchase)
+                  ConsoleWeb.DataCreditController.broadcast(organization)
+                  ConsoleWeb.DataCreditController.broadcast_router_refill_dc_balance(organization)
+                end
             end
           end
-
-          if organization.automatic_charge_amount != nil
-            and organization.automatic_payment_method != nil
-            and organization.dc_balance < 500000
-            and not organization.pending_automatic_purchase do
-
-              organization = Organizations.get_organization_and_lock_for_dc(organization.id)
-              {:ok, organization} = Organizations.update_organization(organization, %{ "pending_automatic_purchase" => true })
-
-              request_body = URI.encode_query(%{
-                "customer" => organization.stripe_customer_id,
-                "amount" => organization.automatic_charge_amount,
-                "currency" => "usd",
-                "payment_method" => organization.automatic_payment_method,
-                "off_session" => "true",
-                "confirm" => "true",
-              })
-
-              with {:ok, stripe_response} <- HTTPoison.post("#{@stripe_api_url}/v1/payment_intents", request_body, @headers) do
-                with 200 <- stripe_response.status_code do
-                  payment_intent = Poison.decode!(stripe_response.body)
-
-                  with "succeeded" <- payment_intent["status"],
-                    {:ok, stripe_response} <- HTTPoison.get("#{@stripe_api_url}/v1/payment_methods/#{payment_intent["payment_method"]}", @headers),
-                    200 <- stripe_response.status_code do
-                      card = Poison.decode!(stripe_response.body)
-
-                      attrs = %{
-                        "dc_purchased" => payment_intent["amount"] * 1000,
-                        "cost" => payment_intent["amount"],
-                        "card_type" => card["card"]["brand"],
-                        "last_4" => card["card"]["last4"],
-                        "user_id" => "Recurring Charge",
-                        "organization_id" => organization.id,
-                        "payment_id" => payment_intent["id"],
-                      }
-
-                      with {:ok, %DcPurchase{} = dc_purchase } <- DcPurchases.create_dc_purchase_update_org(attrs, organization) do
-                        organization = Organizations.get_organization!(organization.id)
-                        Organizations.get_administrators(organization)
-                        |> Enum.each(fn administrator ->
-                          Email.dc_top_up_notification_email(organization, dc_purchase, administrator.email)
-                          |> Mailer.deliver_later()
-                        end)
-                        ConsoleWeb.DataCreditController.broadcast(organization, dc_purchase)
-                        ConsoleWeb.DataCreditController.broadcast(organization)
-                        ConsoleWeb.DataCreditController.broadcast_router_refill_dc_balance(organization)
-                      end
-                  end
-                end
-              end
-          end
         end
-
-        conn
-        |> send_resp(200, "")
     end
   end
 end
