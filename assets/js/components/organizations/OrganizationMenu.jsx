f027f55b9b910ad28dd4e17fae27664fffb26c30
Alejandra Buznego <abuznego23@gmail.com>
2020-11-30 10:05:46 -0800

    check for role in organization menu

--- assets/js/components/organizations/OrganizationMenu.jsx
+++ assets/js/components/organizations/OrganizationMenu.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Icon, Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export const OrganizationMenu = props => {
-  const { current, orgs, handleClick, ...rest } = props;
+@connect(mapStateToProps, null)
+class OrganizationMenu extends Component {
+  render() {
+    const { current, orgs, handleClick, role, ...rest } = this.props;
+    return (
+      <Menu {...rest} onClick={handleClick}>
+        <Menu.ItemGroup title="Current Organization">
+          <Menu.Item key='current'><Link to="/organizations">{current}</Link></Menu.Item>
+        </Menu.ItemGroup>
+        {orgs.length > 0 && <Menu.Divider /> && 
+        <Menu.ItemGroup title="Switch Organization">
+          {orgs.map(org => (
+            <Menu.Item key={org.id}>{org.name}</Menu.Item>
+          ))}
+        </Menu.ItemGroup>} 
+        {role === 'admin' && <Menu.Divider />}
+        {role === 'admin' && <Menu.Item key='new'><Icon type="plus" /> New Organization</Menu.Item>}
+      </Menu>
+    );
+  }
+}
 
-  return (
-    <Menu {...rest} onClick={handleClick}>
-      <Menu.ItemGroup title="Current Organization">
-        <Menu.Item key='current'><Link to="/organizations">{current}</Link></Menu.Item>
-      </Menu.ItemGroup>
-      {orgs.length > 0 && <Menu.Divider /> && 
-      <Menu.ItemGroup title="Switch Organization">
-        {orgs.map(org => (
-          <Menu.Item key={org.id}>{org.name}</Menu.Item>
-        ))}
-      </Menu.ItemGroup>} 
-      <Menu.Divider />
-      <Menu.Item key='new'><Icon type="plus" /> New Organization</Menu.Item>
-    </Menu>
-  );
-};
+function mapStateToProps(state) {
+  return {
+    role: state.organization.currentRole
+  }
+}
+
+export default OrganizationMenu
