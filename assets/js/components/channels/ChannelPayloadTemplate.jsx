12628ab58930a74950d1bd1cc44cd31dace0dae3
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2020-12-02 14:50:30 -0800

    fix syntax highlighting for custom script editor (#312)
    * add theme to use for syntax highlighting
    
    * consistent theme for integration payload too

--- assets/js/components/channels/ChannelPayloadTemplate.jsx
+++ assets/js/components/channels/ChannelPayloadTemplate.jsx
@@ -7,6 +7,7 @@ import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism-coy.css';
 
 class ChannelPayloadTemplate extends Component {
   onClickEditor = () => {
