c23591ced0b0b9ab90901dbb4c5b56a898edaf7b
victor <victor@helium.com>
2021-01-15 13:46:49 -0800

    Update visuals

--- assets/js/components/flows/FlowsWorkspace.jsx
+++ assets/js/components/flows/FlowsWorkspace.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactFlow, { isNode, removeElements, addEdge } from 'react-flow-renderer';
+import findIndex from 'lodash/findIndex'
 import LabelNode from './LabelNode'
 import FunctionNode from './FunctionNode'
 import ChannelNode from './ChannelNode'
@@ -14,22 +15,41 @@ const getLayoutedElements = (elements, direction = 'TB') => {
   dagreGraph.setGraph({ rankdir: direction });
   elements.forEach((el) => {
     if (isNode(el)) {
-      dagreGraph.setNode(el.id, { width: 150, height: 50 });
+      dagreGraph.setNode(el.id, { width: 200, height: 70 });
     } else {
       dagreGraph.setEdge(el.source, el.target);
     }
   });
   dagre.layout(dagreGraph);
+
+  const edgeSet = elements.reduce((acc, el) => {
+    if (!isNode(el)) return Object.assign({}, acc, { [el.source]: true, [el.target]: true })
+    return acc
+  }, {})
+
+  const unconnectedNodes = elements.filter(el => isNode(el) && !edgeSet[el.id])
+
   return elements.map((el) => {
     if (isNode(el)) {
-      const nodeWithPosition = dagreGraph.node(el.id);
-      el.targetPosition = isHorizontal ? 'left' : 'top';
-      el.sourcePosition = isHorizontal ? 'right' : 'bottom';
-      // unfortunately we need this little hack to pass a slightly different position in order to notify react flow about the change
-      el.position = {
-        x: nodeWithPosition.x + Math.random() / 1000,
-        y: nodeWithPosition.y,
-      };
+      if (!edgeSet[el.id]) {
+        const index = findIndex(unconnectedNodes, {id: el.id})
+        console.log(el)
+        el.position = {
+          x: 1000 + Math.random() / 1000,
+          y: 40 + index * 100,
+        };
+      } else {
+        const nodeWithPosition = dagreGraph.node(el.id);
+        el.targetPosition = isHorizontal ? 'left' : 'top';
+        el.sourcePosition = isHorizontal ? 'right' : 'bottom';
+        // unfortunately we need this little hack to pass a slightly different position in order to notify react flow about the change
+        let x = nodeWithPosition.x + Math.random() / 1000
+        if (el.type == 'channelNode') x += 250
+        el.position = {
+          x: x - 50,
+          y: nodeWithPosition.y,
+        };
+      }
     }
     return el;
   });
