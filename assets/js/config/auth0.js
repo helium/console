0ccc6895a0fb5c369ae847b8e40ef3f2ce73ce44
victor <victor@helium.com>
2021-01-25 15:32:47 -0800

    Update for open source runtime auth0 env vars

--- assets/js/config/auth0.js
+++ assets/js/config/auth0.js
@@ -1,4 +1,4 @@
 export const config = {
-  domain: process.env.AUTH_0_DOMAIN || 'dev-j03rcvdv.auth0.com',
-  clientId: process.env.AUTH_0_CLIENT_ID || 'dAS0iCnOsSBTsyanPPcYEmDVitsRcQ8c'
+  domain: window.auth0_domain || process.env.AUTH_0_DOMAIN || 'dev-j03rcvdv.auth0.com',
+  clientId: window.auth0_client_id || process.env.AUTH_0_CLIENT_ID || 'dAS0iCnOsSBTsyanPPcYEmDVitsRcQ8c'
 }
