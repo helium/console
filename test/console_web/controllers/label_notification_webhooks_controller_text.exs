258b6ba98a9c4a02b74520657a81d640613b46aa
Alejandra Buznego <abuznego23@gmail.com>
2021-01-26 15:51:22 -0800

    add tests

--- test/console_web/controllers/label_notification_webhooks_controller_text.exs
+++
@@ -1,8 +0,0 @@
-defmodule ConsoleWeb.LabelNotificationWebhooksControllerTest do
-  use ConsoleWeb.ConnCase
-
-  import Console.FactoryHelper
-  import Console.Factory
-
-  alias Console.LabelNotificationWebhooks
-end
