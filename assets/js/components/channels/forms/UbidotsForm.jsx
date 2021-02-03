9b5cfa1fd7a929213648c28cf8c7b7b7d74ada42
victor <victor@helium.com>
2021-02-03 09:42:51 -0800

    Change ubidots url

--- assets/js/components/channels/forms/UbidotsForm.jsx
+++ assets/js/components/channels/forms/UbidotsForm.jsx
@@ -5,12 +5,7 @@ const { Option } = Select
 
 class UbidotsForm extends Component {
   state = {
-    endpoint: "",
     authToken: "",
-  }
-
-  handleSelectEndpoint = endpoint => {
-    this.setState({ endpoint }, this.validateInput)
   }
 
   handleTokenUpdate = e => {
@@ -18,12 +13,12 @@ class UbidotsForm extends Component {
   }
 
   validateInput = () => {
-    const { endpoint, authToken } = this.state
-    if (endpoint.length > 0 && authToken.length > 0) {
+    const { authToken } = this.state
+    if (authToken.length > 0) {
       this.props.onValidInput({
         method: "post",
-        endpoint,
-        headers: { 'X-Auth-Token': authToken },
+        endpoint: "https://industrial.api.ubidots.com/api/v2.0/plugin_types/~helium/plugins/",
+        headers: { 'X-Auth-Token': authToken, 'Content-Type': 'application/json' },
       })
     }
   }
@@ -31,30 +26,6 @@ class UbidotsForm extends Component {
   render() {
     return(
       <div>
-        <div>
-          <Text>Select Endpoint:</Text>
-        </div>
-        <div style={{ marginBottom: 20 }}>
-          <Select
-            value={this.state.endpoint}
-            onChange={this.handleSelectEndpoint}
-            style={{ width: '50%' }}
-            size="large"
-          >
-            <Option value="http://things.ubidots.com">
-              http://things.ubidots.com
-            </Option>
-            <Option value="https://things.ubidots.com">
-              https://things.ubidots.com
-            </Option>
-            <Option value="http://industrial.api.ubidots.com">
-              http://industrial.api.ubidots.com
-            </Option>
-            <Option value="https://industrial.api.ubidots.com">
-              https://industrial.api.ubidots.com
-            </Option>
-          </Select>
-        </div>
         <div>
           <Text style={{ display: 'block' }}>Enter Auth Token:</Text>
         </div>
