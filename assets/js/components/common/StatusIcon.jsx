356e6ca396364feb917f442150220f8905e12197
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2021-02-04 11:26:57 -0800

    Update StatusIcon.jsx

--- assets/js/components/common/StatusIcon.jsx
+++ assets/js/components/common/StatusIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Icon from '@ant-design/icons';
 
 const StatusSvg = () => (
-  <svg height="10" width="10">
+  <svg height="11" width="10">
     <circle cx="5" cy="5" r="5" fill="green" />
   </svg> 
 );
