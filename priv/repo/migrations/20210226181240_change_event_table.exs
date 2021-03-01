b2eabcbe621cc3b153dd0bf2d6a573defb764b34
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2021-03-01 14:32:15 -0800

    Event log updates: Database changes (#399)
    * migration for table
    * fixes for /events endpoint

---
+++ priv/repo/migrations/20210226181240_change_event_table.exs
@@ -0,0 +1,31 @@
+defmodule Console.Repo.Migrations.ChangeEventTable do
+  use Ecto.Migration
+
+  def up do
+    alter table(:events) do
+      remove :hotspots
+      remove :channels
+      remove :payload_size
+      remove :dc_used
+      remove :port
+      remove :devaddr
+      add :sub_category, :string
+      add :data, :map, default: %{}, null: false
+      add :router_uuid, :string
+    end
+  end
+
+  def down do
+    alter table(:events) do
+      add :hotspots, {:array, :map}
+      add :channels, {:array, :map}
+      add :payload_size, :integer
+      add :dc_used, :integer
+      add :port, :integer
+      add :devaddr, :string
+      remove :sub_category
+      remove :data
+      remove :router_uuid
+    end
+  end
+end
