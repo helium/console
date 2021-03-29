a49fef98173e9bc520b3cb7a0fea4e7ebba19ba2
Alejandra Buznego <abuznego23@gmail.com>
2021-03-29 15:14:56 -0700

    change wallet_id to transaction_id overall and address to hotspot in router broadcast

--- lib/console_web/controllers/v1/device_controller.ex
+++ lib/console_web/controllers/v1/device_controller.ex
@@ -97,7 +97,7 @@ defmodule ConsoleWeb.V1.DeviceController do
     end
   end
 
-  def discover_device(conn, params = %{ "hotspot_name" => name, "hotspot_address" => address, "wallet_id" => wallet_id, "signature" => signature }) do
+  def discover_device(conn, params = %{ "hotspot_name" => name, "hotspot_address" => address, "transaction_id" => transaction_id, "signature" => signature }) do
     current_organization = conn.assigns.current_organization
     discovery_mode_organization = Organizations.get_discovery_mode_org()
     if current_organization.id !== discovery_mode_organization.id do
@@ -108,12 +108,12 @@ defmodule ConsoleWeb.V1.DeviceController do
         with {:ok, %Device{} = device} <- Devices.create_device(%{ "name" => name, "hotspot_address" => address, "organization_id" => current_organization.id }, current_organization) do
           discovery_mode_label = Labels.get_label_by_name("Discovery Mode", discovery_mode_organization.id)
           Labels.add_devices_to_label([device.id], discovery_mode_label.id, current_organization)
-          ConsoleWeb.Endpoint.broadcast("device:all", "device:all:discover:devices", %{ "address" => address, "wallet_id" => wallet_id, "signature" => signature, "device_id" => device.id })
+          ConsoleWeb.Endpoint.broadcast("device:all", "device:all:discover:devices", %{ "hotspot" => address, "transaction_id" => transaction_id, "signature" => signature, "device_id" => device.id })
           conn
             |> send_resp(:ok, "Device created and broadcasted to router")
         end
       else
-        ConsoleWeb.Endpoint.broadcast("device:all", "device:all:discover:devices", %{ "address" => address, "wallet_id" => wallet_id, "signature" => signature, "device_id" => existing_device.id })
+        ConsoleWeb.Endpoint.broadcast("device:all", "device:all:discover:devices", %{ "hotspot" => address, "transaction_id" => transaction_id, "signature" => signature, "device_id" => existing_device.id })
         conn
           |> send_resp(:ok, "Device broadcasted to router")
       end
