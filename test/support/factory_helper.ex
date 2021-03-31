6e7e7bdf0f8df8b484936257eafe2449e43af210
victor <victor@helium.com>
2021-03-30 17:19:37 -0700

    Move factory helper function into factory

--- test/support/factory_helper.ex
+++ test/support/factory_helper.ex
@@ -1,17 +1,3 @@
 defmodule Console.FactoryHelper do
   import Plug.Conn
-  
-  import Console.Factory
-
-  alias Console.Organizations
-
-  def authenticate_user(%{conn: conn}) do
-    user = params_for(:user)
-    {:ok, organization} = Organizations.create_organization(user, params_for(:organization))
-    conn = conn
-           |> put_req_header("accept", "application/json")
-           |> put_req_header("authorization", user.id <> " " <> user.email)
-           |> put_req_header("organization", organization.id)
-    {:ok, conn: conn}
-  end
 end
