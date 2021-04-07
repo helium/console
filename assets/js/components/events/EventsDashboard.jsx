fc8859087d04650243c4ef182d0461a62cfce25c
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2021-04-06 18:11:41 -0600

    Show no integration attached when no_channel in external integrations (#501)
    * check for no_channel in external integrations only
    
    * handle when only internal integration is found
    
    * safeguard for no integrations
    
    * safeguard for integration responses too

--- assets/js/components/events/EventsDashboard.jsx
+++ assets/js/components/events/EventsDashboard.jsx
@@ -357,9 +357,14 @@ class EventsDashboard extends Component {
         title: 'Type',
         dataIndex: 'category',
         render: (data, row) => {
-          const integrationResponses = row.integrations && row.integrations.filter(i => i.subCategory === 'uplink_integration_res');
+          const integrationResponses = row.integrations ? row.integrations.filter(i => i.subCategory === 'uplink_integration_res') : [];
           const integrationError = integrationResponses.findIndex(i => i.status === 'error') === -1 ? false : true;
-          const integrationMissing = row.integrations.findIndex(i => i.id === 'no_channel') === -1 ? false : true;
+
+          const externalIntegrations = row.integrations ? row.integrations.filter(i => i.name !== 'Internal Integration') : [];
+          const missingExternalIntegration = externalIntegrations.findIndex(i => i.id === 'no_channel') !== -1;
+          const onlyInternalIntegration = row.integrations && row.integrations.length > 0 && externalIntegrations.length === 0;
+          const integrationMissing = missingExternalIntegration || onlyInternalIntegration ? true : false;
+
           return <Text>{categoryTag(row.category, row.sub_categories)} {integrationError && integrationErrorTag()}{integrationMissing && integrationMissingTag()}</Text>;
         }
       },
