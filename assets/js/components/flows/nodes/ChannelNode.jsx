1a91b818bd3fafb43fc733cdbdf0e8d3b00a4695
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2021-04-09 16:21:25 -0600

    Show selected icon for nodes (#511)
    * first pass at selected node icon
    
    * apply selected icon to rest of node components
    
    * add animation to selected node icon
    
    * infinite animation

--- assets/js/components/flows/nodes/ChannelNode.jsx
+++ assets/js/components/flows/nodes/ChannelNode.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Handle } from 'react-flow-renderer';
 import { Typography } from 'antd';
-const { Text } = Typography
-import ChannelIcon from '../../../../img/channel-node-icon.svg'
+const { Text } = Typography;
+import ChannelIcon from '../../../../img/channel-node-icon.svg';
+import SelectedNodeIcon from './SelectedNodeIcon';
 
-export default ({ data, unconnected }) => {
+export default ({ data, unconnected, selected }) => {
   return (
-    <div style={{
-      background: '#12CB9E',
-      padding: '10px 15px 6px 15px',
-      borderRadius: 5,
-      minWidth: 200,
-      minHeight: 50,
-    }}>
-      {
-        !unconnected && (
-          <Handle
-            type="target"
-            position="left"
-            style={{ height: '100%', borderRadius: 10, background: '#ffffff', border: '3.5px solid #12CB9E', height: '12px', width: '12px' }}
-          />
-        )
-      }
-      <div style={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
-        <img src={ChannelIcon} style={{ height: 16, display: 'block' }} />
-        <Text style={{ display: 'block', fontSize: 16, color: '#ffffff', fontWeight: 500 }}>{data.label}</Text>
-        <Text style={{ fontSize: 10, color: '#ffffff', position: 'relative', top: -5 }}>Integration Type: {data.type}</Text>
+    <Fragment>
+      {selected && <SelectedNodeIcon />}
+      <div style={{
+        background: '#12CB9E',
+        padding: '10px 15px 6px 15px',
+        borderRadius: 5,
+        minWidth: 200,
+        minHeight: 50,
+      }}>
+        {
+          !unconnected && (
+            <Handle
+              type="target"
+              position="left"
+              style={{ height: '100%', borderRadius: 10, background: '#ffffff', border: '3.5px solid #12CB9E', height: '12px', width: '12px' }}
+            />
+          )
+        }
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
+          <img src={ChannelIcon} style={{ height: 16, display: 'block' }} />
+          <Text style={{ display: 'block', fontSize: 16, color: '#ffffff', fontWeight: 500 }}>{data.label}</Text>
+          <Text style={{ fontSize: 10, color: '#ffffff', position: 'relative', top: -5 }}>Integration Type: {data.type}</Text>
+        </div>
       </div>
-    </div>
-  );
+    </Fragment>
+  )
 };
