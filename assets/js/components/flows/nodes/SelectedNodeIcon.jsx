c87f3a7e3c84d4f0282d98abffd0c7b989d06bd7
Alejandra Buznego <abuznego23@gmail.com>
2021-04-09 15:53:29 -0600

    add animation to selected node icon

--- assets/js/components/flows/nodes/SelectedNodeIcon.jsx
+++ assets/js/components/flows/nodes/SelectedNodeIcon.jsx
@@ -3,8 +3,21 @@ import SelectedIcon from '../../../../img/selected-node.svg';
 
 export default () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <img src={SelectedIcon} style={{ position: 'absolute', top: '-40px', height: 35 }} />
+    <div style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center' 
+    }}>
+      <img src={SelectedIcon} style={{ 
+        position: 'absolute',
+        top: '-40px',
+        height: 35,
+        opacity: 0,
+        animationName: 'bounceIn',
+        animationDuration: '400ms',
+        animationTimingFunction: 'linear',
+        animationFillMode: 'forwards'
+      }} />
     </div>
   );
 }
