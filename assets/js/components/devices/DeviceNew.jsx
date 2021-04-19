189333fd06f4adac8f2511d89e45d9e075b1e0fe
victor <victor@helium.com>
2021-04-19 14:43:24 -0700

    Change device show to 2 col

--- assets/js/components/devices/DeviceNew.jsx
+++ assets/js/components/devices/DeviceNew.jsx
@@ -7,7 +7,7 @@ import withGql from '../../graphql/withGql'
 import { ALL_LABELS } from '../../graphql/labels'
 import UserCan from '../common/UserCan'
 import analyticsLogger from '../../util/analyticsLogger'
-import { Card, Button, Typography, Input } from 'antd';
+import { Card, Button, Typography, Input, Row, Col } from 'antd';
 import { EyeOutlined, EyeInvisibleOutlined, SaveOutlined } from '@ant-design/icons';
 import LabelAppliedNew from '../common/LabelAppliedNew';
 const { Text } = Typography
@@ -56,91 +56,98 @@ class DeviceNew extends Component {
 
     return (
       <div style={{ padding: '30px 30px 20px 30px' }}>
-        <Card title="Enter Device Details">
-          <Input
-            placeholder="Device Name"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputUpdate}
-            addonBefore="Name"
-            ref={this.nameInputRef}
-            autoFocus
-          />
+        <Text style={{ fontSize: 22, fontWeight: 600 }}>All Devices</Text>
+        <Row gutter={20} style={{ marginTop: 20 }}>
+          <Col span={12}>
+            <Card title="Enter Device Details">
+              <Input
+                placeholder="Device Name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleInputUpdate}
+                addonBefore="Name"
+                ref={this.nameInputRef}
+                autoFocus
+              />
 
-          <Input
-            placeholder="Device EUI"
-            name="devEUI"
-            value={this.state.devEUI}
-            onChange={this.handleInputUpdate}
-            style={{ marginTop: 10 }}
-            maxLength={16}
-            addonBefore="Dev EUI"
-            suffix={
-              <Text type={this.state.devEUI.length !== 16 ? "danger" : ""}>{Math.floor(this.state.devEUI.length / 2)} / 8 Bytes</Text>
-            }
-          />
-
-          <Input
-            placeholder="App EUI"
-            name="appEUI"
-            value={this.state.appEUI}
-            onChange={this.handleInputUpdate}
-            style={{ marginTop: 10 }}
-            maxLength={16}
-            addonBefore="App EUI"
-            suffix={
-              <Text type={this.state.appEUI.length !== 16 ? "danger" : ""}>{Math.floor(this.state.appEUI.length / 2)} / 8 Bytes</Text>
-            }
-          />
-
-          <Input
-            placeholder="App Key"
-            name="appKey"
-            value={
-              this.state.showAppKey ? this.state.appKey : '✱'.repeat(28)
-            }
-            disabled={!this.state.showAppKey}
-            onChange={this.handleInputUpdate}
-            style={{ marginTop: 10 }}
-            maxLength={56}
-            addonBefore={
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                App Key
-                {
-                  this.state.showAppKey ? (
-                    <EyeOutlined
-                      onClick={() => this.setState({ showAppKey: !this.state.showAppKey })}
-                      style={{ marginLeft: 5 }}
-                    />
-                  ) : (
-                    <EyeInvisibleOutlined
-                      onClick={() => this.setState({ showAppKey: !this.state.showAppKey })}
-                      style={{ marginLeft: 5 }}
-                    />
-                  )
+              <Input
+                placeholder="Device EUI"
+                name="devEUI"
+                value={this.state.devEUI}
+                onChange={this.handleInputUpdate}
+                style={{ marginTop: 10 }}
+                maxLength={16}
+                addonBefore="Dev EUI"
+                suffix={
+                  <Text type={this.state.devEUI.length !== 16 ? "danger" : ""}>{Math.floor(this.state.devEUI.length / 2)} / 8 Bytes</Text>
                 }
-              </div>
-            }
-            suffix={
-              <Text type={this.state.appKey.length !== 32 ? "danger" : ""}>{Math.floor(this.state.appKey.length / 2)} / 16 Bytes</Text>
-            }
-          />
+              />
+
+              <Input
+                placeholder="App EUI"
+                name="appEUI"
+                value={this.state.appEUI}
+                onChange={this.handleInputUpdate}
+                style={{ marginTop: 10 }}
+                maxLength={16}
+                addonBefore="App EUI"
+                suffix={
+                  <Text type={this.state.appEUI.length !== 16 ? "danger" : ""}>{Math.floor(this.state.appEUI.length / 2)} / 8 Bytes</Text>
+                }
+              />
+
+              <Input
+                placeholder="App Key"
+                name="appKey"
+                value={
+                  this.state.showAppKey ? this.state.appKey : '✱'.repeat(28)
+                }
+                disabled={!this.state.showAppKey}
+                onChange={this.handleInputUpdate}
+                style={{ marginTop: 10 }}
+                maxLength={56}
+                addonBefore={
+                  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    App Key
+                    {
+                      this.state.showAppKey ? (
+                        <EyeOutlined
+                          onClick={() => this.setState({ showAppKey: !this.state.showAppKey })}
+                          style={{ marginLeft: 5 }}
+                        />
+                      ) : (
+                        <EyeInvisibleOutlined
+                          onClick={() => this.setState({ showAppKey: !this.state.showAppKey })}
+                          style={{ marginLeft: 5 }}
+                        />
+                      )
+                    }
+                  </div>
+                }
+                suffix={
+                  <Text type={this.state.appKey.length !== 32 ? "danger" : ""}>{Math.floor(this.state.appKey.length / 2)} / 16 Bytes</Text>
+                }
+              />
 
 
-          <Text style={{marginTop: 30, display: 'block'}} strong>Attach a Label (Optional)</Text>
-          <LabelAppliedNew
-            allLabels={allLabels}
-            value={this.state.labelName}
-            select={value => this.setState({ labelName: value })}
-          />
-        </Card>
-        <div style={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
-          <UserCan>
-            <Button key="submit" icon={<SaveOutlined />} onClick={this.handleSubmit} style={{ margin: 0 }}>
-              Save Device
-            </Button>
-          </UserCan>
-        </div>
+              <Text style={{marginTop: 30, display: 'block'}} strong>Attach a Label (Optional)</Text>
+              <LabelAppliedNew
+                allLabels={allLabels}
+                value={this.state.labelName}
+                select={value => this.setState({ labelName: value })}
+              />
+            </Card>
+            <div style={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <UserCan>
+                <Button key="submit" icon={<SaveOutlined />} onClick={this.handleSubmit} style={{ margin: 0 }}>
+                  Save Device
+                </Button>
+              </UserCan>
+            </div>
+          </Col>
+          <Col span={12}>
+          </Col>
+        </Row>
       </div>
     )
   }
