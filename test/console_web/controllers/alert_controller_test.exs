58440bb9408fcc3f1572653203375768c87c6cfb
Alejandra Buznego <abuznego23@gmail.com>
2021-04-27 17:36:35 -0600

    add tests

--- test/console_web/controllers/alert_controller_test.exs
+++ test/console_web/controllers/alert_controller_test.exs
@@ -65,5 +65,78 @@ defmodule ConsoleWeb.AlertControllerTest do
       resp_conn = delete conn, alert_path(conn, :delete, alert_2.id)
       assert response(resp_conn, 204)
     end
+
+    test "adds alerts to nodes properly", %{conn: conn} do
+      organization_id = conn |> get_req_header("organization") |> List.first()
+      alert_1 = insert(:alert, %{ organization_id: organization_id, name: "alert name", node_type: "integration", config: %{
+        "email" => %{"device_join_otaa_first_time" => %{"recipient" => "admin"}},
+        "webhook" => %{}
+      } })
+      device_1 = insert(:device, %{ organization_id: organization_id })
+
+      resp_conn = post conn, alert_path(conn, :add_alert_to_node, %{
+        node_id: device_1.id,
+        node_type: "device",
+        alert_id: alert_1.id
+      })
+      assert response(resp_conn, 204)
+
+      not_my_org = insert(:organization)
+      not_my_device = insert(:device, %{ organization_id: not_my_org.id })
+
+      assert_error_sent 404, fn ->
+        post conn, alert_path(conn, :add_alert_to_node, %{
+          node_id: not_my_device.id,
+          node_type: "device",
+          alert_id: alert_1.id
+        })
+      end
+
+      not_my_alert = insert(:alert, %{ organization_id: not_my_org.id, name: "some other alert name", node_type: "integration", config: %{
+        "email" => %{"device_join_otaa_first_time" => %{"recipient" => "admin"}},
+        "webhook" => %{}
+      } })
+
+      assert_error_sent 404, fn ->
+        post conn, alert_path(conn, :add_alert_to_node, %{
+          node_id: device_1.id,
+          node_type: "device",
+          alert_id: not_my_alert.id
+        })
+      end
+    end
+
+    test "removes alerts from nodes properly", %{conn: conn} do
+      organization_id = conn |> get_req_header("organization") |> List.first()
+      alert_1 = insert(:alert, %{ organization_id: organization_id, name: "alert name", node_type: "integration", config: %{
+        "email" => %{"device_join_otaa_first_time" => %{"recipient" => "admin"}},
+        "webhook" => %{}
+      } })
+      device_1 = insert(:device, %{ organization_id: organization_id })
+      alert_node = insert(:alert_node, %{ alert_id: alert_1.id, node_id: device_1.id, node_type: "device" })
+      
+      resp_conn = post conn, alert_path(conn, :remove_alert_from_node, %{
+        alert_id: alert_1.id,
+        node_id: device_1.id,
+        node_type: "device"
+      })
+      assert response(resp_conn, 204)
+
+      not_my_org = insert(:organization)
+      not_my_device = insert(:device, %{ organization_id: not_my_org.id })
+      not_my_alert = insert(:alert, %{ organization_id: not_my_org.id, name: "some other alert name", node_type: "integration", config: %{
+        "email" => %{"device_join_otaa_first_time" => %{"recipient" => "admin"}},
+        "webhook" => %{}
+      } })
+      not_my_alert_node = insert(:alert_node, %{ alert_id: not_my_alert.id, node_id: not_my_device.id, node_type: "device" })
+
+      assert_error_sent 404, fn ->
+        post conn, alert_path(conn, :remove_alert_from_node, %{
+          node_id: not_my_device.id,
+          node_type: "device",
+          alert_id: not_my_alert.id
+        })
+      end
+    end
   end
 end
