ed5967d449ea7b64fcab93c565432c0dff90a27c
victor <victor@helium.com>
2021-04-28 09:20:36 -0700

    Fix

--- assets/js/components/multi_buy/DeleteMultiplePacketModal.jsx
+++ assets/js/components/multi_buy/DeleteMultiplePacketModal.jsx
@@ -18,25 +18,25 @@ export default ({ open, selected, close }) => {
 
   return (
     <Modal
-        title='Delete Multiple Packet'
-        visible={open}
-        onCancel={close}
-        centered
-        onOk={handleSubmit}
-        footer={[
-          <Button key="back" onClick={close}>
-            Cancel
-          </Button>,
-          <Button key="submit" type="primary" onClick={handleSubmit}>
-            Submit
-          </Button>,
-        ]}
-      >
-        {selected &&
-          <div>
-            <Text>Are you sure you want to delete <b>{selected.name}</b>?</Text>
-          </div>
-        }
-      </Modal>
+      title='Delete Multiple Packet'
+      visible={open}
+      onCancel={close}
+      centered
+      onOk={handleSubmit}
+      footer={[
+        <Button key="back" onClick={close}>
+          Cancel
+        </Button>,
+        <Button key="submit" type="primary" onClick={handleSubmit}>
+          Submit
+        </Button>,
+      ]}
+    >
+      {selected &&
+        <div>
+          <Text>Are you sure you want to delete <b>{selected.name}</b>?</Text>
+        </div>
+      }
+    </Modal>
   );
 }
