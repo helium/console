5c859c05c0429f3be34331db863f562dbbc1b854
victor <victor@helium.com>
2021-04-28 16:43:52 -0700

    add multi buy badge

--- assets/js/components/flows/nodes/DeviceNode.jsx
+++ assets/js/components/flows/nodes/DeviceNode.jsx
@@ -4,6 +4,7 @@ import { Typography } from 'antd';
 const { Text } = Typography;
 import DeviceIcon from '../../../../img/device-node-icon.svg';
 import AdrTag from '../../../../img/adr/adr-node-tag.svg';
+import MultiBuyTag from '../../../../img/multi_buy/multi-buy-node-tag.svg';
 import SelectedNodeIcon from './SelectedNodeIcon';
 
 export default ({ data, fromSidebar, selected }) => {
@@ -23,11 +24,18 @@ export default ({ data, fromSidebar, selected }) => {
             <img draggable="false" src={DeviceIcon} style={{ height: 14, marginRight: 8 }} />
             <Text style={{ display: 'block', fontSize: 16, color: '#ffffff', fontWeight: 500 }}>{data.label}</Text>
           </div>
-          {
-            data.adrAllowed && (
-              <img draggable="false" src={AdrTag} style={{ height: 16, marginLeft: 20, position: 'relative', top: -2 }} />
-            )
-          }
+          <div>
+            {
+              data.adrAllowed && (
+                <img draggable="false" src={AdrTag} style={{ height: 20, marginLeft: 20, position: 'relative', top: -2 }} />
+              )
+            }
+            {
+              data.multi_buy_id && (
+                <img draggable="false" src={MultiBuyTag} style={{ height: 20, marginLeft: 4, position: 'relative', top: -2 }} />
+              )
+            }
+          </div>
         </div>
 
         {!fromSidebar && (
