2928e345bf8bd9419a4ae211709d3f895381f4d0
victor <victor@helium.com>
2021-04-28 14:04:46 -0700

    Create initial components

---
+++ assets/js/components/flows/infoSidebar/MultiPacketNodeSettings.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_MULTI_BUYS } from '../../../graphql/multiBuys';
+import { SkeletonLayout } from '../../common/SkeletonLayout';
+import { Switch, Typography, Table } from 'antd';
+import { Link } from 'react-router-dom';
+import UserCan from '../../common/UserCan';
+const { Text } = Typography
+
+export default (props) => {
+  const { loading, error, data } = useQuery(ALL_MULTI_BUYS, { fetchPolicy: 'cache-first' });
+  if (loading) return (<div style={{ padding: '20px 40px 0px 40px' }}><SkeletonLayout /></div>);
+  if (error) return (<div style={{ padding: '20px 40px 0px 40px' }}><Text>Data failed to load, please reload the page and try again</Text></div>);
+
+  return (
+    <div style={{ padding: '0px 40px 0px 40px' }}>
+      <Table
+        dataSource={data.allMultiBuys}
+        rowKey={record => record.id}
+        pagination={false}
+        columns={[
+          {
+            title: '',
+            dataIndex: 'name',
+            render: (data, record) => (
+              <Link to={`/multi_buys/${record.id}`}>{data}</Link>
+            )
+          },
+          {
+            title: '',
+            dataIndex: '',
+            render: (text, record) => (
+              <UserCan>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
+                  <Switch
+                    checked={true}
+                    onChange={(checked) => {}}
+                  />
+                </div>
+              </UserCan>
+            )
+          }
+        ]}
+      />
+    </div>
+  )
+}
