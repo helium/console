1a98e9b1a4169f0cd75df3b19062ca2a1a513cb6
Alejandra Buznego <abuznego23@gmail.com>
2021-05-07 16:14:36 -0600

    add broadcasts

--- lib/console_web/controllers/v1/alert_controller.ex
+++ lib/console_web/controllers/v1/alert_controller.ex
@@ -1,10 +1,10 @@
 defmodule ConsoleWeb.V1.AlertController do
   use ConsoleWeb, :controller
 
-  alias Console.Organizations
   alias Console.Alerts
   alias Console.Alerts.Alert
   alias Console.Alerts.AlertNode
+  alias Console.Organizations.Organization
   action_fallback(ConsoleWeb.FallbackController)
 
   plug CORSPlug, origin: "*"
@@ -17,6 +17,7 @@ defmodule ConsoleWeb.V1.AlertController do
       })
     
     with {:ok, %Alert{} = alert} <- Alerts.create_alert(alert_params) do
+      broadcast_alerts_change(current_organization)
       conn
       |> put_status(:created)
       |> render("show.json", alert: alert)
@@ -30,6 +31,8 @@ defmodule ConsoleWeb.V1.AlertController do
         {:error, :not_found, "Alert not found"}
       %Alert{} = alert -> 
         with {:ok, %Alert{} = alert} <- Alerts.update_alert(alert, alert_params) do
+          broadcast_alerts_change(current_organization)
+          broadcast_alert_show(alert)
           conn
           |> send_resp(:ok, "Alert updated")
         end
@@ -44,6 +47,7 @@ defmodule ConsoleWeb.V1.AlertController do
         {:error, :not_found, "Alert not found"}
       %Alert{} = alert ->
         with {:ok, _} <- Alerts.delete_alert(alert) do
+          broadcast_alerts_change(current_organization)
           conn
           |> send_resp(:ok, "Alert deleted")
         end
@@ -60,7 +64,8 @@ defmodule ConsoleWeb.V1.AlertController do
           {:error, :not_found, "Alert not found"}
         %Alert{} = alert ->
           with {:ok, %AlertNode{} = alert_node} <- Alerts.add_alert_node(current_organization, alert, node_id, node_type) do
-          conn
+            broadcast_alert_settings(alert_node.node_id, alert_node.node_type)
+            conn
             |> send_resp(:ok, "Alert was successfully added to the #{node_type} node")
         end
       end
@@ -75,7 +80,8 @@ defmodule ConsoleWeb.V1.AlertController do
     alert_node = Alerts.get_alert_node(alert_id, node_id, node_type)
 
     if alert != nil and alert_node != nil do
-      with {:ok, %AlertNode{} = deleted_alert_node} <- Alerts.remove_alert_node(current_organization, alert_node) do
+      with {:ok, %AlertNode{} = _deleted_alert_node} <- Alerts.remove_alert_node(current_organization, alert_node) do
+        broadcast_alert_settings(alert_node.node_id, alert_node.node_type)
         conn
           |> send_resp(:ok, "Alert was successfully removed from the #{node_type} node")
       end
@@ -87,5 +93,17 @@ defmodule ConsoleWeb.V1.AlertController do
         end
       {:error, :not_found, msg}
     end
+  end
+
+  defp broadcast_alerts_change(%Organization{} = organization) do
+    ConsoleWeb.Endpoint.broadcast("graphql:alerts_index_table", "graphql:alerts_index_table:#{organization.id}:alert_list_update", %{})
+  end
+
+  defp broadcast_alert_show(%Alert{} = alert) do
+    ConsoleWeb.Endpoint.broadcast("graphql:alert_show", "graphql:alert_show:#{alert.id}:alert_update", %{})
+  end
+
+  defp broadcast_alert_settings(node_id, node_type) do
+    ConsoleWeb.Endpoint.broadcast("graphql:alert_settings_table", "graphql:alert_settings_table:#{node_type}-#{node_id}:alert_settings_update", %{})
   end
 end
