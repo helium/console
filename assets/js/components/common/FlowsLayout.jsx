e952e313c80ddee65caba4add64a9dab245d05c0
Alejandra Buznego <abuznego23@gmail.com>
2021-05-10 17:37:22 -0600

    fix overlapping node issue + container cutting off

--- assets/js/components/common/FlowsLayout.jsx
+++ assets/js/components/common/FlowsLayout.jsx
@@ -1,33 +1,30 @@
-import React, { useState } from 'react';
-import ReactFlow, {
-  isNode,
-  useStoreState
-} from 'react-flow-renderer';
-import dagre from 'dagre';
-import find from 'lodash/find';
+import React, { useState, useEffect } from "react";
+import ReactFlow, { isNode, useStoreState } from "react-flow-renderer";
+import dagre from "dagre";
+import find from "lodash/find";
 
-import LabelNode from './nodes/LabelNode';
-import FunctionNode from './nodes/FunctionNode';
-import ChannelNode from './nodes/ChannelNode';
-import DeviceNode from './nodes/DeviceNode';
+import LabelNode from "./nodes/LabelNode";
+import FunctionNode from "./nodes/FunctionNode";
+import ChannelNode from "./nodes/ChannelNode";
+import DeviceNode from "./nodes/DeviceNode";
 
 const nodeTypes = {
   labelNode: LabelNode,
   functionNode: FunctionNode,
   channelNode: ChannelNode,
-  deviceNode: DeviceNode
+  deviceNode: DeviceNode,
 };
 
 const getLayoutElements = (elements, nodeSizes) => {
-  const DEFAULT_NODE_WIDTH = 300; // TODO reduce so that flow can be shorter + fix if node is wider than this
+  const DEFAULT_NODE_WIDTH = 50; // for initial render
   const DEFAULT_NODE_HEIGHT = 30;
   const g = new dagre.graphlib.Graph({});
-  g.setGraph({ rankdir: 'LR' });
+  g.setGraph({ rankdir: "LR" });
   g.setDefaultEdgeLabel(() => ({}));
 
   elements.forEach((el) => {
     if (isNode(el)) {
-      const existingNode = find(nodeSizes, {id: el.id});
+      const existingNode = find(nodeSizes, { id: el.id });
       let nodeWidth = existingNode ? existingNode.width : DEFAULT_NODE_WIDTH;
       let nodeHeight = existingNode ? existingNode.height : DEFAULT_NODE_HEIGHT;
       g.setNode(el.id, { width: nodeWidth, height: nodeHeight });
@@ -35,16 +32,16 @@ const getLayoutElements = (elements, nodeSizes) => {
       g.setEdge(el.source, el.target);
     }
   });
-  
+
   dagre.layout(g);
 
   return elements.map((el) => {
     if (isNode(el)) {
       const nodeWithPosition = g.node(el.id);
-      el.targetPosition = 'left';
-      el.sourcePosition = 'right';
+      el.targetPosition = "left";
+      el.sourcePosition = "right";
 
-      const existingNode = find(nodeSizes, {id: el.id});
+      const existingNode = find(nodeSizes, { id: el.id });
       let nodeWidth = existingNode ? existingNode.width : DEFAULT_NODE_WIDTH;
       let nodeHeight = existingNode ? existingNode.height : DEFAULT_NODE_HEIGHT;
 
@@ -59,20 +56,22 @@ const getLayoutElements = (elements, nodeSizes) => {
 
     return el;
   });
-}
+};
 
 export default ({ elements }) => {
   const nodeStates = useStoreState((state) => state.nodes);
-  const nodeSizes = nodeStates.map(node => (
-    { 
-      id: node.id,
-      width: node.__rf.width,
-      height: node.__rf.height
-    }
-  ));
+  const nodeSizes = nodeStates.map((node) => ({
+    id: node.id,
+    width: node.__rf.width,
+    height: node.__rf.height,
+  }));
 
   const layoutedElements = getLayoutElements(elements, nodeSizes);
-  const [flowElements] = useState(layoutedElements);
+  const [flowElements, setFlowElements] = useState([]);
+
+  useEffect(() => {
+    setFlowElements(layoutedElements);
+  }, [nodeSizes.length]);
 
   return (
     <ReactFlow
@@ -86,5 +85,5 @@ export default ({ elements }) => {
       zoomOnDoubleClick={false}
       paneMoveable={false}
     />
-  ); 
-}
+  );
+};
