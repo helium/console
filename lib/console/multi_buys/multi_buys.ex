f4b980beb3221467fed2ebe5f8f40be68a04e75f
Victor Mu <victor@helium.com>
2021-05-14 10:16:23 -0700

    Inform router when devices and associated resources are updated (#612)
    * Update router broadcasts when console resources change
    
    * Update label controller
    
    * update flow controller
    
    * Update device controller
    
    * fix
    
    * ON delete set null

--- lib/console/multi_buys/multi_buys.ex
+++ lib/console/multi_buys/multi_buys.ex
@@ -2,13 +2,15 @@ defmodule Console.MultiBuys do
   import Ecto.Query, warn: false
   alias Console.Repo
   alias Console.MultiBuys.MultiBuy
+  alias Console.Devices.Device
+  alias Console.Labels.Label
 
   def get_multi_buy!(id), do: Repo.get!(MultiBuy, id)
   def get_multi_buy(id), do: Repo.get(MultiBuy, id)
 
   def get_multi_buy!(organization, id) do
     Repo.get_by!(MultiBuy, [id: id, organization_id: organization.id])
- end
+  end
 
   def create_multi_buy(attrs \\ %{}) do
     %MultiBuy{}
@@ -24,5 +26,30 @@ defmodule Console.MultiBuys do
     multi_buy
     |> MultiBuy.changeset(attrs)
     |> Repo.update()
+  end
+
+  def get_all_multi_buy_associated_device_ids(id) do
+    device_ids =
+      get_associated_multi_buy_devices(id)
+
+    label_device_ids =
+      get_associated_multi_buy_labels(id)
+      |> Enum.map(fn l -> l.devices end)
+      |> List.flatten()
+      |> Enum.map(fn d -> d.id end)
+
+    device_ids ++ label_device_ids |> Enum.uniq()
+  end
+
+  defp get_associated_multi_buy_devices(id) do
+    from(d in Device, where: d.multi_buy_id == ^id)
+    |> select([d], d.id)
+    |> Repo.all()
+  end
+
+  defp get_associated_multi_buy_labels(id) do
+    from(l in Label, where: l.multi_buy_id == ^id)
+    |> preload([:devices])
+    |> Repo.all()
   end
 end
--- lib/console_web/controllers/label_controller.ex
+++ lib/console_web/controllers/label_controller.ex
@@ -32,12 +32,14 @@ defmodule ConsoleWeb.LabelController do
 
   def update(conn, %{"id" => id, "label" => label_params}) do
     current_organization = conn.assigns.current_organization
-    label = Labels.get_label!(current_organization, id)
+    label = Labels.get_label!(current_organization, id) |> Labels.fetch_assoc([:devices])
+    device_ids = label.devices |> Enum.map(fn d -> d.id end)
     name = label.name
 
     with {:ok, %Label{} = label} <- Labels.update_label(label, label_params) do
       ConsoleWeb.Endpoint.broadcast("graphql:label_show", "graphql:label_show:#{label.id}:label_update", %{})
       ConsoleWeb.Endpoint.broadcast("graphql:resources_update", "graphql:resources_update:#{current_organization.id}:organization_resources_update", %{})
+      broadcast_router_update_devices(device_ids)
 
       msg =
         cond do
@@ -54,10 +56,12 @@ defmodule ConsoleWeb.LabelController do
   def delete(conn, %{"id" => id}) do
     current_organization = conn.assigns.current_organization
     label = Labels.get_label!(current_organization, id) |> Labels.fetch_assoc([:devices])
+    device_ids = label.devices |> Enum.map(fn d -> d.id end)
 
     with {:ok, %Label{} = label} <- Labels.delete_label(label) do
       ConsoleWeb.Endpoint.broadcast("graphql:device_index_labels_bar", "graphql:device_index_labels_bar:#{current_organization.id}:label_list_update", %{})
       Alerts.delete_alert_nodes(id, "label")
+      broadcast_router_update_devices(device_ids)
 
       conn
       |> put_resp_header("message", "#{label.name} deleted successfully")
@@ -83,6 +87,7 @@ defmodule ConsoleWeb.LabelController do
         ConsoleWeb.Endpoint.broadcast("graphql:label_show", "graphql:label_show:#{destination_label.id}:label_update", %{})
         ConsoleWeb.Endpoint.broadcast("graphql:label_show_table", "graphql:label_show_table:#{destination_label.id}:update_label_devices", %{})
         ConsoleWeb.Endpoint.broadcast("graphql:device_index_labels_bar", "graphql:device_index_labels_bar:#{current_organization.id}:label_list_update", %{})
+        broadcast_router_update_devices(devices)
 
         conn
         |> put_resp_header("message", msg)
@@ -132,6 +137,7 @@ defmodule ConsoleWeb.LabelController do
       ConsoleWeb.Endpoint.broadcast("graphql:label_show", "graphql:label_show:#{label.id}:label_update", %{})
       ConsoleWeb.Endpoint.broadcast("graphql:label_show_table", "graphql:label_show_table:#{label.id}:update_label_devices", %{})
       ConsoleWeb.Endpoint.broadcast("graphql:device_index_labels_bar", "graphql:device_index_labels_bar:#{current_organization.id}:label_list_update", %{})
+      broadcast_router_update_devices(devices)
 
       conn
       |> put_resp_header("message", "Device(s) successfully removed from label")
@@ -148,6 +154,7 @@ defmodule ConsoleWeb.LabelController do
       ConsoleWeb.Endpoint.broadcast("graphql:device_index_labels_bar", "graphql:device_index_labels_bar:#{current_organization.id}:label_list_update", %{})
       ConsoleWeb.Endpoint.broadcast("graphql:device_show", "graphql:device_show:#{device.id}:device_update", %{})
       ConsoleWeb.Endpoint.broadcast("graphql:device_show_labels_table", "graphql:device_show_labels_table:#{device.id}:device_update", %{})
+      broadcast_router_update_devices(device_id)
 
       conn
       |> put_resp_header("message", "Label(s) successfully removed from device")
@@ -176,7 +183,7 @@ defmodule ConsoleWeb.LabelController do
       ConsoleWeb.Endpoint.broadcast("graphql:device_index_labels_bar", "graphql:device_index_labels_bar:#{current_organization.id}:label_list_update", %{})
 
       assoc_labels = labels |> Labels.multi_fetch_assoc([:devices])
-      assoc_devices = Enum.map(assoc_labels, fn l -> l.devices end) |> List.flatten() |> Enum.uniq()
+      assoc_devices = Enum.map(assoc_labels, fn l -> l.devices end) |> List.flatten() |> Enum.map(fn d -> d.id end) |> Enum.uniq()
       broadcast_router_update_devices(assoc_devices)
 
       conn
@@ -189,7 +196,10 @@ defmodule ConsoleWeb.LabelController do
     current_organization = conn.assigns.current_organization
 
     Labels.delete_all_labels_from_devices_for_org(current_organization)
+    all_device_ids = Devices.get_devices(current_organization.id) |> Enum.map(fn d -> d.id end)
+
     ConsoleWeb.Endpoint.broadcast("graphql:devices_index_table", "graphql:devices_index_table:#{current_organization.id}:device_list_update", %{})
+    broadcast_router_update_devices(all_device_ids)
 
     conn
       |> put_resp_header("message", "All devices successfully removed from labels")
@@ -222,7 +232,7 @@ defmodule ConsoleWeb.LabelController do
           ConsoleWeb.Endpoint.broadcast("graphql:devices_index_table", "graphql:devices_index_table:#{current_organization.id}:device_list_update", %{})
           ConsoleWeb.Endpoint.broadcast("graphql:device_show", "graphql:device_show:#{device.id}:device_update", %{})
 
-          broadcast_router_update_devices(label)
+          broadcast_router_update_devices(devices)
 
           conn
           |> put_resp_header("message", "#{count} Devices added to label successfully")
@@ -254,7 +264,7 @@ defmodule ConsoleWeb.LabelController do
         ConsoleWeb.Endpoint.broadcast("graphql:device_show_labels_table", "graphql:device_show_labels_table:#{device.id}:device_update", %{})
 
         assoc_devices = devices_labels |> Enum.map(fn dl -> dl.device_id end)
-        ConsoleWeb.Endpoint.broadcast("device:all", "device:all:refetch:devices", %{ "devices" => assoc_devices })
+        broadcast_router_update_devices(assoc_devices)
 
         conn
         |> put_resp_header("message", msg)
@@ -263,17 +273,7 @@ defmodule ConsoleWeb.LabelController do
     end
   end
 
-  defp broadcast_router_update_devices(%Label{} = label) do
-    assoc_device_ids = label |> Labels.fetch_assoc([:devices]) |> Map.get(:devices) |> Enum.map(fn d -> d.id end)
-    if length(assoc_device_ids) > 0 do
-      ConsoleWeb.Endpoint.broadcast("device:all", "device:all:refetch:devices", %{ "devices" => assoc_device_ids })
-    end
-  end
-
-  defp broadcast_router_update_devices(devices) do
-    assoc_device_ids = devices |> Enum.map(fn d -> d.id end)
-    if length(assoc_device_ids) > 0 do
-      ConsoleWeb.Endpoint.broadcast("device:all", "device:all:refetch:devices", %{ "devices" => assoc_device_ids })
-    end
+  defp broadcast_router_update_devices(device_ids) do
+    ConsoleWeb.Endpoint.broadcast("device:all", "device:all:refetch:devices", %{ "devices" => device_ids })
   end
 end
