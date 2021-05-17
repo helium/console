9f3210399e0d5fac8562cf13a9b09b6fc1e0da8e
Alejandra Buznego <abuznego23@gmail.com>
2021-05-17 16:27:08 -0600

    fix for alignment in other component

--- assets/js/components/alerts/DeleteAlertModal.jsx
+++ assets/js/components/alerts/DeleteAlertModal.jsx
@@ -1,37 +1,41 @@
-import React from 'react';
-import { Modal, Button, Typography } from 'antd';
+import React from "react";
+import { Modal, Button, Typography } from "antd";
 const { Text } = Typography;
 import { useDispatch } from "react-redux";
-import { deleteAlert } from '../../actions/alert';
+import { deleteAlert } from "../../actions/alert";
 
 export default ({ open, alert, close }) => {
   const dispatch = useDispatch();
 
   const handleSubmit = () => {
-    dispatch(deleteAlert(alert.id)).then(_ => { close() });
-  }
+    dispatch(deleteAlert(alert.id)).then((_) => {
+      close();
+    });
+  };
 
   return (
     <Modal
-        title='Delete Alert'
-        visible={open}
-        onCancel={close}
-        centered
-        onOk={handleSubmit}
-        footer={[
-          <Button key="back" onClick={close}>
-            Cancel
-          </Button>,
-          <Button key="submit" type="primary" onClick={handleSubmit}>
-            Submit
-          </Button>,
-        ]}
-      >
-        {alert && 
-          <div>
-            <Text>Are you sure you want to delete the alert, <b>{alert.name}</b>?</Text>
-          </div>
-        }
-      </Modal>
+      title="Delete Alert"
+      visible={open}
+      onCancel={close}
+      centered
+      onOk={handleSubmit}
+      footer={[
+        <Button key="back" onClick={close}>
+          Cancel
+        </Button>,
+        <Button key="submit" type="primary" onClick={handleSubmit}>
+          Submit
+        </Button>,
+      ]}
+    >
+      {alert && (
+        <div style={{ textAlign: "center" }}>
+          <Text>
+            Are you sure you want to delete the alert, <b>{alert.name}</b>?
+          </Text>
+        </div>
+      )}
+    </Modal>
   );
-}
+};
