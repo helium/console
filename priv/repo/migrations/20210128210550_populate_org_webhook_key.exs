ffecd34e2c142cd633ea7cbee6367985e806f9ef
Alejandra Buznego <abuznego23@gmail.com>
2021-05-18 16:40:19 -0600

    comment out already ran data population

--- priv/repo/migrations/20210128210550_populate_org_webhook_key.exs
+++ priv/repo/migrations/20210128210550_populate_org_webhook_key.exs
@@ -5,23 +5,23 @@ defmodule Console.Repo.Migrations.PopulateOrgWebhookKey do
   alias Console.Organizations.Organization
 
   def up do
-    organizations = from(o in Organization, where: is_nil(o.webhook_key)) |> Console.Repo.all
+    # organizations = from(o in Organization, where: is_nil(o.webhook_key)) |> Console.Repo.all
     
-    Enum.each(organizations, fn organization ->
-      webhook_key = Helpers.generate_token(32)
+    # Enum.each(organizations, fn organization ->
+    #   webhook_key = Helpers.generate_token(32)
     
-      organization
-      |> Organization.changeset(%{ webhook_key: webhook_key })
-      |> Console.Repo.update()
-    end)
+    #   organization
+    #   |> Organization.changeset(%{ webhook_key: webhook_key })
+    #   |> Console.Repo.update()
+    # end)
   end
 
   def down do
-    organizations = from(o in Organization, where: not is_nil(o.webhook_key)) |> Console.Repo.all
+    # organizations = from(o in Organization, where: not is_nil(o.webhook_key)) |> Console.Repo.all
     
-    Enum.each(organizations, fn organization ->
-      changeset = Ecto.Changeset.change(organization, webhook_key: nil)
-      Console.Repo.update!(changeset)
-    end)
+    # Enum.each(organizations, fn organization ->
+    #   changeset = Ecto.Changeset.change(organization, webhook_key: nil)
+    #   Console.Repo.update!(changeset)
+    # end)
   end
 end
