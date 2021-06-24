19ce5ee633a04fa516929ff6175d71cf8bfb55bd
Alejandra Buznego <abuznego23@gmail.com>
2021-06-24 13:12:12 -0700

    initial optimization

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,18 +1,19 @@
-const path = require('path');
-const webpack = require('webpack')
-const dotenv = require('dotenv').config( {
-  path: path.join(__dirname, '.env')
-} );
+const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv").config({
+  path: path.join(__dirname, ".env"),
+});
 
-module.exports = function(env) {
-  const production = process.env.NODE_ENV === 'production';
+module.exports = function (env) {
+  const production = process.env.NODE_ENV === "production";
   return {
-    devtool: production ? 'source-maps' : 'eval',
-    entry: './js/app.js',
+    devtool: production ? "source-maps" : "eval",
+    entry: "./js/app.js",
     output: {
-      path: path.resolve(__dirname, '../priv/static/js'),
-      filename: 'app.js',
-      publicPath: '/',
+      path: path.resolve(__dirname, "../priv/static/js"),
+      filename: "app.js",
+      publicPath: "/",
+      chunkFilename: "[name].bundle.js",
     },
     module: {
       rules: [
@@ -20,32 +21,41 @@ module.exports = function(env) {
           test: /\.jsx?$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
           },
         },
         {
           test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.(png|svg|jpg|gif)$/,
           use: [
             {
-              loader: 'file-loader',
+              loader: "file-loader",
               options: {
-                publicPath: '/js'
-              }
-            }
-          ]
+                publicPath: "/js",
+              },
+            },
+          ],
         },
       ],
     },
     resolve: {
-      modules: ['node_modules', path.resolve(__dirname, 'js')],
-      extensions: ['.js', '.jsx'],
+      modules: ["node_modules", path.resolve(__dirname, "js")],
+      extensions: [".js", ".jsx"],
     },
     plugins: [
-      new webpack.EnvironmentPlugin(['AUTH_0_DOMAIN', 'AUTH_0_CLIENT_ID', 'ENV_DOMAIN', 'STRIPE_PUBLIC_KEY', 'SELF_HOSTED', 'INTERCOM_ID_SECRET', 'CONSOLE_VERSION', 'RELEASE_BLOG_LINK'])
+      new webpack.EnvironmentPlugin([
+        "AUTH_0_DOMAIN",
+        "AUTH_0_CLIENT_ID",
+        "ENV_DOMAIN",
+        "STRIPE_PUBLIC_KEY",
+        "SELF_HOSTED",
+        "INTERCOM_ID_SECRET",
+        "CONSOLE_VERSION",
+        "RELEASE_BLOG_LINK",
+      ]),
     ],
   };
 };
