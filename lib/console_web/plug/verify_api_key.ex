5345ff9234f46312f71502f782605f23eb9213bf
Alejandra Buznego <abuznego23@gmail.com>
2021-06-28 14:01:46 -0700

    fixes

--- lib/console_web/plug/verify_api_key.ex
+++ lib/console_web/plug/verify_api_key.ex
@@ -8,23 +8,36 @@ defmodule ConsoleWeb.Plug.VerifyApiKey do
   def init(default), do: default
 
   def call(conn, _default) do
-    key = conn |> get_req_header("key") |> List.first()
-    bin = :crypto.hash(:sha256, key)
-
-    case ApiKeys.get_api_key(bin) do
+    case conn |> get_req_header("key") do
       nil ->
         conn
-        |> auth_error({:invalid_api_key, :invalid_api_key}, %{})
-        |> halt()
-      %ApiKey{ active: false } ->
-        conn
-        |> auth_error({:api_key_needs_email_verification, :api_key_needs_email_verification}, %{})
-        |> halt()
-      %ApiKey{ organization_id: organization_id, user_id: user_id } ->
-        current_organization = Organizations.get_organization!(organization_id)
-        conn
-        |> assign(:current_organization, current_organization)
-        |> assign(:user_id, user_id)
+          |> send_resp(
+              :forbidden,
+              Poison.encode!(%{
+                type: "forbidden",
+                errors: ["Key header is missing"]
+              })
+            )
+          |> halt()
+      _ ->
+        key = conn |> get_req_header("key") |> List.first()
+        bin = :crypto.hash(:sha256, key)
+
+        case ApiKeys.get_api_key(bin) do
+          nil ->
+            conn
+            |> auth_error({:invalid_api_key, :invalid_api_key}, %{})
+            |> halt()
+          %ApiKey{ active: false } ->
+            conn
+            |> auth_error({:api_key_needs_email_verification, :api_key_needs_email_verification}, %{})
+            |> halt()
+          %ApiKey{ organization_id: organization_id, user_id: user_id } ->
+            current_organization = Organizations.get_organization!(organization_id)
+            conn
+            |> assign(:current_organization, current_organization)
+            |> assign(:user_id, user_id)
+        end
     end
   end
 end
