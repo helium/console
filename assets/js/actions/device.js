bd6c03ed574ad81c24c4ff7e82a62350373cc156
Alejandra Buznego <abuznego23@gmail.com>
2021-07-15 13:06:26 -0700

    fix

--- assets/js/actions/device.js
+++ assets/js/actions/device.js
@@ -1,6 +1,6 @@
-import { replace } from 'connected-react-router';
-import * as rest from '../util/rest';
-import sanitizeHtml from 'sanitize-html'
+import { replace } from "connected-react-router";
+import * as rest from "../util/rest";
+import sanitizeHtml from "sanitize-html";
 
 export const FETCHING_APPLICATIONS = "FETCHING_APPLICATIONS";
 export const FETCHED_APPLICATIONS = "FETCHED_APPLICATIONS";
@@ -17,185 +17,220 @@ export const RESET_GENERIC_IMPORT = "RESET_GENERIC_IMPORT";
 
 export const createDevice = (params, label) => {
   return (dispatch) => {
-    const deviceParams = sanitizeParams(params)
+    const deviceParams = sanitizeParams(params);
 
-    return rest.post('/api/devices', {
+    return rest
+      .post("/api/devices", {
         device: deviceParams,
         label: label,
       })
-      .then(response => {})
-  }
-}
+      .then((response) => {});
+  };
+};
 
 export const updateDevice = (id, params) => {
   return (dispatch) => {
-    const deviceParams = sanitizeParams(params)
+    const deviceParams = sanitizeParams(params);
 
-    return rest.put(`/api/devices/${id}`, {
-      device: deviceParams
-    })
-    .then(response => {})
-  }
-}
+    return rest
+      .put(`/api/devices/${id}`, {
+        device: deviceParams,
+      })
+      .then((response) => {});
+  };
+};
 
 export const deleteDevice = (id, redirect = true) => {
   return (dispatch) => {
-    rest.destroy(`/api/devices/${id}`)
-      .then(response => {
-        if (redirect) dispatch(replace('/devices'))
-      })
-  }
-}
+    rest.destroy(`/api/devices/${id}`).then((response) => {
+      if (redirect) dispatch(replace("/devices"));
+    });
+  };
+};
 
 export const deleteDevices = (devices, label_id = "none") => {
   return (dispatch) => {
     if (devices) {
-      rest.post(`/api/devices/delete`, {
-        devices: devices.map(d => d.id),
-        label_id
-      })
-      .then(response => {})
+      rest
+        .post(`/api/devices/delete`, {
+          devices: devices.map((d) => d.id),
+          label_id,
+        })
+        .then((response) => {});
     } else {
-      rest.post(`/api/devices/delete`)
-      .then(response => {})
+      rest.post(`/api/devices/delete`).then((response) => {});
     }
-  }
-}
+  };
+};
 
 export const setDevicesActive = (device_ids, active, label_id = "none") => {
   return (dispatch) => {
-    rest.post(`/api/devices/set_active`, {
-      device_ids,
-      active,
-      label_id,
-    })
-    .then(response => {})
-  }
-}
+    rest
+      .post(`/api/devices/set_active`, {
+        device_ids,
+        active,
+        label_id,
+      })
+      .then((response) => {});
+  };
+};
 
 export const fetchTtnDevices = (ttnCtlCode) => {
   return async (dispatch) => {
     dispatch(fetchingApplications());
     try {
-      const response = await rest.get(
-        `/api/ttn/devices`,
-        null,
-        { 'TTN-Ctl-Code': ttnCtlCode }
-      );
+      const response = await rest.get(`/api/ttn/devices`, null, {
+        "TTN-Ctl-Code": ttnCtlCode,
+      });
       dispatch(fetchedApplications(response.data));
     } catch (e) {
       dispatch(fetchFailed());
     }
-  }
-}
+  };
+};
 
-export const importTtnDevices = (applications, account_token, add_labels, delete_devices, delete_apps) => {
+export const importTtnDevices = (
+  applications,
+  account_token,
+  add_labels,
+  delete_devices,
+  delete_apps
+) => {
   return async (dispatch) => {
     dispatch(startingDeviceImport());
-    const response = await rest.post(
-      '/api/ttn/devices/import',
-      {applications, account_token, add_labels, delete_devices, delete_apps}
-    );
+    const response = await rest.post("/api/ttn/devices/import", {
+      applications,
+      account_token,
+      add_labels,
+      delete_devices,
+      delete_apps,
+    });
     dispatch(startedDeviceImport());
-  }
-}
+  };
+};
 
 export const scanGenericDevices = (file, onComplete) => {
   return async (dispatch) => {
     function parseCSV(input) {
       let rows = input.split(/\r?\n/);
       const keys = [];
-      rows.shift().split(",").forEach((val, index) => {
-        if (['Name', 'name'].indexOf(val.toLowerCase()) > -1) {
-          keys[index] = "name";
-        } else if (['appkey', 'app_key'].indexOf(val.replace(/\s/g, '').toLowerCase()) > -1) {
-          keys[index] = 'app_key';
-        } else if (['appeui', 'app_eui'].indexOf(val.replace(/\s/g, '').toLowerCase()) > -1) {
-          keys[index] = 'app_eui';
-        } else if (['deveui', 'dev_eui'].indexOf(val.replace(/\s/g, '').toLowerCase()) > -1) {
-          keys[index] = 'dev_eui';
-        } else if (['labelid', 'label_id'].indexOf(val.replace(/\s/g, '').toLowerCase()) > -1) {
-          keys[index] = 'label_id';
-        } else if (['labelid2', 'label_id_2'].indexOf(val.replace(/\s/g, '').toLowerCase()) > -1) {
-          keys[index] = 'label_id_2';
-        }
-      });
+      rows
+        .shift()
+        .split(",")
+        .forEach((val, index) => {
+          if (["Name", "name"].indexOf(val.toLowerCase()) > -1) {
+            keys[index] = "name";
+          } else if (
+            ["appkey", "app_key"].indexOf(
+              val.replace(/\s/g, "").toLowerCase()
+            ) > -1
+          ) {
+            keys[index] = "app_key";
+          } else if (
+            ["appeui", "app_eui"].indexOf(
+              val.replace(/\s/g, "").toLowerCase()
+            ) > -1
+          ) {
+            keys[index] = "app_eui";
+          } else if (
+            ["deveui", "dev_eui"].indexOf(
+              val.replace(/\s/g, "").toLowerCase()
+            ) > -1
+          ) {
+            keys[index] = "dev_eui";
+          } else if (
+            ["labelid", "label_id"].indexOf(
+              val.replace(/\s/g, "").toLowerCase()
+            ) > -1
+          ) {
+            keys[index] = "label_id";
+          } else if (
+            ["labelid2", "label_id_2"].indexOf(
+              val.replace(/\s/g, "").toLowerCase()
+            ) > -1
+          ) {
+            keys[index] = "label_id_2";
+          }
+        });
       let failed = false;
-      ['name', 'app_key', 'app_eui', 'dev_eui'].forEach((key) => {
+      ["name", "app_key", "app_eui", "dev_eui"].forEach((key) => {
         if (keys.indexOf(key) < 0) {
           failed = true;
-          dispatch({type: GENERIC_IMPORT_SCAN_FAILED});
-          onComplete('');
+          dispatch({ type: GENERIC_IMPORT_SCAN_FAILED });
+          onComplete("");
         }
-      })
+      });
       if (!failed) {
         rows = rows.filter((value) => value !== "");
-        dispatch(scannedGenericDevicesImport(rows.map((row) => {
-          return row.split(",").reduce((map, val, i) => {
-            keys[i] && (map[keys[i]] = val);
-            return map;
-          }, {});
-        })));
-        onComplete('csv')
+        dispatch(
+          scannedGenericDevicesImport(
+            rows.map((row) => {
+              return row.split(",").reduce((map, val, i) => {
+                keys[i] && (map[keys[i]] = val);
+                return map;
+              }, {});
+            })
+          )
+        );
+        onComplete("csv");
       }
     }
     parseCSV(file);
-  }
-}
+  };
+};
 
 export const resetGenericDeviceImport = (devices) => {
   return async (dispatch) => {
-    dispatch({type: RESET_GENERIC_IMPORT});
-  }
-}
+    dispatch({ type: RESET_GENERIC_IMPORT });
+  };
+};
 
 export const importGenericDevices = (devices, withLabel) => {
   return async (dispatch) => {
-    dispatch({type: GENERIC_IMPORT_STARTING});
-    await rest.post(
-      '/api/generic/devices/import',
-      {devices, add_labels: withLabel}
-    )
-    dispatch({type: GENERIC_IMPORT_STARTED});
-  }
-}
+    dispatch({ type: GENERIC_IMPORT_STARTING });
+    await rest.post("/api/generic/devices/import", {
+      devices,
+      add_labels: withLabel,
+    });
+    dispatch({ type: GENERIC_IMPORT_STARTED });
+  };
+};
 
-const fetchingApplications = () => ({ type: FETCHING_APPLICATIONS })
+const fetchingApplications = () => ({ type: FETCHING_APPLICATIONS });
 
 const fetchedApplications = (data) => {
   return {
     type: FETCHED_APPLICATIONS,
     ttnApplications: data.apps,
-    ttnAuthCode: data.account_token
-  }
-}
+    ttnAuthCode: data.account_token,
+  };
+};
 
-const fetchFailed = () => ({ type: FETCHING_APPLICATIONS_FAILED })
+const fetchFailed = () => ({ type: FETCHING_APPLICATIONS_FAILED });
 
-const startingDeviceImport = () => ({ type: IMPORT_STARTING })
+const startingDeviceImport = () => ({ type: IMPORT_STARTING });
 
-const startedDeviceImport = () => ({ type: IMPORT_STARTED })
+const startedDeviceImport = () => ({ type: IMPORT_STARTED });
 
 const scannedGenericDevicesImport = (devices) => {
   return {
     type: GENERIC_IMPORT_SCANNED,
-    devices
-  }
-}
+    devices,
+  };
+};
 
 const sanitizeParams = (params) => {
-  if (params.name) params.name = sanitizeHtml(params.name)
-  if (params.dev_eui) params.dev_eui = sanitizeHtml(params.dev_eui)
-  if (params.app_eui) params.app_eui = sanitizeHtml(params.app_eui)
-  if (params.app_key) params.app_key = sanitizeHtml(params.app_key)
-  return params
-}
+  if (params.name) params.name = sanitizeHtml(params.name);
+  if (params.dev_eui) params.dev_eui = sanitizeHtml(params.dev_eui);
+  if (params.app_eui) params.app_eui = sanitizeHtml(params.app_eui);
+  if (params.app_key) params.app_key = sanitizeHtml(params.app_key);
+  return params;
+};
 
 export const getAllEvents = (deviceId) => {
   return (dispatch) => {
-    return rest.get(`/api/devices/${deviceId}/events`)
-      .then(response => {
-        return response.data;
-      });
-  }
+    return rest.get(`/api/devices/${deviceId}/events`).then((response) => {
+      return response.data;
+    });
+  };
 };
--- assets/js/components/devices/import/ChooseImportType.jsx
+++ assets/js/components/devices/import/ChooseImportType.jsx
@@ -1,108 +1,151 @@
-import React, { Fragment, useState } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Button, Typography, Popover } from 'antd';
-import { blueForDeviceStatsLarge } from '../../../util/colors';
-import { scanGenericDevices } from '../../../actions/device';
-import Warning from '../../../../img/warning.svg';
-import ImportIcon from '../../../../img/device-import-icon.svg';
-import DragAndDrop from '../../common/DragAndDrop';
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Button, Typography, Popover } from "antd";
+import { blueForDeviceStatsLarge } from "../../../util/colors";
+import { scanGenericDevices } from "../../../actions/device";
+import Warning from "../../../../img/warning.svg";
+import ImportIcon from "../../../../img/device-import-icon.svg";
+import DragAndDrop from "../../common/DragAndDrop";
 
-const { Text, Title } = Typography
+const { Text, Title } = Typography;
 
-const ChooseImportType = ({onImportSelect, scanGenericDevices, genericImportScanFailed, deviceImports}) => {
-  const [open, setOpen] = useState(false);
-  if (genericImportScanFailed) return (
-    <Fragment>
-      <img src={Warning} style={{marginBottom: 10, height: 50, objectFit: 'cover'}}/>
-      <Title style={{fontSize: 26, width: '100%', textAlign: 'center'}}>Invalid Filetype</Title>
-      <Text style={{width: '100%', textAlign: 'center', margin: '0px 40px 10px'}}>
-        <b>Sorry. The file you supplied doesn’t appear to be formatted correctly.</b>
-        <br/>
-        Please ensure it is a csv file, with correctly formatted headers.
+const FormatPopover = () => (
+  <Popover
+    trigger="click"
+    content={
+      <Text style={{ display: "table", whiteSpace: "normal", width: 200 }}>
+        The fields <b>DevEUI, AppEUI, AppKey,</b> and <b>Name</b> are all
+        required. Optionally you can include a <b>LabelID</b> column to add
+        labels to imported devices.
       </Text>
-      <Popover
-        onVisibleChange={() => setOpen(!open)}
-        visible={open}
-        trigger="click"
-        content={
-          <Text style={{display: 'table', whiteSpace: 'normal', width: 200}}>
-            The fields <b>DevEUI, AppEUI, AppKey,</b> and <b>Name</b> are all required. Optionally you can include a <b>LabelID</b> column to add labels to imported devices.
-          </Text>
-        }
-      >
-        <Text style={{width: '100%', textAlign: 'center', marginBottom: 20, color: 'deepskyblue', textDecoration: 'underline'}}>How do I format my .csv?</Text>
-      </Popover>
-      <DragAndDrop fileSelected={(file) => {
-        let fileReader = new FileReader();
-        fileReader.onloadend = () => {
-          scanGenericDevices(fileReader.result, (type) => onImportSelect(type));
-        }
-        fileReader.readAsText(file);
-      }}>
-        <Text style={{textAlign: 'center', margin: '30px 80px', color: blueForDeviceStatsLarge}}>
-          Drag .csv file here or click to choose file
+    }
+  >
+    <a className="help-link" style={{ marginBottom: 20, color: "deepskyblue" }}>
+      How do I format my .csv?
+    </a>
+  </Popover>
+);
+
+const ChooseImportType = ({
+  onImportSelect,
+  scanGenericDevices,
+  genericImportScanFailed,
+  deviceImports,
+}) => {
+  if (genericImportScanFailed)
+    return (
+      <Fragment>
+        <img
+          src={Warning}
+          style={{ marginBottom: 10, height: 50, objectFit: "cover" }}
+        />
+        <Title style={{ fontSize: 26, width: "100%", textAlign: "center" }}>
+          Invalid Filetype
+        </Title>
+        <Text
+          style={{
+            width: "100%",
+            textAlign: "center",
+            margin: "0px 40px 10px",
+          }}
+        >
+          <b>
+            Sorry. The file you supplied doesn’t appear to be formatted
+            correctly.
+          </b>
+          <br />
+          Please ensure it is a CSV file, with correctly formatted headers.
         </Text>
-      </DragAndDrop>
-    </Fragment>
-  )
+        <FormatPopover />
+        <DragAndDrop
+          fileSelected={(file) => {
+            let fileReader = new FileReader();
+            fileReader.onloadend = () => {
+              scanGenericDevices(fileReader.result, (type) => {
+                if (type) onImportSelect(type);
+              });
+            };
+            fileReader.readAsText(file);
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              margin: "30px 80px",
+              color: blueForDeviceStatsLarge,
+            }}
+          >
+            Drag .csv file here or click to choose file
+          </Text>
+        </DragAndDrop>
+      </Fragment>
+    );
   return (
     <Fragment>
-      <img src={ImportIcon} style={{marginBottom: 10, height: 50, objectFit: 'cover'}}/>
-      <Title style={{fontSize: 26}}>Import Devices</Title>
-      <Text style={{width: '100%', textAlign: 'center', margin: '0px 40px 10px'}}>
-        You can import your devices directly from the Things Network, or in bulk via .csv upload.
-      </Text>
-      <Popover
-        onVisibleChange={() => setOpen(!open)}
-        visible={open}
-        trigger="click"
-        content={
-          <Text style={{display: 'table', whiteSpace: 'normal', width: 200}}>
-            The fields <b>DevEUI, AppEUI, AppKey,</b> and <b>Name</b> are all required. Optionally you can include a <b>LabelID</b> column to add labels to imported devices.
-          </Text>
-        }
+      <img
+        src={ImportIcon}
+        style={{ marginBottom: 10, height: 50, objectFit: "cover" }}
+      />
+      <Title style={{ fontSize: 26 }}>Import Devices</Title>
+      <Text
+        style={{ width: "100%", textAlign: "center", margin: "0px 40px 10px" }}
       >
-        <Text style={{width: '100%', textAlign: 'center', marginBottom: 20, color: 'deepskyblue', textDecoration: 'underline'}}>How do I format my .csv?</Text>
-      </Popover>
-      {
-        (deviceImports && (!deviceImports.entries.length || deviceImports.entries[0].status !== "importing")) && (
+        You can import your devices directly from the Things Network, or in bulk
+        via .csv upload.
+      </Text>
+      <FormatPopover />
+      {deviceImports &&
+        (!deviceImports.entries.length ||
+          deviceImports.entries[0].status !== "importing") && (
           <Fragment>
             <Button
               type="primary"
-              onClick={() => onImportSelect('ttn')}
-              style={{ width: '100%', marginLeft: 60, marginRight: 60 }}
+              onClick={() => onImportSelect("ttn")}
+              style={{ width: "100%", marginLeft: 60, marginRight: 60 }}
             >
               Import from The Things Network
             </Button>
-            <DragAndDrop fileSelected={(file) => {
-              let fileReader = new FileReader();
-              fileReader.onloadend = () => {
-                scanGenericDevices(fileReader.result, (type) => onImportSelect(type));
-              }
-              fileReader.readAsText(file);
-            }}>
-              <Text style={{textAlign: 'center', margin: '30px 80px', color: blueForDeviceStatsLarge}}>
+            <DragAndDrop
+              fileSelected={(file) => {
+                let fileReader = new FileReader();
+                fileReader.onloadend = () => {
+                  scanGenericDevices(fileReader.result, (type) => {
+                    if (type) onImportSelect(type);
+                  });
+                };
+                fileReader.readAsText(file);
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  margin: "30px 80px",
+                  color: blueForDeviceStatsLarge,
+                }}
+              >
                 Drag .csv file here or click to choose file
               </Text>
             </DragAndDrop>
           </Fragment>
-        )
-      }
+        )}
     </Fragment>
-  )
-}
+  );
+};
 
 function mapStateToProps(state) {
   return {
-    genericImportScanFailed: state.devices.genericImportScanFailed
-  }
+    genericImportScanFailed: state.devices.genericImportScanFailed,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    scanGenericDevices
-  }, dispatch);
+  return bindActionCreators(
+    {
+      scanGenericDevices,
+    },
+    dispatch
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChooseImportType);
