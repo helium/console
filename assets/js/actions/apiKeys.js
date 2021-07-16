3a3c375940e41d21005449be27228008c98ed871
Alejandra Buznego <abuznego23@gmail.com>
2021-07-16 15:19:27 -0700

    Add confirmation modal for api key deletion

--- assets/js/actions/apiKeys.js
+++ assets/js/actions/apiKeys.js
@@ -1,19 +1,18 @@
-import * as rest from '../util/rest';
-import sanitizeHtml from 'sanitize-html'
+import * as rest from "../util/rest";
+import sanitizeHtml from "sanitize-html";
 
-export const generateKey = (name, role) => (dispatch) => (
-  rest.post('/api/api_keys', {
-        api_key: {
-          name: sanitizeHtml(name),
-          role
-        }
-      })
-      .then(({ data }) => data)
-)
+export const generateKey = (name, role) => (dispatch) =>
+  rest
+    .post("/api/api_keys", {
+      api_key: {
+        name: sanitizeHtml(name),
+        role,
+      },
+    })
+    .then(({ data }) => data);
 
 export const deleteKey = (id) => {
   return (dispatch) => {
-    rest.destroy(`/api/api_keys/${id}`)
-      .then(response => {})
-  }
-}
+    return rest.destroy(`/api/api_keys/${id}`).then((response) => {});
+  };
+};
