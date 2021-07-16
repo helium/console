ae6bf99e508fdeca462bf2e03afb8e1dd14f1286
Alejandra Buznego <abuznego23@gmail.com>
2021-07-16 13:58:20 -0700

    fix search by removing limit, fix modals

--- assets/js/components/labels/LabelAddDeviceModal.jsx
+++ assets/js/components/labels/LabelAddDeviceModal.jsx
@@ -1,100 +1,102 @@
-import React, { Component } from 'react'
-import withGql from '../../graphql/withGql'
-import omit from 'lodash/omit'
-import analyticsLogger from '../../util/analyticsLogger'
-import { ALL_LABELS_DEVICES } from '../../graphql/labels'
-import { Modal, Button, Typography, Tabs } from 'antd';
-import LabelAddDeviceSelect from './LabelAddDeviceSelect'
-import LabelAddLabelSelect from './LabelAddLabelSelect'
-const { Text } = Typography
-const { TabPane } = Tabs
+import React, { Component } from "react";
+import withGql from "../../graphql/withGql";
+import omit from "lodash/omit";
+import analyticsLogger from "../../util/analyticsLogger";
+import { ALL_LABELS_DEVICES } from "../../graphql/labels";
+import { Modal, Button, Typography, Tabs } from "antd";
+import LabelAddDeviceSelect from "./LabelAddDeviceSelect";
+import LabelAddLabelSelect from "./LabelAddLabelSelect";
+const { Text } = Typography;
+const { TabPane } = Tabs;
 
 class LabelAddDeviceModal extends Component {
   state = {
     checkedDevices: {},
-    checkedLabels: {}
-  }
+    checkedLabels: {},
+  };
 
   handleSubmit = () => {
-    const { checkedDevices, checkedLabels } = this.state
+    const { checkedDevices, checkedLabels } = this.state;
 
-    this.props.addDevicesToLabels(checkedDevices, checkedLabels, this.props.label.id)
-    analyticsLogger.logEvent(
-      "ACTION_ADD_DEVICES_AND_LABELS_TO_LABEL",
-      {
-        id: this.props.label.id,
-        devices: Object.keys(checkedDevices),
-        labels: Object.keys(checkedLabels),
-      }
-    )
-    this.props.onClose()
-  }
+    this.props.addDevicesToLabels(
+      checkedDevices,
+      checkedLabels,
+      this.props.label.id
+    );
+    analyticsLogger.logEvent("ACTION_ADD_DEVICES_AND_LABELS_TO_LABEL", {
+      id: this.props.label.id,
+      devices: Object.keys(checkedDevices),
+      labels: Object.keys(checkedLabels),
+    });
+    this.props.onClose();
+  };
 
   checkAllDevices = (search) => {
-    const { checkedDevices } = this.state
+    const { checkedDevices } = this.state;
     if (Object.keys(checkedDevices).length > 0) {
-      this.setState({ checkedDevices: {} })
+      this.setState({ checkedDevices: {} });
     } else if (search.length > 0) {
-      const devices = {}
-      search.forEach(d => {
-        devices[d.id] = true
-      })
-      this.setState({ checkedDevices: devices })
+      const devices = {};
+      search.forEach((d) => {
+        devices[d.id] = true;
+      });
+      this.setState({ checkedDevices: devices });
     } else {
-      const devices = {}
-      this.props.allResourcesQuery.allDevices.forEach(d => {
-        devices[d.id] = true
-      })
-      this.setState({ checkedDevices: devices })
+      const devices = {};
+      this.props.allResourcesQuery.allDevices.forEach((d) => {
+        devices[d.id] = true;
+      });
+      this.setState({ checkedDevices: devices });
     }
-  }
+  };
 
   checkSingleDevice = (id) => {
-    const { checkedDevices } = this.state
-    let devices
+    const { checkedDevices } = this.state;
+    let devices;
     if (checkedDevices[id]) {
-      devices = omit(checkedDevices, id)
+      devices = omit(checkedDevices, id);
     } else {
-      devices = Object.assign({}, checkedDevices, { [id]: true })
+      devices = Object.assign({}, checkedDevices, { [id]: true });
     }
-    this.setState({ checkedDevices: devices })
-  }
+    this.setState({ checkedDevices: devices });
+  };
 
   checkAllLabels = (search) => {
-    const { checkedLabels } = this.state
+    const { checkedLabels } = this.state;
     if (Object.keys(checkedLabels).length > 0) {
-      this.setState({ checkedLabels: {} })
+      this.setState({ checkedLabels: {} });
     } else if (search.length > 0) {
-      const labels = {}
-      search.forEach(l => {
-        if (this.props.label.id !== l.id) labels[l.id] = true
-      })
-      this.setState({ checkedLabels: labels })
+      const labels = {};
+      search.forEach((l) => {
+        if (this.props.label.id !== l.id) labels[l.id] = true;
+      });
+      this.setState({ checkedLabels: labels });
     } else {
-      const labels = {}
-      this.props.allResourcesQuery.allLabels.forEach(l => {
-        if (this.props.label.id !== l.id) labels[l.id] = true
-      })
-      this.setState({ checkedLabels: labels })
+      const labels = {};
+      this.props.allResourcesQuery.allLabels.forEach((l) => {
+        if (this.props.label.id !== l.id) labels[l.id] = true;
+      });
+      this.setState({ checkedLabels: labels });
     }
-  }
+  };
 
   checkSingleLabel = (id) => {
-    const { checkedLabels } = this.state
-    let labels
+    const { checkedLabels } = this.state;
+    let labels;
 
     if (checkedLabels[id]) {
-      labels = omit(checkedLabels, id)
+      labels = omit(checkedLabels, id);
     } else {
-      labels = Object.assign({}, checkedLabels, { [id]: true })
+      labels = Object.assign({}, checkedLabels, { [id]: true });
     }
-    this.setState({ checkedLabels: labels })
-  }
+    this.setState({ checkedLabels: labels });
+  };
 
   render() {
-    const { open, onClose, label, labelNormalizedDevices } = this.props
-    const { allDevices, allLabels, loading, error } = this.props.allResourcesQuery
-    const { checkedDevices, checkedLabels } = this.state
+    const { open, onClose, label, labelNormalizedDevices } = this.props;
+    const { allDevices, allLabels, loading, error } =
+      this.props.allResourcesQuery;
+    const { checkedDevices, checkedLabels } = this.state;
 
     return (
       <Modal
@@ -109,49 +111,62 @@ class LabelAddDeviceModal extends Component {
           </Button>,
           <Button key="submit" type="primary" onClick={this.handleSubmit}>
             Add Label to Devices
-          </Button>
+          </Button>,
         ]}
         width={620}
       >
-        {
-          !loading && !error && (
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-              <Tabs
-                defaultActiveKey="devices"
-                size="small"
-                onTabClick={tab => this.setState({ tab })}
-                tabPosition="left"
-                style={{ width: '100%', height: 325 }}
-              >
-                <TabPane tab="Devices" key="devices">
-                  <LabelAddDeviceSelect
-                    checkAllDevices={this.checkAllDevices}
-                    allDevices={allDevices}
-                    checkedDevices={checkedDevices}
-                    checkSingleDevice={this.checkSingleDevice}
-                    labelNormalizedDevices={labelNormalizedDevices}
-                  />
-                </TabPane>
-                <TabPane tab="Labels" key="labels">
-                  <Text strong>Choose additional labels to attach to selected devices:</Text>
-                  <LabelAddLabelSelect
-                    checkAllLabels={this.checkAllLabels}
-                    allLabelsWithDevices={allLabels.filter(l => l.device_count && l.device_count > 0)}
-                    checkedLabels={checkedLabels}
-                    checkSingleLabel={this.checkSingleLabel}
-                    currentLabel={label}
-                  />
-                </TabPane>
-              </Tabs>
-            </div>
-          )
-        }
-        {
-          error && <Text>Data failed to load, please reload the page and try again</Text>
-        }
+        {!loading && !error && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Tabs
+              defaultActiveKey="devices"
+              size="small"
+              onTabClick={(tab) => this.setState({ tab })}
+              tabPosition="left"
+              style={{ width: "100%", height: 325 }}
+            >
+              <TabPane tab="Devices" key="devices">
+                <LabelAddDeviceSelect
+                  checkAllDevices={this.checkAllDevices}
+                  allDevices={allDevices}
+                  checkedDevices={checkedDevices}
+                  checkSingleDevice={this.checkSingleDevice}
+                  labelNormalizedDevices={labelNormalizedDevices}
+                />
+              </TabPane>
+              <TabPane tab="Labels" key="labels">
+                <Text strong>
+                  Choose additional labels with devices that you want attached
+                  to the current label, {label.name}:
+                </Text>
+                <LabelAddLabelSelect
+                  checkAllLabels={this.checkAllLabels}
+                  allLabelsWithDevices={allLabels.filter(
+                    (l) => l.device_count && l.device_count > 0
+                  )}
+                  checkedLabels={checkedLabels}
+                  checkSingleLabel={this.checkSingleLabel}
+                  currentLabel={label}
+                />
+              </TabPane>
+            </Tabs>
+          </div>
+        )}
+        {error && (
+          <Text>Data failed to load, please reload the page and try again</Text>
+        )}
       </Modal>
-    )
+    );
   }
 }
 
-export default withGql(LabelAddDeviceModal, ALL_LABELS_DEVICES, props => ({ fetchPolicy: 'network-only', variables: {}, name: 'allResourcesQuery' }))
+export default withGql(LabelAddDeviceModal, ALL_LABELS_DEVICES, (props) => ({
+  fetchPolicy: "network-only",
+  variables: {},
+  name: "allResourcesQuery",
+}));
--- assets/js/components/labels/LabelAddDeviceSelect.jsx
+++ assets/js/components/labels/LabelAddDeviceSelect.jsx
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
-import debounce from 'lodash/debounce'
-import { Checkbox, Input, Card } from 'antd';
-import SearchOutlined from '@ant-design/icons/SearchOutlined';
-import CheckCircleFilled from '@ant-design/icons/CheckCircleFilled';
-import withGql from '../../graphql/withGql'
-import { SEARCH_DEVICES } from '../../graphql/search'
+import React, { Component } from "react";
+import debounce from "lodash/debounce";
+import { Checkbox, Input, Card } from "antd";
+import SearchOutlined from "@ant-design/icons/SearchOutlined";
+import CheckCircleFilled from "@ant-design/icons/CheckCircleFilled";
+import withGql from "../../graphql/withGql";
+import { SEARCH_DEVICES } from "../../graphql/search";
 
 class LabelAddDeviceSelect extends Component {
   state = {
-    searchDevices: []
-  }
+    searchDevices: [],
+  };
 
   runSearch = (value) => {
-    const { loading, refetch } = this.props.searchDevicesQuery
+    const { loading, refetch } = this.props.searchDevicesQuery;
     if (!loading) {
-      refetch({ query: value })
-      .then(({data}) => {
-        const { searchDevices } = data
+      refetch({ query: value }).then(({ data }) => {
+        const { searchDevices } = data;
 
-        this.setState({ searchDevices })
-      })
+        this.setState({ searchDevices });
+      });
     }
-  }
+  };
 
   render() {
     const {
@@ -30,50 +29,67 @@ class LabelAddDeviceSelect extends Component {
       checkedDevices,
       checkSingleDevice,
       labelNormalizedDevices,
-    } = this.props
+    } = this.props;
 
-    const { searchDevices } = this.state
+    const { searchDevices } = this.state;
 
-    const debouncedSearch = debounce(this.runSearch, 300)
+    const debouncedSearch = debounce(this.runSearch, 300);
 
     return (
       <Card
-        title={<Checkbox onChange={e => checkAllDevices(searchDevices)}
-        checked={allDevices.length === Object.keys(checkedDevices).length}>Select All Devices</Checkbox>}
+        title={
+          <Checkbox
+            onChange={(e) => checkAllDevices(searchDevices)}
+            checked={allDevices.length === Object.keys(checkedDevices).length}
+          >
+            Select All Devices
+          </Checkbox>
+        }
         size="small"
-        style={{ height: 300, width: '100%' }}
+        style={{ height: 325, width: "100%" }}
       >
         <Input
           placeholder="Search here"
           suffix={<SearchOutlined />}
-          onChange={e => debouncedSearch(e.target.value)}
-          style={{ width: '100%' }}
+          onChange={(e) => debouncedSearch(e.target.value)}
+          style={{ width: "100%", marginBottom: 5 }}
         />
-        <div style={{ overflowY: 'scroll', height: 202, width: '100%' }}>
-          {
-            searchDevices.length === 0 && allDevices.map(d => (
+        <div style={{ overflowY: "scroll", height: 202, width: "100%" }}>
+          {searchDevices.length === 0 &&
+            allDevices.map((d) => (
               <div style={{ marginTop: 5 }} key={d.id}>
-                <Checkbox onChange={() => checkSingleDevice(d.id)} checked={checkedDevices[d.id]}>{d.name}</Checkbox>
-                {
-                  labelNormalizedDevices[d.id] && <CheckCircleFilled style={{ color: "#4091F7" }} />
-                }
+                <Checkbox
+                  onChange={() => checkSingleDevice(d.id)}
+                  checked={checkedDevices[d.id]}
+                >
+                  {d.name}
+                </Checkbox>
+                {labelNormalizedDevices[d.id] && (
+                  <CheckCircleFilled style={{ color: "#4091F7" }} />
+                )}
               </div>
-            ))
-          }
-          {
-            searchDevices.map(d => (
-              <div style={{ marginTop: 5 }} key={d.id}>
-                <Checkbox onChange={() => checkSingleDevice(d.id)} checked={checkedDevices[d.id]}>{d.name}</Checkbox>
-                {
-                  labelNormalizedDevices[d.id] && <CheckCircleFilled style={{ color: "#4091F7" }} />
-                }
-              </div>
-            ))
-          }
+            ))}
+          {searchDevices.map((d) => (
+            <div style={{ marginTop: 5 }} key={d.id}>
+              <Checkbox
+                onChange={() => checkSingleDevice(d.id)}
+                checked={checkedDevices[d.id]}
+              >
+                {d.name}
+              </Checkbox>
+              {labelNormalizedDevices[d.id] && (
+                <CheckCircleFilled style={{ color: "#4091F7" }} />
+              )}
+            </div>
+          ))}
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default withGql(LabelAddDeviceSelect, SEARCH_DEVICES, props => ({ fetchPolicy: 'network-only', variables: {query:''}, name: 'searchDevicesQuery' }))
+export default withGql(LabelAddDeviceSelect, SEARCH_DEVICES, (props) => ({
+  fetchPolicy: "network-only",
+  variables: { query: "" },
+  name: "searchDevicesQuery",
+}));
