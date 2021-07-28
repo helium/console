a535a31a092fdf442a09112655f4fc085ab2cfe4
Victor Mu <victor@helium.com>
2021-07-27 17:59:44 -0700

    Update quick action disabled color selection (#722)
    * Update quick action disabled color selection
    
    * Fix scrolling pagination selects on labelshowtable

--- assets/js/components/labels/LabelShowTable.jsx
+++ assets/js/components/labels/LabelShowTable.jsx
@@ -411,7 +411,8 @@ class LabelShowTable extends Component {
           pagination={false}
           rowSelection={rowSelection}
           onChange={this.handleSortChange}
-          style={{ minWidth }}
+          style={{ minWidth, overflowX: "scroll", overflowY: "hidden" }}
+          className="no-scroll-bar"
           onRow={(record, rowIndex) => ({
             onClick: (e) => {
               if (e.target.tagName === "TD") {
