6faf3700b1f7817062b2a47fe079a9479ce98cbe
victor <victor@helium.com>
2021-08-09 16:01:57 -0700

    Reduce allowed actions on rate limits

--- lib/console_web/auth_api_pipeline.ex
+++ lib/console_web/auth_api_pipeline.ex
@@ -2,7 +2,7 @@ defmodule ConsoleWeb.AuthApiPipeline do
   use Plug.Builder
 
   plug ConsoleWeb.Plug.CheckDomain
-  plug ConsoleWeb.Plug.RateLimit, ["internal_api_actions", 120]
+  plug ConsoleWeb.Plug.RateLimit, ["internal_api_actions", 90]
   plug ConsoleWeb.Plug.VerifyRemoteIpRange
   plug ConsoleWeb.Plug.VerifyAccessToken
   plug ConsoleWeb.Plug.PutCurrentUser
