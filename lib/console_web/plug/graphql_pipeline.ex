9e7341c53707252e511f2d19577f7f5cf60be82d
victor <victor@helium.com>
2021-08-10 09:32:13 -0700

    Reorder rate limit and check_domain plug

--- lib/console_web/plug/graphql_pipeline.ex
+++ lib/console_web/plug/graphql_pipeline.ex
@@ -1,8 +1,8 @@
 defmodule ConsoleWeb.Plug.GraphqlPipeline do
   use Plug.Builder
 
-  plug ConsoleWeb.Plug.CheckDomain
   plug ConsoleWeb.Plug.RateLimit, ["gql_actions", 90]
+  plug ConsoleWeb.Plug.CheckDomain
   plug ConsoleWeb.Plug.VerifyRemoteIpRange
   plug ConsoleWeb.Plug.VerifyAccessToken
   plug ConsoleWeb.Plug.PutCurrentUser
