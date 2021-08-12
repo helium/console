631e06669f91861bfb0a91f31725126a3295eaa9
Alejandra Buznego <abuznego23@gmail.com>
2021-08-12 16:57:35 -0700

    update nullability on hotspots table

--- config/test.exs
+++ config/test.exs
@@ -34,7 +34,6 @@ config :comeonin, :bcrypt_log_rounds, 4
 config :comeonin, :pbkdf2_rounds, 1
 config :console, oui: 1
 
-
 config :console, :access_token_decoder, Console.AccessTokenDecoder.MockDecodeAccessToken
 
 config :console,
