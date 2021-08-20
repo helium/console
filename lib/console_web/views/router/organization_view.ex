6e9440a01f1727bc9991600b1be4f7d6e4210cb2
Victor Mu <victor@helium.com>
2021-08-20 09:36:54 -0700

    Add index for router org controller (#787)

--- lib/console_web/views/router/organization_view.ex
+++ lib/console_web/views/router/organization_view.ex
@@ -2,6 +2,10 @@ defmodule ConsoleWeb.Router.OrganizationView do
   use ConsoleWeb, :view
   alias ConsoleWeb.Router.OrganizationView
 
+  def render("index.json", %{organizations: organizations}) do
+    render_many(organizations, OrganizationView, "organization.json")
+  end
+
   def render("show.json", %{organization: organization}) do
     render_one(organization, OrganizationView, "organization.json")
   end
@@ -9,6 +13,7 @@ defmodule ConsoleWeb.Router.OrganizationView do
   def render("organization.json", %{organization: organization}) do
     %{
       id: organization.id,
+      name: organization.name,
       dc_balance: organization.dc_balance,
       dc_balance_nonce: organization.dc_balance_nonce
     }
