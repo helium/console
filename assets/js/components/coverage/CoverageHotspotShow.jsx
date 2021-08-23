eb2c48faa688d9c45408a87dd485c8965a130199
victor <victor@helium.com>
2021-08-23 15:00:35 -0700

    Update lazy query failure returns

--- assets/js/components/coverage/CoverageHotspotShow.jsx
+++ assets/js/components/coverage/CoverageHotspotShow.jsx
@@ -23,6 +23,12 @@ export default (props) => {
     })
   }, [props.hotspotAddress]);
 
+  if (error || loading || !data) return (
+    <div style={{ padding: 25, paddingTop: 8 }} />
+  )
+
+  const hotspot = data.hotspot
+
   return (
     <div style={{ padding: 25, paddingTop: 8 }}>
       <Button
@@ -32,72 +38,69 @@ export default (props) => {
       >
         Back
       </Button>
-      {
-        data && data.hotspot && (
-          <Row gutter={12}>
-            <Col sm={20}>
-              <div style={{ marginBottom: 8, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <Text style={{ fontSize: 22, fontWeight: 600, marginRight: 20 }}>{startCase(data.hotspot.hotspot_name)}</Text>
-                {renderStatusLabel(data.hotspot.status)}
-              </div>
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 12 }}>
-                <span style={{ color: '#2C79EE', marginRight: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                  <img
-                    draggable="false"
-                    src={LocationIcon}
-                    style={{ height: 12, marginRight: 4 }}
-                  />
-                  {locationString(data.hotspot.long_city, data.hotspot.short_state, data.hotspot.short_country)}
-                </span>
-                <span style={{ color: '#2C79EE', marginRight: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                  <img
-                    draggable="false"
-                    src={AliasIcon}
-                    style={{ height: 12, marginRight: 4 }}
-                  />
-                  Alias
-                </span>
-              </div>
-              <div style={{ marginBottom: 8 }}>
-                <Text style={{ fontSize: 15 }}>
-                  This Hotspot has heard from of your devices over the past 30 days.
+
+      <Row gutter={12}>
+        <Col sm={20}>
+          <div style={{ marginBottom: 8, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <Text style={{ fontSize: 22, fontWeight: 600, marginRight: 20 }}>{startCase(hotspot.hotspot_name)}</Text>
+            {renderStatusLabel(hotspot.status)}
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 12 }}>
+            <span style={{ color: '#2C79EE', marginRight: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+              <img
+                draggable="false"
+                src={LocationIcon}
+                style={{ height: 12, marginRight: 4 }}
+              />
+              {locationString(hotspot.long_city, hotspot.short_state, hotspot.short_country)}
+            </span>
+            <span style={{ color: '#2C79EE', marginRight: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+              <img
+                draggable="false"
+                src={AliasIcon}
+                style={{ height: 12, marginRight: 4 }}
+              />
+              Alias
+            </span>
+          </div>
+          <div style={{ marginBottom: 8 }}>
+            <Text style={{ fontSize: 15 }}>
+              This Hotspot has heard from of your devices over the past 30 days.
+            </Text>
+          </div>
+        </Col>
+        <Col sm={4}>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Link
+                to="#"
+                onClick={() =>
+                  updateOrganizationHotspot(
+                    hotspot.hotspot_address,
+                    !props.orgHotspotsMap[hotspot.hotspot_address]
+                  )
+                }
+              >
+                <img
+                  draggable="false"
+                  src={props.orgHotspotsMap[hotspot.hotspot_address] ? SelectedFlag : UnselectedFlag}
+                  style={{
+                    height: 20,
+                    marginBottom: 8
+                  }}
+                />
+              </Link>
+              <div style={{ backgroundColor: '#F5F7F9', borderRadius: 10, padding: '4px 8px 4px 8px' }}>
+                <Text
+                  style={{ fontSize: 16, color: '#2C79EE'}}
+                >
+                  {props.orgHotspotsMap[hotspot.hotspot_address] ? "Claimed!" : "Unclaimed"}
                 </Text>
               </div>
-            </Col>
-            <Col sm={4}>
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <Link
-                    to="#"
-                    onClick={() =>
-                      updateOrganizationHotspot(
-                        data.hotspot.hotspot_address,
-                        !props.orgHotspotsMap[data.hotspot.hotspot_address]
-                      )
-                    }
-                  >
-                    <img
-                      draggable="false"
-                      src={props.orgHotspotsMap[data.hotspot.hotspot_address] ? SelectedFlag : UnselectedFlag}
-                      style={{
-                        height: 20,
-                        marginBottom: 8
-                      }}
-                    />
-                  </Link>
-                  <div style={{ backgroundColor: '#F5F7F9', borderRadius: 10, padding: '4px 8px 4px 8px' }}>
-                    <Text
-                      style={{ fontSize: 16, color: '#2C79EE'}}
-                    >
-                      {props.orgHotspotsMap[data.hotspot.hotspot_address] ? "Claimed!" : "Unclaimed"}
-                    </Text>
-                  </div>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        )
-      }
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
