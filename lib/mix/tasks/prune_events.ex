fb851b11a62575168a8dd588b29c3e82deddfa86
Victor Mu <victor@helium.com>
2021-09-10 12:59:24 -0700

    Update prune mix task to include hotspot_stats (#845)

--- lib/mix/tasks/prune_events.ex
+++ lib/mix/tasks/prune_events.ex
@@ -2,9 +2,10 @@ defmodule Mix.Tasks.PruneEvents do
   use Mix.Task
 
   def run(db) do
-    IO.inspect "Pruned events and device_stats from db"
+    IO.inspect "Pruned events, device_stats, hotspot_stats from db"
     Mix.shell.cmd("psql -a #{db} -c 'select prune_events()'")
     Mix.shell.cmd("psql -a #{db} -c 'select prune_device_stats()'")
+    Mix.shell.cmd("psql -a #{db} -c 'select prune_hotspot_stats()'")
   end
 end
 
@@ -21,5 +22,13 @@ end
 # BEGIN
 #   EXECUTE format($f$delete from device_stats where inserted_at < NOW() - INTERVAL '30 days'$f$);
 #   RAISE NOTICE 'Pruned device stats table';
+# END;
+# $$ LANGUAGE plpgsql;
+
+# function created in psql
+# CREATE OR REPLACE FUNCTION prune_hotspot_stats() RETURNS void AS $$
+# BEGIN
+#   EXECUTE format($f$delete from hotspot_stats where inserted_at < NOW() - INTERVAL '7 days'$f$);
+#   RAISE NOTICE 'Pruned hotspot stats table';
 # END;
 # $$ LANGUAGE plpgsql;
