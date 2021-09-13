ae9cb8ae04129de68d710808214a2184b2860179
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2021-09-13 14:46:13 -0700

    fix orgId bug (#847)

--- assets/js/util/rest.js
+++ assets/js/util/rest.js
@@ -1,72 +1,74 @@
-import { store } from '../store/configureStore';
-import axios from '../config/axios.js'
-import { displayError } from './messages'
-import { getIdTokenClaims, logout } from '../components/auth/Auth0Provider'
+import { store } from "../store/configureStore";
+import axios from "../config/axios.js";
+import { displayError } from "./messages";
+import { getIdTokenClaims, logout } from "../components/auth/Auth0Provider";
 
 export const get = async (path, params = {}, extraHeaders = {}) => {
   return axios({
     url: path,
-    method: 'get',
+    method: "get",
     headers: Object.assign(await headers(), extraHeaders),
     params,
-  })
-}
+  });
+};
 
 export const post = async (path, data) => {
   return axios({
     url: path,
-    method: 'post',
+    method: "post",
     headers: await headers(),
-    data
-  })
-}
+    data,
+  });
+};
 
 export const put = async (path, data) => {
   return axios({
     url: path,
-    method: 'put',
+    method: "put",
     headers: await headers(),
-    data
-  })
-}
+    data,
+  });
+};
 
 export const destroy = async (path) => {
   return axios({
     url: path,
-    method: 'delete',
-    headers: await headers()
-  })
-}
+    method: "delete",
+    headers: await headers(),
+  });
+};
 
 const headers = async () => {
   let headerParams = {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   };
-  let organizationId = store.getState().organization && store.getState().organization.currentOrganizationId
+  let organizationId =
+    store.getState().organization &&
+    store.getState().organization.currentOrganizationId;
 
   if (!organizationId) {
     try {
-      organizationId = JSON.parse(localStorage.getItem('organization')).id;
-    } catch (e) {
-      return displayError()
-    }
+      organizationId =
+        localStorage.getItem("organization") &&
+        JSON.parse(localStorage.getItem("organization")).id;
+    } catch (e) {}
   }
 
   if (organizationId) {
-    Object.assign(headerParams, { organization: organizationId })
+    Object.assign(headerParams, { organization: organizationId });
   }
 
-  let tokenClaims = store.getState().apollo.tokenClaims
+  let tokenClaims = store.getState().apollo.tokenClaims;
   if (!tokenClaims) {
     tokenClaims = await getIdTokenClaims();
   } else if (Math.ceil(Date.now() / 1000) > tokenClaims.exp) {
-    return logout()
+    return logout();
   }
 
-  const apikey = tokenClaims.__raw
+  const apikey = tokenClaims.__raw;
   Object.assign(headerParams, {
-    'Authorization': `Bearer ${apikey}`
-  })
+    Authorization: `Bearer ${apikey}`,
+  });
 
   return headerParams;
-}
+};
