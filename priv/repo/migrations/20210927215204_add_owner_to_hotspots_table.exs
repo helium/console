04cc829e528459390448fa7534b3521faf7b5de8
Alejandra Buznego <abuznego23@gmail.com>
2021-09-27 16:08:59 -0700

    remove update of status column, unnecessary

--- priv/repo/migrations/20210927215204_add_owner_to_hotspots_table.exs
+++ priv/repo/migrations/20210927215204_add_owner_to_hotspots_table.exs
@@ -1,17 +1,9 @@
 defmodule Console.Repo.Migrations.AddOwnerToHotspotsTable do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:hotspots) do
       add :owner, :string, null: true
-      modify :status, :string, null: true
-    end
-  end
-
-  def down do
-    alter table(:hotspots) do
-      remove :owner
-      modify :status, :string, null: false
     end
   end
 end
