80a3c3bef9becac4cd5cb2739c52f542c3a8ab7b
victor <victor@helium.com>
2021-10-04 15:51:17 -0700

    Cut event request body if too long

--- priv/repo/migrations/20211004213534_update_indices.exs
+++ priv/repo/migrations/20211004213534_update_indices.exs
@@ -3,6 +3,6 @@ defmodule Console.Repo.Migrations.UpdateIndices do
 
   def change do
     create index(:hotspot_stats, [:organization_id])
-    drop index(:events, [:reported_at_naive])
+    drop_if_exists index(:events, [:reported_at_naive])
   end
 end
