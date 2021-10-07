3963b8ff88282a4b8462479fb3c4b1d9ca9ac9dc
victor <victor@helium.com>
2021-10-07 15:00:56 -0700

    validate magic token

--- lib/console_web/controllers/session_controller.ex
+++ lib/console_web/controllers/session_controller.ex
@@ -3,37 +3,37 @@ defmodule ConsoleWeb.SessionController do
   import ConsoleWeb.AuthErrorHandler
 
   def create(conn, _) do
-    token =
+    did_token =
       conn
       |> get_req_header("authorization")
       |> List.first()
       |> String.replace("Bearer ", "")
 
+    with true <- Magic.Token.validate!(did_token) do
+      issuer = Magic.Token.get_issuer(did_token)
+      header = ["X-Magic-Secret-Key": Application.get_env(:console, :magic_secret_key)]
+      response =
+        "https://api.magic.link/v1/admin/auth/user/get?issuer=#{issuer}"
+          |> URI.encode()
+          |> HTTPoison.get!(header)
+          |> Map.get(:body)
+          |> Poison.decode!()
 
-    # with <- validate its valid with bones encode if not valid, use auth handler
-
-    header = ["X-Magic-Secret-Key": Application.get_env(:console, :magic_secret_key)]
-    response =
-      "https://api.magic.link/v1/admin/auth/user/get?issuer=#{"did:ethr:0x062668F47f9DF2AA701556C3cE97812De286Adf6"}"
-        |> URI.encode()
-        |> HTTPoison.get!(header)
-        |> Map.get(:body)
-        |> Poison.decode!()
-
-    with %{ "status" => "ok", "data" => user_metadata } <- response do
-      with {:ok, token, claims} <-
-        ConsoleWeb.Guardian.encode_and_sign(
-          %{ id: user_metadata["issuer"] },
-          %{
-            email: user_metadata["email"],
-            typ: "magic-auth-session",
-          },
-          ttl: {1, :day}
-        )
-      do
-        conn
-        |> put_status(:created)
-        |> render("show.json", jwt: token, claims: claims)
+      with %{ "status" => "ok", "data" => user_metadata } <- response do
+        with {:ok, token, claims} <-
+          ConsoleWeb.Guardian.encode_and_sign(
+            %{ id: user_metadata["issuer"] },
+            %{
+              email: user_metadata["email"],
+              typ: "magic-auth-session",
+            },
+            ttl: {1, :day}
+          )
+        do
+          conn
+          |> put_status(:created)
+          |> render("show.json", jwt: token, claims: claims)
+        end
       end
     end
   end
