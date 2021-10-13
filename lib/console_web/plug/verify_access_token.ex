0780ca7cacf6e580f57a0657d2c22ec80c7e4baa
victor <victor@helium.com>
2021-10-13 12:22:00 -0700

    Fix issue with auth header being nil

--- lib/console_web/plug/verify_access_token.ex
+++ lib/console_web/plug/verify_access_token.ex
@@ -47,8 +47,10 @@ defmodule ConsoleWeb.Plug.VerifyAccessToken do
   def init(default), do: default
 
   def call(conn, _default) do
-    case conn |> get_req_header("authorization") do
-      nil ->
+    auth_header = conn |> get_req_header("authorization")
+
+    cond do
+      auth_header != nil and List.first(auth_header) != nil ->
         conn
           |> send_resp(
             :forbidden,
@@ -58,7 +60,7 @@ defmodule ConsoleWeb.Plug.VerifyAccessToken do
             })
           )
           |> halt()
-      _ ->
+      true ->
         if Application.get_env(:console, :use_magic_auth) do
           token =
             conn
@@ -83,33 +85,27 @@ defmodule ConsoleWeb.Plug.VerifyAccessToken do
               |> halt()
           end
         else
-          try do
-            token = conn
-            |> get_req_header("authorization")
-            |> List.first()
-            |> String.replace("Bearer ", "")
+          token = conn
+          |> get_req_header("authorization")
+          |> List.first()
+          |> String.replace("Bearer ", "")
 
-            case @access_token_decoder.decode_conn_access_token(token) do
-              %{user_id: user_id, email: email, auth0_id: auth0_id} ->
-                conn
-                  |> assign(:user_id, user_id)
-                  |> assign(:email, email)
-                  |> assign(:auth0_id, auth0_id)
-              :error ->
-                conn
-                |> send_resp(
-                  :forbidden,
-                  Poison.encode!(%{
-                    type: "forbidden",
-                    errors: ["Could not validate your credentials"]
-                  })
-                )
-                |> halt()
-            end
-          rescue
-            _ ->
-              auth_header = conn |> get_req_header("authorization") |> List.first()
-              Appsignal.send_error(%RuntimeError{ message: auth_header }, "Failed to verify access token", ["plug/verify_access_token.ex/call"])
+          case @access_token_decoder.decode_conn_access_token(token) do
+            %{user_id: user_id, email: email, auth0_id: auth0_id} ->
+              conn
+                |> assign(:user_id, user_id)
+                |> assign(:email, email)
+                |> assign(:auth0_id, auth0_id)
+            :error ->
+              conn
+              |> send_resp(
+                :forbidden,
+                Poison.encode!(%{
+                  type: "forbidden",
+                  errors: ["Could not validate your credentials"]
+                })
+              )
+              |> halt()
           end
         end
     end
