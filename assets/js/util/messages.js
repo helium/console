d10b0005e3a58c060c7d0bec7ffa1b2a6b816439
Alejandra Buznego <abuznego23@gmail.com>
2021-10-27 12:57:57 -0700

    fix typo

--- assets/js/util/messages.js
+++ assets/js/util/messages.js
@@ -1,20 +1,24 @@
-import { message } from 'antd'
+import { message } from "antd";
 
-const timeouts = {}
+const timeouts = {};
 
 const debounceMessage = (key, messageCallback) => {
-  if (timeouts[key]) return
-  timeouts[key] = setTimeout(() => { delete timeouts[key] }, 300)
-  messageCallback()
-}
+  if (timeouts[key]) return;
+  timeouts[key] = setTimeout(() => {
+    delete timeouts[key];
+  }, 300);
+  messageCallback();
+};
 
 export const displayInfo = (msg) => {
   if (msg) {
-    debounceMessage(msg, () => message.success(msg))
+    debounceMessage(msg, () => message.success(msg));
   }
-}
+};
 
 export const displayError = (errorMsg) => {
-  const msg = errorMsg || "An unexpected error has occurred, please refesh the page and try again"
-  debounceMessage(msg, () => message.error(msg))
-}
+  const msg =
+    errorMsg ||
+    "An unexpected error has occurred, please refresh the page and try again";
+  debounceMessage(msg, () => message.error(msg));
+};
