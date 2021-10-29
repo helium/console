7483125f3af0925bff47fd30d9dc34ee795f3a38
victor <victor@helium.com>
2021-10-29 09:59:47 -0700

    Remove mfa on profile page with magic auth

--- assets/js/config/magic.js
+++ assets/js/config/magic.js
@@ -1,3 +1,3 @@
 export const config = {
-  useMagicAuth: window.use_magic_auth || process.env.USE_MAGIC_AUTH || process.env.NODE_ENV === 'development',
+  useMagicAuth: window.use_magic_auth === 'true' || process.env.USE_MAGIC_AUTH || process.env.NODE_ENV === 'development',
 }
