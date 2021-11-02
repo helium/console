1d8d8d8c8f4800a2375aeefbcd32ff59f3b3248d
Victor Mu <victor@helium.com>
2021-11-02 14:20:20 -0700

    Add check to prevent double counting with slow event runs (#915)

--- lib/console_web/channels/monitor.ex
+++ lib/console_web/channels/monitor.ex
@@ -4,10 +4,18 @@ defmodule ConsoleWeb.Monitor do
   end
 
   def get_router_address do
-    Agent.get(__MODULE__, fn state -> state end)
+    Agent.get(__MODULE__, fn state -> Map.get(state, :address, "") end)
   end
 
   def update_router_address(address) do
-    Agent.update(__MODULE__, fn _ -> address end)
+    Agent.update(__MODULE__, fn state -> Map.put(state, :address, address) end)
+  end
+
+  def get_event_stat_running?() do
+    Agent.get(__MODULE__, fn state -> Map.get(state, :running_stats, false) end)
+  end
+
+  def set_event_stat_running(status) do
+    Agent.update(__MODULE__, fn state -> Map.put(state, :running_stats, status) end)
   end
 end
