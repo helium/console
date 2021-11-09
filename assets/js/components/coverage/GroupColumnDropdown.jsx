42a3fe54c396a90838c2088a97153f8dcba2cee3
Alejandra Buznego <abuznego23@gmail.com>
2021-11-09 12:51:29 -0800

    limit height of menu

--- assets/js/components/coverage/GroupColumnDropdown.jsx
+++ assets/js/components/coverage/GroupColumnDropdown.jsx
@@ -64,7 +64,7 @@ export default ({ appliedGroups, hotspotId }) => {
   };
 
   const content = (
-    <div>
+    <div style={{ maxHeight: 300, overflowY: "scroll" }}>
       {groups.map((group) => {
         const isGroupSelected = !!(appliedGroups || []).find(
           (groupId) => groupId === group.id
