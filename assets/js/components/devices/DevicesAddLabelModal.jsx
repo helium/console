33950fe177d3707577a5590243c372d6a8575f73
victor <victor@helium.com>
2021-12-02 10:07:13 -0800

    Add label conflict checking

--- assets/js/components/devices/DevicesAddLabelModal.jsx
+++ assets/js/components/devices/DevicesAddLabelModal.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import withGql from "../../graphql/withGql";
+import flatten from "lodash/flatten"
 import analyticsLogger from "../../util/analyticsLogger";
 import { ALL_LABELS } from "../../graphql/labels";
 import { grayForModalCaptions } from "../../util/colors";
@@ -16,6 +17,7 @@ class DevicesAddLabelModal extends Component {
     labelId: undefined,
     labelName: undefined,
     applyToAll: false,
+    showLabelConfigProfileConflicts: false,
   };
 
   handleInputUpdate = (e) => {
@@ -31,15 +33,32 @@ class DevicesAddLabelModal extends Component {
       : this.props.devicesToUpdate.map((d) => d.id);
 
     if (labelId) {
-      this.props.addDevicesToLabel(!applyToAll && deviceIds, labelId)
-      .then((response) => {
-        if (response.status === 204) {
-          analyticsLogger.logEvent("ACTION_ADD_LABEL_TO_DEVICES", {
-            devices: applyToAll ? "all" : deviceIds,
-            label: labelId,
-          });
-        }
-      });
+      const labelToApply = this.props.allLabelsQuery.allLabels.find(
+        (l) => l.id === labelId
+      );
+
+      const devicesLabelsHaveConflicts =
+        flatten(this.props.devicesToUpdate.map(d => d.labels))
+        .find(l => l.config_profile_id !== null && l.config_profile_id !== labelToApply.config_profile_id)
+
+      if (devicesLabelsHaveConflicts) {
+        this.setState({ showLabelConfigProfileConflicts: true })
+      } else {
+        this.props.addDevicesToLabel(!applyToAll && deviceIds, labelId)
+        .then((response) => {
+          if (response.status === 204) {
+            analyticsLogger.logEvent("ACTION_ADD_LABEL_TO_DEVICES", {
+              devices: applyToAll ? "all" : deviceIds,
+              label: labelId,
+            });
+            // const devicesHaveConflictingProfiles =
+            //   this.props.devicesToUpdate.find(d => d.config_profile_id !== null && d.config_profile_id !== labelToApply.config_profile_id)
+            // Check if user has device specific conflicts and new modal
+          }
+        });
+        this.setState({ applyToAll: false });
+        this.props.onClose();
+      }
     } else if (labelName) {
       this.props
         .addDevicesToNewLabel(!applyToAll && deviceIds, labelName)
@@ -51,11 +70,33 @@ class DevicesAddLabelModal extends Component {
             });
           }
         });
+      this.setState({ applyToAll: false });
+      this.props.onClose();
     }
-    this.setState({ applyToAll: false });
-
-    this.props.onClose();
   };
+
+  handleConflictConfirm = (e) => {
+    e.preventDefault();
+    const { labelName, labelId, applyToAll } = this.state;
+    const deviceIds = applyToAll
+      ? []
+      : this.props.devicesToUpdate.map((d) => d.id);
+
+    this.props.addDevicesToLabel(!applyToAll && deviceIds, labelId)
+    .then((response) => {
+      if (response.status === 204) {
+        analyticsLogger.logEvent("ACTION_ADD_LABEL_TO_DEVICES", {
+          devices: applyToAll ? "all" : deviceIds,
+          label: labelId,
+        });
+        // const devicesHaveConflictingProfiles =
+        //   this.props.devicesToUpdate.find(d => d.config_profile_id !== null && d.config_profile_id !== labelToApply.config_profile_id)
+        // Check if user has device specific conflicts and new modal
+      }
+    });
+    this.setState({ applyToAll: false });
+    this.props.onClose();
+  }
 
   handleSelectOption = (labelId) => {
     this.setState({ labelId, labelName: null });
@@ -65,78 +106,115 @@ class DevicesAddLabelModal extends Component {
     const { open, onClose, devicesToUpdate, allDevicesSelected, totalDevices } =
       this.props;
     const { error, allLabels } = this.props.allLabelsQuery;
-    const { labelName, labelId } = this.state;
+    const { labelName, labelId, showLabelConfigProfileConflicts } = this.state;
 
     return (
       <>
-        <Modal
-          title={`Add Label to ${
-            devicesToUpdate ? devicesToUpdate.length : 0
-          } Device${devicesToUpdate && devicesToUpdate.length > 1 ? "s" : ""}`}
-          visible={open}
-          centered
-          onCancel={onClose}
-          onOk={this.handleSubmit}
-          footer={[
-            <Button key="back" onClick={onClose}>
-              Cancel
-            </Button>,
-            <Button
-              key="submit"
-              onClick={this.handleSubmit}
-              enabled={
-                devicesToUpdate &&
-                devicesToUpdate.length !== 0 &&
-                (labelName || labelId)
-              }
-              type="primary"
+        {
+          showLabelConfigProfileConflicts ? (
+            <Modal
+              title="Do you want the new label & config profile to apply to these devices?"
+              visible={open}
+              centered
+              onCancel={() => {
+                this.setState({ showLabelConfigProfileConflicts: false })
+                onClose()
+              }}
+              onOk={this.handleConflictConfirm}
+              footer={[
+                <Button
+                  key="back"
+                  onClick={() => {
+                    this.setState({ showLabelConfigProfileConflicts: false })
+                    onClose()
+                  }}
+                >
+                  Cancel
+                </Button>,
+                <Button
+                  key="submit"
+                  onClick={this.handleConflictConfirm}
+                  type="primary"
+                >
+                  Confirm
+                </Button>,
+              ]}
             >
-              Add Label
-            </Button>,
-          ]}
-        >
-          <div>
-            <Select
-              placeholder={error ? "No Labels found..." : "Choose Label"}
-              style={{ width: 270, marginRight: 10 }}
-              onSelect={this.handleSelectOption}
-              value={labelId}
+              <Text style={{ display: 'block', marginBottom: 8 }}>The label you are trying to apply has a configuration profile that is different from labels previously attached to these devices.</Text>
+              <Text style={{ display: 'block', marginBottom: 8 }}><Text strong>Confirm</Text> - These devices will follow the configuration profile settings of this new label.</Text>
+              <Text style={{ display: 'block', marginBottom: 8 }}><Text strong>Cancel</Text> - These devices will retain configuration profile settings of preexisting labels. This new label will not be applied to these devices.</Text>
+            </Modal>
+          ) : (
+            <Modal
+              title={`Add Label to ${
+                devicesToUpdate ? devicesToUpdate.length : 0
+              } Device${devicesToUpdate && devicesToUpdate.length > 1 ? "s" : ""}`}
+              visible={open}
+              centered
+              onCancel={onClose}
+              onOk={this.handleSubmit}
+              footer={[
+                <Button key="back" onClick={onClose}>
+                  Cancel
+                </Button>,
+                <Button
+                  key="submit"
+                  onClick={this.handleSubmit}
+                  enabled={
+                    devicesToUpdate &&
+                    devicesToUpdate.length !== 0 &&
+                    (labelName || labelId)
+                  }
+                  type="primary"
+                >
+                  Add Label
+                </Button>,
+              ]}
             >
-              {allLabels &&
-                allLabels.map((l) => (
-                  <Option value={l.id} key={l.id}>
-                    <LabelTag text={l.name} />
-                  </Option>
-                ))}
-            </Select>
-          </div>
+              <div>
+                <Select
+                  placeholder={error ? "No Labels found..." : "Choose Label"}
+                  style={{ width: 270, marginRight: 10 }}
+                  onSelect={this.handleSelectOption}
+                  value={labelId}
+                >
+                  {allLabels &&
+                    allLabels.map((l) => (
+                      <Option value={l.id} key={l.id}>
+                        <LabelTag text={l.name} />
+                      </Option>
+                    ))}
+                </Select>
+              </div>
 
-          <div style={{ marginTop: 15, marginBottom: 15 }}>
-            <Text style={{ color: grayForModalCaptions }}>or</Text>
-          </div>
+              <div style={{ marginTop: 15, marginBottom: 15 }}>
+                <Text style={{ color: grayForModalCaptions }}>or</Text>
+              </div>
 
-          <div>
-            <Text>Add Label</Text>
-            <Input
-              placeholder="Enter Label Name"
-              name="labelName"
-              value={this.state.labelName}
-              onChange={this.handleInputUpdate}
-            />
-            <Text style={{ marginBottom: 20, color: grayForModalCaptions }}>
-              Label names must be unique
-            </Text>
-          </div>
-          {allDevicesSelected && (
-            <Checkbox
-              style={{ marginTop: 20 }}
-              checked={this.state.applyToAll}
-              onChange={(e) => this.setState({ applyToAll: e.target.checked })}
-            >
-              {`Apply to all ${totalDevices} devices?`}
-            </Checkbox>
-          )}
-        </Modal>
+              <div>
+                <Text>Add Label</Text>
+                <Input
+                  placeholder="Enter Label Name"
+                  name="labelName"
+                  value={this.state.labelName}
+                  onChange={this.handleInputUpdate}
+                />
+                <Text style={{ marginBottom: 20, color: grayForModalCaptions }}>
+                  Label names must be unique
+                </Text>
+              </div>
+              {allDevicesSelected && (
+                <Checkbox
+                  style={{ marginTop: 20 }}
+                  checked={this.state.applyToAll}
+                  onChange={(e) => this.setState({ applyToAll: e.target.checked })}
+                >
+                  {`Apply to all ${totalDevices} devices?`}
+                </Checkbox>
+              )}
+            </Modal>
+          )
+        }
       </>
     );
   }
