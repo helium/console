555bf834b34d1796bf781dd1b7aee2711d211cc6
victor <victor@helium.com>
2021-12-09 09:37:44 -0800

    Add common message for moobile unavailable

--- assets/js/components/mobile/MediaQuery.jsx
+++ assets/js/components/mobile/MediaQuery.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import MediaQuery from 'react-responsive'
+import { Typography } from 'antd';
+const { Text } = Typography
 
 export const MobileDisplay = (props) => {
   if (props.children) {
     return (
-      <MediaQuery maxWidth={600}>
+      <MediaQuery maxWidth={719}>
         {props.children}
       </MediaQuery>
     )
   } else {
     return (
-      <MediaQuery maxWidth={600}>
-        This page is not available in mobile mode. Please switch to desktop mode.
+      <MediaQuery maxWidth={719}>
+        <div style={{ backgroundColor: "#F5F7F9", height: '100%', width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', padding: 15, textAlign: 'center' }}>
+          <Text strong style={{ fontSize: 18 }}>This page is currently unavailable in Mobile View. Please switch to Desktop View to access this page.</Text>
+        </div>
       </MediaQuery>
     )
   }
@@ -19,7 +23,7 @@ export const MobileDisplay = (props) => {
 
 export const DesktopDisplay = (props) => {
   return (
-    <MediaQuery minWidth={601}>
+    <MediaQuery minWidth={720}>
       {props.children}
     </MediaQuery>
   )
