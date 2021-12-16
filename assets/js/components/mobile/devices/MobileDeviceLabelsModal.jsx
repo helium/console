90ece0ee5187274f71accb631196f9d48746d4fc
victor <victor@helium.com>
2021-12-16 12:56:55 -0800

    stuff

--- assets/js/components/mobile/devices/MobileDeviceLabelsModal.jsx
+++ assets/js/components/mobile/devices/MobileDeviceLabelsModal.jsx
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { useQuery } from "@apollo/client";
+import { useDispatch } from "react-redux";
 import find from 'lodash/find'
+import { removeLabelsFromDevice } from "../../../actions/label";
+import LabelsBlueIcon from "../../../../img/mobile/label-node-icon-blue.svg";
+import LabelCheckedIcon from "../../../../img/mobile/label-row-checked.svg";
+import MenuCaret from "../../../../img/channels/mobile/menu-caret.svg";
 import { ALL_LABELS } from "../../../graphql/labels";
 import MobileTableRow from '../../common/MobileTableRow'
 import analyticsLogger from '../../../util/analyticsLogger'
@@ -8,42 +13,73 @@ import { Modal, Button, Typography } from 'antd';
 const { Text } = Typography
 
 export default ({ open, onClose, device }) => {
+  const [showLabelAdd, setShowLabelAdd] = useState(false)
+
+  const dispatch = useDispatch()
+
   const { loading, error, data } = useQuery(ALL_LABELS, {
     fetchPolicy: "cache-and-network",
     variables: {},
   });
 
+  const closeModal = () => {
+    setShowLabelAdd(false)
+    onClose()
+  }
+
   return (
     <Modal
       title="Device Labels"
       visible={open}
-      onCancel={onClose}
+      onCancel={closeModal}
       centered
       footer={[
-        <Button key="back" onClick={onClose}>
+        <Button key="back" onClick={closeModal}>
           Cancel
         </Button>
       ]}
       bodyStyle={{ padding: 0 }}
     >
       {
-        device.labels.map((label, index) => {
-          let subText = ""
-          if (data && data.allLabels.length > 0) {
-
-          }
-          return (
+        !showLabelAdd && (
+          <div>
+            {
+              device.labels.map((label, index) => {
+                let subText = ""
+                if (error) subText = "Could not load device count"
+                if (data && data.allLabels.length > 0) {
+                  const foundLabel = find(data.allLabels, { id: label.id })
+                  if (foundLabel) subText = `${foundLabel.device_count} Device${foundLabel.device_count === 1 ? "" : "s"}`
+                }
+                return (
+                  <MobileTableRow
+                    key={label.id}
+                    mainTitle={<span><img src={LabelsBlueIcon} style={{ height: 14, marginRight: 8 }} />{label.name}</span>}
+                    subtext={subText}
+                    rightAction={
+                      <img
+                        src={LabelCheckedIcon}
+                        style={{ height: 20, cursor: 'pointer' }}
+                        onClick={() => dispatch(removeLabelsFromDevice([label], device.id))}
+                      />
+                    }
+                    onClick={() => {}}
+                    borderTop={index === 0}
+                    borderBottom={true}
+                  />
+                )
+              })
+            }
             <MobileTableRow
-              key={label.id}
-              mainTitle={label.name}
-              subtext={"hkjgal"}
-              rightAction={<span />}
-              onClick={() => {}}
-              borderTop={index === 0}
+              mainTitle={<span><img src={LabelsBlueIcon} style={{ height: 14, position: 'relative', top: -2, marginRight: 8 }} />Add to Label</span>}
+              subtext={""}
+              rightAction={<img src={MenuCaret} style={{ height: 14, marginRight: 8}} />}
+              onClick={() => setShowLabelAdd(true)}
+              borderTop={device.labels.length === 0}
               borderBottom={true}
             />
-          )
-        })
+          </div>
+        )
       }
     </Modal>
   )
