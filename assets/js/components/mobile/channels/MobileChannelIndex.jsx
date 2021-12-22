7d5360312a1f52eb665fef677f5408b0ae3bb62e
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2021-12-22 14:30:41 -0800

    Search (Mobile) (#979)

--- assets/js/components/mobile/channels/MobileChannelIndex.jsx
+++ assets/js/components/mobile/channels/MobileChannelIndex.jsx
@@ -1,42 +1,106 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { SkeletonLayout } from "../../common/SkeletonLayout";
-import { Typography } from "antd";
+import { Typography, Input } from "antd";
 import MobileTableRow from "../../common/MobileTableRow";
-import useElementOnScreen from "../ElementOnScreen.jsx"
+import useElementOnScreen from "../ElementOnScreen.jsx";
 const { Text } = Typography;
 import MenuCaret from "../../../../img/menu-caret.svg";
 import MobileAddResourceButton from "../../common/MobileAddResourceButton";
 import { useHistory } from "react-router-dom";
+import SearchOutlined from "@ant-design/icons/SearchOutlined";
+import { SEARCH_CHANNELS_MOBILE } from "../../../graphql/search";
+import { useLazyQuery } from "@apollo/client";
+import debounce from "lodash/debounce";
+import ErrorMessage from "../../common/ErrorMessage";
 
 export default ({ loading, channels, refetch }) => {
   const history = useHistory();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [
+    searchChannelsMobile,
+    {
+      loading: searchChannelsMobileLoading,
+      error: searchChannelsMobileError,
+      data: searchChannelsMobileData,
+    },
+  ] = useLazyQuery(SEARCH_CHANNELS_MOBILE, {
+    fetchPolicy: "cache-and-network",
+  });
 
-  const [ containerRef ] = useElementOnScreen({
-    root: null,
-    rootMargin: "0px",
-    threshold: 1.0
-  }, refetch, channels)
+  const searchChannels = searchChannelsMobileData
+    ? searchChannelsMobileData.searchChannelsMobile
+    : [];
+
+  const runSearch = () => {
+    if (!searchChannelsMobileLoading) {
+      searchChannelsMobile({
+        variables: { query: searchTerm },
+      });
+    }
+  };
+
+  const debouncedSearch = useCallback(
+    debounce(() => {
+      runSearch();
+    }, 800)
+  );
+
+  const [containerRef] = useElementOnScreen(
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    },
+    refetch,
+    channels
+  );
+
+  useEffect(() => {
+    debouncedSearch();
+  }, [searchTerm]);
 
   return (
     <div>
       <div style={{ padding: 15 }}>
         <Text style={{ fontSize: 32, fontWeight: 600 }}>Integrations</Text>
+        <Input
+          name="searchTerm"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          placeholder="Search Integrations..."
+          prefix={<SearchOutlined style={{ color: "lightgray" }} />}
+          maxLength={50}
+          style={{ border: "none", borderRadius: 50, margin: "8px 0px" }}
+        />
       </div>
-      {loading && <SkeletonLayout />}
-      {channels && (
+      {(loading || (searchTerm && searchChannelsMobileLoading)) && (
+        <SkeletonLayout />
+      )}
+      {(searchTerm
+        ? searchChannels &&
+          !searchChannelsMobileLoading &&
+          !searchChannelsMobileError
+        : channels) && (
         <div style={{ marginBottom: 15 }}>
-          <Text
-            style={{
-              marginLeft: 15,
-              marginBottom: 10,
-              fontWeight: 600,
-              fontSize: 16,
-              display: "block",
-            }}
-          >
-            {channels.totalEntries} Integrations
-          </Text>
-          {channels.entries.map((channel) => (
+          {!loading && (!searchTerm || !searchChannelsMobileLoading) && (
+            <Text
+              style={{
+                marginLeft: 15,
+                marginBottom: 10,
+                fontWeight: 600,
+                fontSize: 16,
+                display: "block",
+              }}
+            >
+              {searchTerm ? searchChannels.length : channels.totalEntries}{" "}
+              Integrations
+            </Text>
+          )}
+          {(searchTerm && !searchChannelsMobileError
+            ? searchChannels
+            : channels.entries
+          ).map((channel) => (
             <MobileTableRow
               id={channel.id}
               key={channel.id}
@@ -58,7 +122,8 @@ export default ({ loading, channels, refetch }) => {
               }
             />
           ))}
-          <div style={{ height: 1 }} ref={containerRef}/>
+          {searchChannelsMobileError && <ErrorMessage />}
+          {!searchTerm && <div style={{ height: 1 }} ref={containerRef} />}
         </div>
       )}
 
--- lib/console/search/search_resolver.ex
+++ lib/console/search/search_resolver.ex
@@ -20,4 +20,12 @@ defmodule Console.Search.SearchResolver do
   def paginated_search_hotspots(%{query: query, page: page, page_size: page_size, column: column, order: order}, %{context: %{current_organization: _current_organization}}) do
     {:ok, Search.run_for_hotspots(query, page, page_size, column, order)}
   end
+
+  def search_channels_mobile(%{query: query}, %{context: %{current_organization: current_organization}}) do
+    {:ok, Search.run_for_channels_mobile(query, current_organization)}
+  end
+
+  def search_devices_mobile(%{query: query}, %{context: %{current_organization: current_organization}}) do
+    {:ok, Search.run_for_devices_mobile(query, current_organization)}
+  end
 end
