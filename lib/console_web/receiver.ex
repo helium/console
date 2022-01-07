22ed2de18e4cd53ef71aa0751681263b033b956d
victor <victor@helium.com>
2022-01-07 15:51:24 -0800

    Next level

--- lib/console_web/receiver.ex
+++ lib/console_web/receiver.ex
@@ -1,31 +1,58 @@
 defmodule ConsoleWeb.Receiver do
   use GenServer
+  use AMQP
 
-  #Client
   def start_link(initial_state) do
     GenServer.start_link(__MODULE__, initial_state)
   end
 
-  #Server
-  def init(events) do
-    {:ok, connection} = AMQP.Connection.open
-    {:ok, channel} = AMQP.Channel.open(connection)
-    {:ok, _} = AMQP.Queue.declare(channel, "events_queue")
-    AMQP.Queue.subscribe(channel, "events_queue", fn payload, _meta -> IO.puts("Received: #{payload}") end)
+  def init(_opts) do
+    {:ok, conn} = Connection.open()
+    {:ok, channel} = Channel.open(conn)
+    {:ok, _} = Queue.declare(channel, "events_queue", durable: true)
 
-    {:ok, events}
+    # Limit unacknowledged messages to 10
+    # :ok = Basic.qos(channel, prefetch_count: 10)
+    {:ok, _consumer_tag} = Basic.consume(channel, "events_queue")
+    {:ok, channel}
   end
 
-  def handle_info({:basic_consume_ok, _}, events) do
-    {:noreply, events}
+  # Confirmation sent by the broker after registering this process as a consumer
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, channel) do
+    {:noreply, channel}
   end
 
-  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, events) do
+  # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, channel) do
+    {:stop, :normal, channel}
+  end
+
+  # Confirmation sent by the broker to the consumer process after a Basic.cancel
+  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, channel) do
+    {:noreply, channel}
+  end
+
+  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, channel) do
     # You might want to run payload consumption in separate Tasks in production
-    IO.inspect payload
-    IO.inspect tag
-    IO.inspect redelivered
-    IO.inspect payload
-    {:noreply, events}
+    consume(channel, tag, redelivered, payload)
+    {:noreply, channel}
+  end
+
+  defp consume(channel, tag, redelivered, payload) do
+    number = String.to_integer(payload)
+    :ok = Basic.ack channel, tag
+    IO.puts "Consumed a #{number}."
+
+  rescue
+    # Requeue unless it's a redelivered message.
+    # This means we will retry consuming a message once in case of exception
+    # before we give up and have it moved to the error queue
+    #
+    # You might also want to catch :exit signal in production code.
+    # Make sure you call ack, nack or reject otherwise consumer will stop
+    # receiving messages.
+    exception ->
+      :ok = Basic.reject channel, tag, requeue: not redelivered
+      IO.puts "Error converting #{payload} to integer"
   end
 end
