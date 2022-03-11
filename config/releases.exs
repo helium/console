45b984cca5a2a0d81af0eb73076024efb3d3e5de
victor <victor@helium.com>
2022-03-10 16:10:04 -0800

    Change to download hotspots zip file

--- config/releases.exs
+++ config/releases.exs
@@ -34,8 +34,7 @@ config :console, ConsoleWeb.Guardian,
 
 config :console,
   router_secrets: String.split(System.get_env("ROUTER_SECRETS"), ","),
-  blockchain_api_retry: String.to_integer(System.get_env("BLOCKCHAIN_API_RETRY") || "1"),
-  blockchain_api_url: System.get_env("BLOCKCHAIN_API_URL")
+  blockchain_api_retry: String.to_integer(System.get_env("BLOCKCHAIN_API_RETRY") || "1")
 
 config :console,
   self_hosted: true
