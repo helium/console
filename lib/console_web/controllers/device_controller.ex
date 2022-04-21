e03e50178e873f541685e179ab635576aaa5002e
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2022-04-21 15:11:50 -0600

    change msg (#1132)

--- lib/console_web/controllers/device_controller.ex
+++ lib/console_web/controllers/device_controller.ex
@@ -57,7 +57,7 @@ defmodule ConsoleWeb.DeviceController do
         |> put_resp_header("message",  "Device #{device.name} added successfully")
         |> render("show.json", device: device)
       {:error, "Device limit reached"} ->
-        {:error, :forbidden, "The device/organization cap has been met. To add devices or organizations for commercial use cases, reach out to sales@nova.xyz."}
+        {:error, :forbidden, "The device/organization cap has been met. To add devices or organizations for commercial use cases, check docs for Console Hosting Providers."}
       {:error, %Ecto.Changeset{ valid?: false, errors: [dev_eui: {msg, [constraint: :unique, constraint_name: "devices_dev_eui_app_eui_app_key_index"]}]}} ->
         {:error, :forbidden, msg}
       {:error, %Ecto.Changeset{ valid?: false, errors: [message: msg]}} ->
