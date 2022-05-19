c088e4f741e302c827f05f75da8992ca2b9e633a
Alejandra Buznego <abuznego23@gmail.com>
2022-05-18 17:10:45 -0700

    broadcast device ids to router when hotspots are preferred/unpreferred

--- lib/console/packet_configs/packet_configs.ex
+++ lib/console/packet_configs/packet_configs.ex
@@ -13,7 +13,7 @@ defmodule Console.PacketConfigs do
   end
 
   def get_all_organization_packet_configs(org_id) do
-    from(m in PacketConfig, where: m.organization_id == ^org_id)
+    from(pc in PacketConfig, where: pc.organization_id == ^org_id)
     |> Repo.all()
   end
 
@@ -33,12 +33,12 @@ defmodule Console.PacketConfigs do
     |> Repo.update()
   end
 
-  def get_all_packet_config_associated_device_ids(id) do
+  def get_all_packet_config_associated_device_ids(ids) do
     device_ids =
-      get_associated_packet_config_devices(id)
+      get_associated_packet_config_devices(ids)
 
     label_device_ids =
-      get_associated_packet_config_labels(id)
+      get_associated_packet_config_labels(ids)
       |> Enum.map(fn l -> l.devices end)
       |> List.flatten()
       |> Enum.map(fn d -> d.id end)
@@ -46,14 +46,14 @@ defmodule Console.PacketConfigs do
     device_ids ++ label_device_ids |> Enum.uniq()
   end
 
-  defp get_associated_packet_config_devices(id) do
-    from(d in Device, where: d.packet_config_id == ^id)
+  defp get_associated_packet_config_devices(ids) do
+    from(d in Device, where: d.packet_config_id in ^ids)
     |> select([d], d.id)
     |> Repo.all()
   end
 
-  defp get_associated_packet_config_labels(id) do
-    from(l in Label, where: l.packet_config_id == ^id)
+  defp get_associated_packet_config_labels(ids) do
+    from(l in Label, where: l.packet_config_id in ^ids)
     |> preload([:devices])
     |> Repo.all()
   end
@@ -61,5 +61,13 @@ defmodule Console.PacketConfigs do
   def turn_off_preferred_active_for_all(organization) do
     from(pc in PacketConfig, where: pc.organization_id == ^organization.id and pc.preferred_active == true)
     |> Repo.update_all(set: [preferred_active: false])
+  end
+
+  def get_device_ids_associated_with_preferred_active_packet_configs(organization) do
+    from(pc in PacketConfig,
+      select: pc.id,
+      where: pc.organization_id == ^organization.id and pc.preferred_active == true)
+    |> Repo.all()
+    |> get_all_packet_config_associated_device_ids()
   end
 end
