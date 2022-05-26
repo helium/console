c8bebb10cbee3ce05aa0cda1ca91c1439fb315c6
victor <victor@helium.com>
2022-05-26 13:30:51 -0700

    Change HOSTNAME env var to ENV_DOMAIN for consolidation

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :console, ConsoleWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
+  url: [scheme: "https", host: System.get_env("ENV_DOMAIN"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
