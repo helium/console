c57cf026317e5c8393aa8f8193769fd36a4c5c76
Victor Mu <victor@helium.com>
2022-10-12 10:31:25 -0700

    Fix issue with magic 2fa login

--- assets/js/components/auth/MagicSocialCallback.jsx
+++ assets/js/components/auth/MagicSocialCallback.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
-import { Spin } from "antd";
 import { getRedirectResult } from '../../actions/magic';
 
-const MagicSocialCallback = () => {
+const MagicSocialCallback = ({ history }) => {
+  const urlParams = new URLSearchParams(history.location.search)
+  const fromGoogleAuth = urlParams.get('from-gauth') !== 'false'
+
   useEffect(() => {
-    getRedirectResult()
+    if (fromGoogleAuth) getRedirectResult()
   }, []);
 
   return (
     <div style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-      <p>Please wait while you are automatically redirected...</p>
+      {
+        fromGoogleAuth ? (
+          <p>Please wait while you are automatically redirected...</p>
+        ) : (
+          <p>Login successful, you may close this window and go back to your original page.</p>
+        )
+      }
+      
     </div>
   );
 };
