c888d652d35616a9d6f71458fd5c10b962d064ff
Rutger Luyckx <rutger.luyckx@blockbax.com>
2023-03-16 16:22:49 +0100

    Fix bug in communicty channel creation

--- lib/console_web/controllers/v1/channel_controller.ex
+++ lib/console_web/controllers/v1/channel_controller.ex
@@ -82,7 +82,7 @@ defmodule ConsoleWeb.V1.ChannelController do
               "name" => name,
               "type" => type,
               "organization_id" => current_organization.id,
-              "credentials" -> %{
+              "credentials" => %{
                 "blockbaxAccessToken" => blockbaxAccessToken,
                 "blockbaxInbdoundConnectorEndpoint" => blockbaxInbdoundConnectorEndpoint,
               }
