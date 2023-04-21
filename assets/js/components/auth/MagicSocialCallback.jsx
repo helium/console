c43f65cf4d0d05480f9da34914531d446fdbb7b4
Victor Mu <victor@helium.com>
2023-04-21 09:01:07 -0700

    Update magic

--- assets/js/components/auth/MagicSocialCallback.jsx
+++ assets/js/components/auth/MagicSocialCallback.jsx
@@ -1,24 +1,14 @@
 import React, { useEffect } from 'react';
 import { getRedirectResult } from '../../actions/magic';
 
-const MagicSocialCallback = ({ history }) => {
-  const urlParams = new URLSearchParams(history.location.search)
-  const fromGoogleAuth = urlParams.get('from-gauth') !== 'false'
-
+const MagicSocialCallback = () => {
   useEffect(() => {
-    if (fromGoogleAuth) getRedirectResult()
+    getRedirectResult()
   }, []);
 
   return (
     <div style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-      {
-        fromGoogleAuth ? (
-          <p>Please wait while you are automatically redirected...</p>
-        ) : (
-          <p>Login successful, you may close this window and go back to your original page.</p>
-        )
-      }
-      
+      <p>Please wait while you are automatically redirected...</p>
     </div>
   );
 };
