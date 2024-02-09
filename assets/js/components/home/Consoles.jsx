e9fadbd23e25c36d8a5d99903fb731faefcd3f8a
mbthiery <mbthiery@gmail.com>
2024-02-09 11:40:25 -0500

    Left hand side of new home page

---
+++ assets/js/components/home/Consoles.jsx
@@ -0,0 +1,52 @@
+import { Typography } from "antd";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../../img/mobile/logo.svg";
+import styles from "./consoles.module.css";
+const { Text, Title } = Typography;
+
+export const Consoles = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.body}>
+        <div className={styles.nav}>
+          <img className={styles.logo} src={Logo} />
+          <NavLink className={styles.legacyLink} to="/login">
+            Login to Legacy
+          </NavLink>
+        </div>
+        <div className={styles.onboard}>
+          <h1 className={styles.header}>Onboard a device</h1>
+          <p className={styles.text}>
+            The Helium ecosystem supports multiple network operators as well as
+            the ability to run your own LoRaWAN Network Server.
+          </p>
+          <div className={styles.buttonWrapper}>
+            <a
+              className={styles.button}
+              href="https://docs.helium.com/iot/find-a-lns-provider"
+            >
+              <span className={styles.buttonText}>Use an existing LNS</span>
+            </a>
+            <a
+              className={styles.button}
+              href="https://docs.helium.com/iot/run-an-lns"
+            >
+              <span className={styles.buttonText}>Run your own LNS</span>
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.bottom}>
+          <p className={styles.text}>
+            Still testing things out? Try onboarding a device below:{" "}
+            <a className={styles.devicePortal} href="https://helium.com">
+              Onboard in Device Portal
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className={styles.consoles}></div>
+    </div>
+  );
+};
